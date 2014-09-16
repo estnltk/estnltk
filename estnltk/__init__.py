d14f3217e8a54338d36cc3e3629dfc0d40d07994
Timo Petmanson <brainscauseminds@gmail.com>
2014-09-17 00:11:32 +0300

    Added basic clause segmenter test

--- estnltk/__init__.py
+++ estnltk/__init__.py
@@ -2,5 +2,6 @@
 
 from corpus import PlainTextDocumentImporter
 from morf import PyVabamorfAnalyzer
+from javabridge_support import terminate_jvm
 from clausesegmenter import ClauseSegmenter
 from encoding import EncodingDetector
