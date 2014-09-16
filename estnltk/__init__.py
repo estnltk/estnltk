6ff7f5850e2229e58f465ec5cfd8f4c6c6578009
Timo Petmanson <brainscauseminds@gmail.com>
2014-09-16 15:08:19 +0300

    Timo local changes

--- estnltk/__init__.py
+++ estnltk/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from corpus import PlainTextDocumentImporter
+from morf import PyVabamorfAnalyzer
+from clausesegmenter import ClauseSegmenter
+from encoding import EncodingDetector
