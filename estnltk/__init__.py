d3b84af1487df3b4d9570858f2c3cf15136ef6f9
Timo Petmanson <brainscauseminds@gmail.com>
2014-09-18 18:31:31 +0300

    Removed javabridge import from __init__.py

--- estnltk/__init__.py
+++ estnltk/__init__.py
@@ -2,6 +2,5 @@
 
 from corpus import PlainTextDocumentImporter
 from morf import PyVabamorfAnalyzer
-from javabridge_support import terminate_jvm
 from clausesegmenter import ClauseSegmenter
 from encoding import EncodingDetector
