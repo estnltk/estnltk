556e977588acaa684a85254639d6d2691eeec88f
Timo Petmanson <brainscauseminds@gmail.com>
2014-12-09 17:18:25 +0200

    Updated morphoanalyzer test

--- estnltk/tests/test_morph.py
+++ estnltk/tests/test_morph.py
@@ -4,26 +4,32 @@ from __future__ import unicode_literals, print_function
 from estnltk import Tokenizer
 from estnltk import PyVabamorfAnalyzer
 from estnltk.core import JsonPaths
+from estnltk.names import *
 
 import unittest
 import os
 
-# also include pyvabamorf tests as they are part of estnltk
-from pyvabamorf.tests.test_analyze import *
-from pyvabamorf.tests.test_synthesize import *
-from pyvabamorf.tests.test_multi import *
 
 class PyVabamorfAnalyzerTest(unittest.TestCase):
     
-    def test_morphoanalysis(self):
-        to = Tokenizer()
-        an = PyVabamorfAnalyzer()
-        result = an(to(self.text()))
+    def setUp(self):
+        self.to = Tokenizer()
+        self.an = PyVabamorfAnalyzer()
+    
+    def test_morphoanalysis_corpus(self):
+        result = self.an(self.to(self.text()))
+        # we only check if each word has an analysis element, not
+        # the exact output
+        for word in result.words:
+            self.assertTrue(ANALYSIS in word)
+    
+    def test_morphoanalysis_json(self):
+        result = self.an(self.to(self.text()).to_json())
         # we only check if each word has an analysis element, not
         # the exact output
         for words in JsonPaths.words.find(result):
             for word in words.value:
-                self.assertTrue('analysis' in word)
+                self.assertTrue(ANALYSIS in word)
         
     
     def text(self):
