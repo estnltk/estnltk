78fd6ba10e77e673f686d687b1b62c7759f35e56
Timo Petmanson <brainscauseminds@gmail.com>
2014-12-09 16:56:53 +0200

    Fixed types in core module test

--- estnltk/tests/test_core.py
+++ estnltk/tests/test_core.py
@@ -5,7 +5,9 @@ from estnltk.core import as_unicode, as_binary
 import unittest
 import six
 
+
 class TestAsUnicode(unittest.TestCase):
+    '''Tests for as_unicode function'''
     
     def test_unicode_input(self):
         w = as_unicode('ööTää')
@@ -13,7 +15,7 @@ class TestAsUnicode(unittest.TestCase):
         if six.PY2:
             self.assertTrue(isinstance(w, unicode))
         if six.PY3:
-            self.assertEqual(isinstance(w, str))
+            self.assertTrue(isinstance(w, str))
             
     def test_binary_input(self):
         w = as_unicode('ööTää'.encode('utf-8'))
@@ -21,13 +23,15 @@ class TestAsUnicode(unittest.TestCase):
         if six.PY2:
             self.assertTrue(isinstance(w, unicode))
         if six.PY3:
-            self.assertEqual(isinstance(w, str))
+            self.assertTrue(isinstance(w, str))
             
     def test_invalid_input_type(self):
         w = ['list']
         self.assertRaises(ValueError, as_unicode, w)
-    
+
+
 class TestAsBinary(unittest.TestCase):
+    '''Tests for as_binary function.'''
     
     def test_unicode_input(self):
         w = as_binary('ööTää')
@@ -35,7 +39,7 @@ class TestAsBinary(unittest.TestCase):
         if six.PY2:
             self.assertTrue(isinstance(w, str))
         if six.PY3:
-            self.assertEqual(isinstance(w, bytes))
+            self.assertTrue(isinstance(w, bytes))
         
     def test_binary_input(self):
         w = as_binary('ööTää'.encode('utf-8'))
@@ -43,7 +47,7 @@ class TestAsBinary(unittest.TestCase):
         if six.PY2:
             self.assertTrue(isinstance(w, str))
         if six.PY3:
-            self.assertEqual(isinstance(w, bytes))
+            self.assertTrue(isinstance(w, bytes))
         
     def test_binary_wrong_encoding_raises_valueerror(self):
         w = 'ööTää'.encode('latin-1')
