4d6fb9348a06b057f1519f89954ab5dd6c6899e7
Timo Petmanson <brainscauseminds@gmail.com>
2014-12-11 15:57:15 +0200

    Updated NER test to include a failing case that does not work with Python2, but does work with Python3

--- estnltk/tests/test_ner.py
+++ estnltk/tests/test_ner.py
@@ -17,6 +17,9 @@ Riigikogu on Eesti Vabariigi parlament. Riigikogule kuulub Eestis seadusandlik v
 
 2005. aastal sai peaministriks Andrus Ansip, kes püsis sellel kohal 2014. aastani.
 2006. aastal valiti presidendiks Toomas Hendrik Ilves.
+
+Inimestelt saadud vihjed pole veel politseil aidanud leida 43-aastast Kajar Paasi, kes tema naise sõnul Ardus maanteel rööviti.
+Tuhanded Šotimaa kodud on lääneranniku piirkondi tabanud „ilmapommi“-tormi tõttu elektrita
 '''
 
 
@@ -24,7 +27,7 @@ class NerTest(unittest.TestCase):
     
     def test_basic(self):
         ner_tagged = tagger(analyzer(tokenizer(text)))
-        self.assertEqual(len(ner_tagged.named_entities), 11)
+        self.assertEqual(len(ner_tagged.named_entities), 13)
 
     def test_second_time(self):
         # just repeat the same thing to test if anything breaks second time
@@ -32,7 +35,7 @@ class NerTest(unittest.TestCase):
         
     def test_basic_json(self):
         ner_tagged = Corpus.construct(tagger(analyzer(tokenizer(text)).to_json()))
-        self.assertEqual(len(ner_tagged.named_entities), 11)
+        self.assertEqual(len(ner_tagged.named_entities), 13)
 
 if __name__ == '__main__':
     unittest.main()
--- estnltk/tests/test_render_html.py
+++ estnltk/tests/test_render_html.py
@@ -12,8 +12,8 @@ from estnltk.render_html import render_html
 import unittest
 
 
-text1 = '''Inimestelt saadud vihjed pole veel politseil aidanud leida 43-aastast Kajar Paasi, kes tema naise sõnul Ardus maanteel rööviti.'''
-text2 = '''Tuhanded Šotimaa kodud on lääneranniku piirkondi tabanud „ilmapommi“-tormi tõttu elektrita'''
+text1 = 'Inimestelt saadud vihjed pole veel politseil aidanud leida 43-aastast Kajar Paasi, kes tema naise sõnul Ardus maanteel rööviti.'
+text2 = 'Tuhanded Šotimaa kodud on lääneranniku piirkondi tabanud „ilmapommi“-tormi tõttu elektrita'
 
 tok = Tokenizer()
 ana = PyVabamorfAnalyzer()
@@ -24,6 +24,7 @@ class RenderHtmlTest(unittest.TestCase):
 
     def test_ner_values(self):
         analyzed = List([tag(ana(tok(text1))), tag(ana(tok(text2)))])
+        pprint(analyzed.labels)
         html = render_html(analyzed, NAMED_ENTITIES, LABEL, [('PER', 'color:red'), ('ORG', 'color:green'), ('LOC', 'color:blue')])
         self.assertTrue('Tuhanded <span style="color:blue">' in html)
         self.assertTrue('style="color:red">Kajar Paasi</span>' in html)
