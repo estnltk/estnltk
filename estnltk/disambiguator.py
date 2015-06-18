976570106db8d4ae1607ccdc248e3c75318e19c3
Siim Orasmaa <siim.orasmaa@ut.ee>
2015-06-18 15:22:30 +0300

    Added the lemma-based post-disambiguation of morphological analyses. Still requires further testing.

--- estnltk/disambiguator.py
+++ estnltk/disambiguator.py
@@ -17,33 +17,62 @@ class Disambiguator(object):
         
         use_pre_disambiguation    = kwargs.get('pre_disambiguate', True)
         use_post_disambiguation   = kwargs.get('post_disambiguate', True)
-        # For testing purposes, vabamorf disambiguation can also be switched off:
+        # For testing purposes, morph analysis and morph disambiguation can also 
+        # be switched off:
+        use_vabamorf              = kwargs.get('vabamorf', True)
         use_vabamorf_disambiguate = kwargs.get('vabamorf_disambiguate', True)
         
-        docs = [Text(doc, **kwargs) for doc in docs]
+        # Check, whether the input is a list of lists of docs, or just a list of docs
+        if not self.__isListOfLists( docs ):
+            collections = [ docs ]
+        else:
+            collections = docs
+        
+        #  I. perform morphological analysis, pre_disambiguation, and 
+        #     statistical (vabamorf) disambiguation with-in a single 
+        #     document collection;
+        for docs in collections:
+            docs = [Text(doc, **kwargs) for doc in docs]
 
-        # morf.analysis without disambiguation
-        docs = [doc.tag_analysis() for doc in docs]
+            # morf.analysis without disambiguation
+            if use_vabamorf:
+                docs = [doc.tag_analysis() for doc in docs]
 
-        if use_pre_disambiguation:
-            docs = self.pre_disambiguate(docs)
+            if use_pre_disambiguation:
+                docs = self.pre_disambiguate(docs)
 
-        if use_vabamorf_disambiguate:
-            docs = self.__vabamorf_disambiguate(docs)
+            if use_vabamorf_disambiguate:
+                docs = self.__vabamorf_disambiguate(docs)
 
+        #
+        #  II. perform post disambiguation over all document collections;
+        #
         if use_post_disambiguation:
-            docs = self.post_disambiguate(docs)
+           collections = self.post_disambiguate( collections )
+        return collections if len(collections)>1 else collections[0]
 
-        return docs
 
     def __vabamorf_disambiguate(self, docs):
         # TODO for Timo: extract vabamorf disambiguator from analyzer and apply it here
         return docs
 
+
+    def __isListOfLists(self, docs):
+        ''' Checks whether the input is a list of list of X, where X is either a
+            string or a Text; 
+              TODO: is it compatible with Python 2.7 ???
+        '''
+        return isinstance(docs, list) and \
+                all(isinstance(d1, list) and \
+                  all(isinstance(d11, str) or isinstance(d11, Text) for d11 in d1) for d1 in docs)
+
     # =========================================================
     # =========================================================
     #     Korpusepõhine pärisnimede eelühestamine 
     #     ( corpus-based pre-disambiguation of proper names )
+    #
+    #     Algne algoritm ja teostus:  Heiki-Jaan Kaalep
+    #     Python-i implementatsioon:  Siim Orasmaa
     # =========================================================
     # =========================================================
 
@@ -311,15 +340,248 @@ class Disambiguator(object):
         self.__disambiguate_proper_names_2(docs, lexicon)
         return docs
 
+
     # =========================================================
     # =========================================================
     #    Korpusepõhine lemmade järelühestamine 
     #    ( corpus-based post-disambiguation of lemmas )
+    #
+    #     Algne algoritm ja teostus:  Heiki-Jaan Kaalep
+    #     Python-i implementatsioon:  Siim Orasmaa
     # =========================================================
     # =========================================================
-    
-    def post_disambiguate(self, docs):
-        # TODO: sisendiks peaks  docs  asemel olema  list of docs
-        # TODO: implementatsioon
-        return docs
+
+    def __analyses_match(self, analysisA, analysisB):
+        ''' Leiame, kas tegu on duplikaatidega ehk täpselt üht ja sama
+            morfoloogilist infot sisaldavate analüüsidega. '''
+        return POSTAG in analysisA and POSTAG in analysisB and \
+               analysisA[POSTAG]==analysisB[POSTAG] and \
+               ROOT in analysisA and ROOT in analysisB and \
+               analysisA[ROOT]==analysisB[ROOT] and \
+               FORM in analysisA and FORM in analysisB and \
+               analysisA[FORM]==analysisB[FORM] and \
+               CLITIC in analysisA and CLITIC in analysisB and \
+               analysisA[CLITIC]==analysisB[CLITIC] and \
+               ENDING in analysisA and ENDING in analysisB and \
+               analysisA[ENDING]==analysisB[ENDING]
+
+
+    def __remove_duplicate_and_problematic_analyses(self, docs):
+        ''' 1) Eemaldab sisendkorpuse kõigi sõnade morf analüüsidest duplikaadid 
+               ehk siis korduvad analüüsid; Nt sõna 'palk' saab kaks analyysi: 
+               'palk' (mis käändub 'palk\palgi') ja 'palk' (mis käändub 'palk\palga'),
+               aga pärast duplikaatide eemaldamist jääb alles vaid üks;
+            2) Kui verbi analüüside hulgas on alles nii '-tama' kui ka '-ma', siis
+               jätta alles vaid '-ma' analüüsid;
+        '''
+        for doc in docs:
+            for word in doc[WORDS]:
+                # 1) Leiame k6ik analyysi-duplikaadid (kui neid on)
+                toDelete = []
+                for i in range(len(word[ANALYSIS])):
+                    if i+1 < len(word[ANALYSIS]):
+                        for j in range(i+1, len(word[ANALYSIS])):
+                            analysisI = word[ANALYSIS][i]
+                            analysisJ = word[ANALYSIS][j]
+                            if self.__analyses_match(analysisI, analysisJ):
+                                if j not in toDelete:
+                                    toDelete.append(j)
+                # 2) Kustutame yleliigsed analyysid
+                if toDelete:
+                    for a in sorted(toDelete, reverse=True):
+                        del word[ANALYSIS][a]
+                #
+                # *) Kui verbi analüüside puhul on olemas nii '-tama' kui ka '-ma' 
+                #    lõpp, siis jätta alles vaid -ma, ülejäänud kustutada;
+                #    Nt   lõpetama:    lõp+tama,   lõppe+tama,  lõpeta+ma
+                #         teatama:     tead+tama,  teata+ma
+                #
+                if any([ a[POSTAG]=='V' and a[ENDING]=='tama' for a in word[ANALYSIS] ]) and \
+                   any([ a[POSTAG]=='V' and a[ENDING]=='ma' for a in word[ANALYSIS] ]):
+                    toDelete = []
+                    for a in range( len(word[ANALYSIS]) ):
+                        if word[ANALYSIS][a][POSTAG]=='V' and \
+                           word[ANALYSIS][a][ENDING]=='tama':
+                            toDelete.append(a)
+                    if toDelete:
+                        for a in sorted(toDelete, reverse=True):
+                            del word[ANALYSIS][a]
+
+
+    def __find_hidden_analyses(self, docs):
+        ''' Jätab meelde, millised analüüsid on nn peidetud ehk siis mida ei 
+            tule arvestada lemmade järelühestamisel:
+             *) kesksõnade nud, dud, tud mitmesused; 
+             *) muutumatute sõnade sõnaliigi mitmesus;
+             *) oleviku 'olema' mitmesus ('nad on' vs 'ta on');
+             *) asesõnade ainsuse-mitmuse mitmesus;
+             *) arv- ja asesõnade vaheline mitmesus; 
+            Tagastab sõnastiku peidetud analüüse sisaldanud sõnade asukohtadega, 
+            iga võti kujul (doc_index, word_index); '''
+        hidden = dict()
+        nudTudLopud = re.compile('^.*[ntd]ud$')
+        for d in range(len(docs)):
+            for w in range(len(docs[d][WORDS])):
+                word = docs[d][WORDS][w]
+                if ANALYSIS in word and len(word[ANALYSIS]) > 1:
+                    #
+                    # 1) Kui enamus analüüse on nud/tud/dud analüüsid, peida mitmesus:
+                    #    kõla+nud //_V_ nud, //    kõla=nud+0 //_A_ //    kõla=nud+0 //_A_ sg n, //    kõla=nud+d //_A_ pl n, //
+                    nudTud = [ nudTudLopud.match(a[ROOT]) != None or \
+                               nudTudLopud.match(a[ENDING]) != None \
+                               for a in word[ANALYSIS] ]
+                    if nudTud.count( True ) > 1:
+                        hidden[(d, w)] = 1
+                    #
+                    # 2) Kui analyysidel on sama lemma ja puudub vormitunnus, siis peida mitmesused ära:
+                    #    Nt    kui+0 //_D_ //    kui+0 //_J_ //
+                    #          nagu+0 //_D_ //    nagu+0 //_J_ //
+                    lemmas = set([ a[ROOT] for a in word[ANALYSIS] ])
+                    forms  = set([ a[FORM] for a in word[ANALYSIS] ])
+                    if len(lemmas) == 1 and len(forms) == 1 and (list(forms))[0] == '':
+                        hidden[(d, w)] = 1
+                    #
+                    # 3) Kui 'olema'-analyysidel on sama lemma ning sama l6pp, peida mitmesused:
+                    #    Nt  'nad on' vs 'ta on' saavad sama olema-analyysi, mis jääb mitmeseks;
+                    endings  = set([ a[ENDING] for a in word[ANALYSIS] ])
+                    if len(lemmas) == 1 and (list(lemmas))[0] == 'ole' and len(endings) == 1 \
+                       and (list(endings))[0] == '0':
+                        hidden[(d, w)] = 1
+                    #
+                    # 4) Kui asesõnadel on sama lemma ja lõpp, peida ainsuse/mitmuse mitmesus:
+                    #    Nt     kõik+0 //_P_ sg n //    kõik+0 //_P_ pl n //
+                    #           kes+0 //_P_ sg n //    kes+0 //_P_ pl n //
+                    postags  = set([ a[POSTAG] for a in word[ANALYSIS] ])
+                    if len(lemmas) == 1 and len(postags) == 1 and 'P' in postags and \
+                       len(endings) == 1:
+                        hidden[(d, w)] = 1
+                    #
+                    # 5) Kui on sama lemma ja lõpp, peida arv- ja asesõnadevaheline mitmesus:
+                    #    Nt     teine+0 //_O_ pl n, //    teine+0 //_P_ pl n, //
+                    #           üks+l //_N_ sg ad, //    üks+l //_P_ sg ad, //
+                    if len(lemmas) == 1 and 'P' in postags and ('O' in postags or \
+                       'N' in postags) and len(endings) == 1:
+                        hidden[(d, w)] = 1
+        return hidden
+
+
+    def __create_lemma_frequency_lexicon(self, docs, hiddenWords, lexicon, amb_lexicon):
+        ''' Täiendab etteantud sagedusleksikone antud korpuse (docs) põhjal:
+            *) yldist sagedusleksikoni, kus on k6ik lemmad, v.a. lemmad, 
+               mis kuuluvad nn peidetud sõnade hulka (hiddenWords); 
+            *) mitmeste sagedusleksikoni, kus on vaid mitmeste analyysidega
+               s6nades esinenud lemmad, v.a. (hiddenWords) lemmad, koos
+               nende yldiste esinemissagedustega (esimesest leksikonist);
+        '''
+        for d in range(len(docs)):
+            for w in range(len(docs[d][WORDS])):
+                word = docs[d][WORDS][w]
+                # Jätame vahele nn peidetud sõnad
+                if (d, w) in hiddenWords:
+                    continue
+                isAmbiguous = (len(word[ANALYSIS])>1)
+                # Jäädvustame sagedused, verbide omad eraldiseisva märkega:
+                for a in word[ANALYSIS]:
+                    lemma = a[ROOT]+'ma' if a[POSTAG]=='V' else a[ROOT]
+                    # 1) Jäädvustame üldise sageduse
+                    if lemma not in lexicon:
+                        lexicon[lemma] = 1
+                    else:
+                        lexicon[lemma] += 1
+                    # 2) Jäädvustame mitmeste sõnade esinemise
+                    if isAmbiguous:
+                        amb_lexicon[lemma] = 1
+        #  Kanname yldisest sagedusleksikonist sagedused yle mitmeste lemmade
+        # sagedusleksikoni
+        for lemma in amb_lexicon.keys():
+            amb_lexicon[lemma] = lexicon[lemma]
+
+
+    def __disambiguate_with_lexicon(self, docs, lexicon, hiddenWords):
+        ''' Teostab lemmade leksikoni järgi mitmeste morf analüüside 
+            ühestamise - eemaldab üleliigsed analüüsid;
+            Toetub ideele "üks tähendus teksti kohta": kui mitmeseks jäänud 
+            lemma esineb tekstis/korpuses ka mujal ning lõppkokkuvõttes 
+            esineb sagedamini kui alternatiivsed analüüsid, siis tõenäoliselt
+            see ongi õige lemma/analüüs;
+        '''
+        for d in range(len(docs)):
+            for w in range(len(docs[d][WORDS])):
+                word = docs[d][WORDS][w]
+                # Jätame vahele nn peidetud sõnad
+                if (d, w) in hiddenWords:
+                    continue
+                # Vaatame vaid mitmeseks jäänud analüüsidega sõnu
+                if len(word[ANALYSIS]) > 1:
+                    #  1) Leiame suurima esinemissageduse mitmeste lemmade seas
+                    highestFreq = 0
+                    for analysis in word[ANALYSIS]:
+                        lemma = analysis[ROOT]+'ma' if analysis[POSTAG]=='V' else analysis[ROOT]
+                        if lemma in lexicon and lexicon[lemma] > highestFreq:
+                            highestFreq = lexicon[lemma]
+                    if highestFreq > 0:
+                        #  2) Jätame välja kõik analüüsid, mille lemma esinemissagedus
+                        #     on väiksem kui suurim esinemissagedus;
+                        toDelete = []
+                        for analysis in word[ANALYSIS]:
+                            lemma = analysis[ROOT]+'ma' if analysis[POSTAG]=='V' else analysis[ROOT]
+                            freq = lexicon[lemma] if lemma in lexicon else 0
+                            if freq < highestFreq:
+                                toDelete.append(analysis)
+                        for analysis in toDelete:
+                            word[ANALYSIS].remove(analysis)
+
+
+    def post_disambiguate(self, collections):
+        '''  Teostab mitmeste lemmade järelühestamise. Järelühestamine toimub kahes etapis:
+            kõigepealt ühe dokumendikollektsiooni piires ning seejärel üle kõigi
+            dokumendikollektsioonide (kui sisendis on rohkem kui 1 dokumendikollektsioon);
+             Sisuliselt kasutatakse ühestamisel "üks tähendus teksti kohta" idee laiendust:
+            kui mitmeseks jäänud lemma esineb ka mujal (samas kollektsioonis või kõigis
+            kollektsioonides) ning lõppkokkuvõttes esineb sagedamini kui alternatiivsed
+            analüüsid, siis tõenäoliselt see ongi õige lemma/analüüs;
+        '''
+        #
+        #  I etapp: ühestame ühe dokumendikollektsiooni piires 
+        #           (nt üle kõigi samal päeval ilmunud ajaleheartiklite);
+        #
+        for docs in collections:
+            # 1) Eemaldame analüüside seast duplikaadid ja probleemsed
+            self.__remove_duplicate_and_problematic_analyses(docs)
+            # 2) Leiame sõnad, mis sisaldavad nn ignoreeritavaid mitmesusi
+            #    (selliseid mitmesusi, mida me ühestamisel ei arvesta);
+            hiddenWords = self.__find_hidden_analyses(docs)
+            # 3) Leiame kaks lemmade sagedusleksikoni: üldise lemmade sagedus-
+            #    leksikoni ja mitmeseks jäänud sonade lemmade sagedusleksikoni;
+            #    Mitmeste lemmade leksikoni läheb kirja vastavate lemmade yldine 
+            #    sagedus korpuses (kuhu arvatud ka sagedus ühestatud sõnades);
+            genLemmaLex = dict()
+            ambLemmaLex = dict()
+            self.__create_lemma_frequency_lexicon(docs, hiddenWords, ambLemmaLex, genLemmaLex)
+            # 4) Teostame lemmade-p6hise yhestamise: mitmeseks j22nud analyyside 
+            #    puhul j2tame alles analyysid, mille lemma esinemisagedus on suurim
+            #    (ja kui k6igi esinemissagedus on v6rdne, siis ei tee midagi)
+            self.__disambiguate_with_lexicon(docs, ambLemmaLex, hiddenWords)
+        #
+        #  II etapp: ühestame üle kõikide dokumendikollektsioonide 
+        #            (nt üle kõigi ühe aasta ajalehenumbrite, kus
+        #             üks ajalehenumber sisaldab kõiki sama päeva artikleid);
+        #
+        if len(collections) > 1:
+            # Genereerime mitmeste sagedusleksikoni
+            genLemmaLex = dict()
+            ambLemmaLex = dict()
+            for docs in collections:
+                # *) Leiame sõnad, mis sisaldavad nn ignoreeritavaid mitmesusi
+                hiddenWords = self.__find_hidden_analyses(docs)
+                # *) Täiendame üldist lemmade sagedusleksikoni ja mitmeseks jäänud
+                #    lemmade sagedusleksikoni;
+                self.__create_lemma_frequency_lexicon(docs, hiddenWords, ambLemmaLex, genLemmaLex)
+            # Teostame järelühestamise
+            for docs in collections:
+                # *) Leiame sõnad, mis sisaldavad nn ignoreeritavaid mitmesusi
+                hiddenWords = self.__find_hidden_analyses(docs)
+                # *) Teostame lemmade-p6hise yhestamise;
+                self.__disambiguate_with_lexicon(docs, ambLemmaLex, hiddenWords)
+        return collections
 
