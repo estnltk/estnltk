0026913da0cf49b299c134de709a53e921519209
Karl <karl.valliste@gmail.com>
2015-07-01 13:32:41 +0300

    esimesed katsed

--- estnltk/prettyprinter/prettyprinter.py
+++ estnltk/prettyprinter/prettyprinter.py
@@ -21,11 +21,20 @@ from estnltk import Text
 
 class PrettyPrinter(object):
     """Class for formatting Text instances as HTML & CSS."""
-    text=""
-    vorming=[]
     def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            print(key, value)
+        variables = {'color': 'black', 'background': 'white', 'font': 'serif', 'weight': 'normal',
+                     'italics': 'normal', 'underline': 'normal', 'size': 'normal', 'tracking': 'normal'}
+        for k,v in kwargs.items():
+            print(k, v)
+        self.color = variables['color']
+        self.background = variables['background']
+        self.font = variables['font']
+        self.weight = variables['weight']
+        self.italics = variables['italics']
+        self.underline = variables['underline']
+        self.size = variables['size']
+        self.tracking = variables['tracking']
+        self.text=kwargs['text']
         return
 
     def render(self, text):
@@ -38,3 +47,4 @@ class PrettyPrinter(object):
         return ''
 kwargs={'text': "mis see on", 'vorming': ["a1", "a2"]}
 p1=PrettyPrinter(**kwargs)
+print(p1.text)
