7dda2e7e471179d22e748f5054679af5c0214870
Karl <karl.valliste@gmail.com>
2015-07-01 15:51:20 +0300

    esimesed katsed

--- estnltk/prettyprinter/prettyprinter.py
+++ estnltk/prettyprinter/prettyprinter.py
@@ -22,10 +22,26 @@ from estnltk import Text
 class PrettyPrinter(object):
     """Class for formatting Text instances as HTML & CSS."""
     def __init__(self, **kwargs):
-        variables = {'color': 'black', 'background': 'white', 'font': 'serif', 'weight': 'normal',
-                     'italics': 'normal', 'underline': 'normal', 'size': 'normal', 'tracking': 'normal'}
+        asi=kwargs['asesõna']
+        print(asi)
+        asesõna = wordType(asi)
+        return
+
+    def render(self, text):
+        text = Text(text)
+        return text.get.word_texts.lemmas.postag_descriptions.as_dict
+
+    @property
+    def css(self):
+        """The CSS of the prettyprinter."""
+        return ''
+
+class wordType(object):
+    def __init__(self, **kwargs):
+        variables = {'color': 'black', 'background': 'white', 'font': 'serif', 'weight': 'normal','italics': 'normal',
+                     'underline': 'normal', 'size': 'normal', 'tracking': 'normal', 'text': ''}
         for k,v in kwargs.items():
-            print(k, v)
+            variables[k] = v
         self.color = variables['color']
         self.background = variables['background']
         self.font = variables['font']
@@ -34,17 +50,24 @@ class PrettyPrinter(object):
         self.underline = variables['underline']
         self.size = variables['size']
         self.tracking = variables['tracking']
-        self.text=kwargs['text']
+        self.text=variables['text']
         return
 
-    def render(self, text):
-        text = Text(text)
-        return text
+"""Current test protocols"""
 
-    @property
-    def css(self):
-        """The CSS of the prettyprinter."""
-        return ''
-kwargs={'text': "mis see on", 'vorming': ["a1", "a2"]}
-p1=PrettyPrinter(**kwargs)
-print(p1.text)
+kwargs = {'asesõna': {'text': "mis", 'color': 'red', 'size': 'large'},
+          'tegusõna': {'text': "on", 'color': 'green', 'size': 'small'}}
+p2 = PrettyPrinter(**kwargs)
+p2Render = p2.render(p2.text)
+print(p2Render['postag_descriptions'])
+
+
+print(p2.asesõna.text)
+print(p2.asesõna.color)
+print(p2.asesõna.background)
+print(p2.asesõna.font)
+print(p2.asesõna.weight)
+print(p2.asesõna.italics)
+print(p2.asesõna.underline)
+print(p2.asesõna.size)
+print(p2.asesõna.tracking)
