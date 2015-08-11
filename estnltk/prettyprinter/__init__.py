d42e33f29e5fb8b72183b18aa3e943b38e742196
Timo Petmanson <tpetmanson@gmail.com>
2015-08-11 22:31:04 +0300

    Created shortcut in overall tests directory linking to PP tests

---
+++ estnltk/prettyprinter/__init__.py
@@ -0,0 +1,1 @@
+from .prettyprinter import PrettyPrinter
---
+++ estnltk/tests/test_prettyprinter.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+
+from ..prettyprinter.tests.test_initialization import *
+from ..prettyprinter.tests.test_css import *
+from ..prettyprinter.tests.test_render import *
