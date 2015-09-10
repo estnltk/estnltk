2b5493cc813c5791be9fa5b584174e189be94b9e
Timo Petmanson <tpetmanson@gmail.com>
2015-09-10 19:09:26 +0300

    Update top-level package imports for prettyprinter tests

--- estnltk/tests/test_prettyprinter.py
+++ estnltk/tests/test_prettyprinter.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals, print_function, absolute_import
 
 from ..prettyprinter.tests.test_initialization import *
 from ..prettyprinter.tests.test_css import *
-from ..prettyprinter.tests.test_render import *
-from ..prettyprinter.tests.test_rules import *
+from ..prettyprinter.tests.test_basic_render import *
+from ..prettyprinter.tests.test_rules_render import *
