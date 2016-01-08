ee2e7525fe7ee0f6d42c57407851a3c2b73b0541
Uku Raudvere <u.raudvere@gmail.com>
2016-01-08 12:34:43 +0200

    Nope, reverted to workaround

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,3 +3,4 @@ CC=${PREFIX}/bin/g++
 CXX=${PREFIX}/bin/g++
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
