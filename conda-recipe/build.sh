3753782784d6a90bfd2128fc5d4bbdced7fee5f6
Uku Raudvere <u.raudvere@gmail.com>
2016-01-08 12:25:23 +0200

    removed workaround for build

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,9 +2,4 @@
 CC=${PREFIX}/bin/g++
 CXX=${PREFIX}/bin/g++
 
-#lazy and quick workaround for https://github.com/estnltk/estnltk/issues/47
-#better would be to just copy the two files we need copied
-#even better would be to fix the setup.py
-#hoping for a fix from upstream
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
