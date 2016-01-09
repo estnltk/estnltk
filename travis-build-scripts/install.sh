94abbc8ebc288f79cc4af0eb61bd2ebc9030ed47
Uku Raudvere <u.raudvere@gmail.com>
2016-01-09 13:31:34 +0200

    typos

--- travis-build-scripts/install.sh
+++ travis-build-scripts/install.sh
@@ -9,15 +9,14 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 bash miniconda.sh -b -p "$HOME/miniconda"
 export PATH="$HOME/miniconda/bin:$PATH"
 
-conda config --set always_yes yes O
+conda config --set always_yes yes
 
 #do not change the bash prompt
 conda config --set changeps1 no
 conda update -q conda
 
 
-  - conda install conda-build anaconda-client
-  o
+conda install conda-build anaconda-client
 #Useful for debugging any issues with conda
 conda info -a
 conda install conda-build anaconda-client
