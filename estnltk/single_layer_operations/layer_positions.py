302f5cb8e881f0d0ad09e23a0b869189c9343bd1
Uku Raudvere <u.raudvere@gmail.com>
2016-04-27 11:40:37 +0300

    Documentation

--- estnltk/single_layer_operations/layer_positions.py
+++ estnltk/single_layer_operations/layer_positions.py
@@ -159,3 +159,58 @@ def iterate_intersecting_pairs(layer):
                     yielded.add(inds)
                     yield elem1, elem2
 
+
+
+
+'''
+#Küsimusi ja vastuseid:
+#K: Miks see moodul eksisteerib?
+#V: Näiteprobleemiks mitmeselt tokeniseeritud tekstide järelpuhastamine. Näiteks kui samasse kihti said tekstist
+#  "12.jaanuar 2014"
+# markerid {"12":täisarv, "2014":täisarv, "12.jaanuar 2014": kuupäev},
+#soovime me neist alles jätta vaid kuupäevatähenduse. Selle mooduliga saab sääraseid konflikte lihtkihtides lahendada.
+
+#K: Miks see moodul just selline on?
+#V: ... see on keeruline küsimus. Nõudmised olid, et
+# 1) Kasutaja saaks kihist kustutada konkreetset objekti (mitte kõiki sama väärtusega objekte)
+# 2) Kasutaja saaks keset iteratsiooni kihti elemente lisada ja kihi elementide järjekorda muuta.
+# Tulemuseks on, et me itereerime üle shallow-copy elementidest väljakutse hetkel ja enne ülekattega paari
+# väljastamist kontrollime, kas väljastatavad elemendid on ikka veel esialgses listis alles.
+# Seega ei garanteeri me, et me leiame ülekatted, mis tekkisid peale iteratsiooni algust. Samas ei garanteeri me,
+# et me EI leia ülekatteid, mis tekkisid peale iteratsiooni algust.
+
+#K: Mida te siis õigupoolest garanteerite?
+#V: funktsioon "iterate_intersecting_pairs" leiab kõik ülekattega paarid, mis eksisteerivad kihis väljakutse hetkel.
+#   funktsioonid "delete_left" ja "delete_right" ei tekita uusi ülekatteid
+#   funktsioon "pop_first_by_identity" ei tekita uusi ülekatteid
+
+#K: Kas sa näitlikustaksid seda moodulit väga sünteetilise näitega?
+#V: See on küsimus ainult sõna formaalses mõttes. Aga miks ka mitte...
+
+import estnltk
+text = estnltk.Text('Kui Arno isaga koolimajja jõudis, olid tunnid juba alanud.')
+text['overlapping'] = [{'start':0, 'end':text.text.find(',')},
+                       {'end':len(text.text), 'start':text.text.find(',') - 5},
+                       {'start':0, 'end':len(text.text)}]
+
+print('At the start we are overlapping ', len(list(iterate_intersecting_pairs(text['overlapping']))))
+
+for a, b in iterate_intersecting_pairs(text['overlapping']):
+    #Tagame, et a ei ole b algusest paremal
+    if a['start'] < b['start']:
+        pass
+    else:
+        a, b = b, a
+
+    #Kui b on a-st ülekattega paremal
+    if overlapping_right(a, b):
+        delete_left(a, b)
+
+    #Kui b on täielikult a sees
+    if nested(a, b):
+        #kustutame kihist kahest elemendist suurema
+        pop_first_by_identity(text['overlapping'], a)
+
+
+print('In the end we are overlapping  ', len(list(iterate_intersecting_pairs(text['overlapping']))))
+'''
