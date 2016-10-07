2ecc3d4a11b93990684de64c39989b0f3d0bdd0d
Siim Orasmaa <siim.orasmaa@ut.ee>
2016-10-07 14:22:37 +0300

    Updated split_large_*_files: fixed the bug that caused crashing while trying to apply split_by() on the (split) texts, and added better parsing of command line arguments.

--- estnltk/examples/split_large_koondkorpus_files.py
+++ estnltk/examples/split_large_koondkorpus_files.py
@@ -21,6 +21,7 @@ import re
 import sys, codecs
 import os, os.path
 import json
+import argparse
 
 from pprint import pprint 
 from timeit import default_timer as timer
@@ -30,9 +31,6 @@ from estnltk.names import *
 from estnltk.corpus import write_document, read_document
 
 max_sentences = 2500
-#max_sentences = 100
-#max_sentences = 2
-
 
 def format_time( sec ):
     ''' Re-formats time duration in seconds (*sec*) into more easily readable 
@@ -88,7 +86,8 @@ def split_Text( text, file_name, verbose = True ):
                if key != TEXT and key != SENTENCES and key != PARAGRAPHS:
                   newText[key] = text[key]
            newText['_text_split_id']     = i
-           newText['_text_split_origin'] = spans[i]
+           newText['_text_split_origin'] = str(spans[i])  #  Convert it to string; Otherwise, split_by(*) may mistakenly consider 
+                                                          #  it a layer and may run into error while trying to split it;
            newText['_text_split_file']   = file_name
            #print( json.dumps(newText) )
            new_texts.append( newText )
@@ -125,70 +124,72 @@ def write_Text_into_file( text, old_file_name, out_dir, suffix='__split', verbos
        print('  ==>  '+new_path+' (file writing time: '+timestamp+')' )
 
 
+arg_parser = argparse.ArgumentParser(description=\
+'''
+  Splits large 'koondkorpus' text files into smaller, more easily processable files. More specifically: 1) Processes a 
+ single file or a directory of files (depending on the command line arguments), 2) Reads a file into an EstNLTK (ver 1.4) 
+ Text, and tokenizes into sentences, 3) If the number of sentences exceeds *max_sentences*, splits the text into smaller 
+ texts (texts with maximum size *max_sentences*), writes smaller texts into separate files, and deletes the old file; 
+ otherwise (if the number of sentences does not exceed *max_sentences*) leaves the file as is it.
+''')
+arg_parser.add_argument('in_file_or_dir', default = None, \
+                                    help='the input file (for processing a single file) or'+\
+                                         ' the directory containing input files'+\
+                                         ' (for processing the directory instead of a '+\
+                                         ' single file);'
+)
+arg_parser.add_argument('out_dir', default = None, \
+                                    help='the output directory where the results of the processing'+\
+                                         ' (split JSON files) will be written.' )
+arg_parser.add_argument('-l', '--limit',  metavar='<max_sentences>', \
+                                          type = int, default = max_sentences, \
+                                          help='the maximum number sentences the JSON file is allowed'+\
+                                               ' to contain; if this number is exceeded, the file will'+\
+                                               ' be split into smaller files. Default: '+str(max_sentences) )
+args = arg_parser.parse_args()
+in_dir  = args.in_file_or_dir if os.path.isdir(args.in_file_or_dir) else None
+in_file = args.in_file_or_dir if os.path.isfile(args.in_file_or_dir) else None
+out_dir = args.out_dir if os.path.isfile(args.out_dir) else None
+max_sentences = args.limit
 
-in_args_found = False
-
-if len(sys.argv) > 1:
-    in_dir  = None
-    out_dir = None
-    in_file = None
-    # *** Collect input arguments ***
-    for i in range(1, len(sys.argv)):
-        arg = sys.argv[i]
-        if os.path.isdir(arg):
-            if not in_dir:
-                in_dir = arg
-            elif not out_dir:
-                out_dir = arg
-        elif arg.lower() in ['--o', '-o', 'o'] and i+1<len(sys.argv):
-            arg2 = sys.argv[i+1]
-            if os.path.isdir(arg2):
-               out_dir = arg2
-               i += 1
-        elif arg.lower() in ['--f', '-f', 'f'] and i+1<len(sys.argv):
-            arg2 = sys.argv[i+1]
-            if os.path.isfile(arg2):
-               in_file = arg2
-               i += 1
-    if out_dir and (in_dir or in_file):
-       start = timer()
-       if in_file:
-          # *** Process a single file ***
-          text  = read_document( in_file )
-          texts = split_Text( text, in_file )
-          if len(texts) > 1:
-             # If the text has been split into smaller texts, write smaller texts into 
-             # new files:
-             for k, small_text in enumerate(texts):
-                 write_Text_into_file( small_text, in_file, out_dir, suffix='__'+("%03d"%k) )
-             # Remove the old file afterwards
-             os.remove( in_file )
-       elif in_dir:
-          # *** Process a directory ***
-          all_files = os.listdir( in_dir )
-          processed_files = 0
-          for in_file_name in all_files:
-              if not in_file_name.endswith('.txt'):
-                  continue
-              in_file = os.path.join( in_dir, in_file_name )
-              text  = read_document( in_file )
-              texts = split_Text( text, in_file )
-              if len(texts) > 1:
-                  # If the text has been split into smaller texts, write smaller texts into 
-                  # new files:
-                  for k, small_text in enumerate(texts):
-                      write_Text_into_file( small_text, in_file, out_dir, suffix='__'+("%03d"%k) )
-                  # Remove the old file afterwards
-                  os.remove( in_file )
-              processed_files += 1
-          print(' > Processed ',processed_files,' files.')
-       end = timer() 
-       print(' Total processing time: '+format_time( end-start ) )
-       in_args_found = True
-    else:
-       in_args_found = False
-
-if not in_args_found:
-   print('Please provide input arguments: \n <in_dir> <out_dir>  or  -f <in_file> -o <out_dir> !')
+if (in_dir or in_file) and out_dir:
+    start = timer()
+    if in_file:
+       # *** Process a single file ***
+       text  = read_document( in_file )
+       texts = split_Text( text, in_file )
+       if len(texts) > 1:
+           # If the text has been split into smaller texts, write smaller texts into 
+           # new files:
+           for k, small_text in enumerate(texts):
+               write_Text_into_file( small_text, in_file, out_dir, suffix='__'+("%03d"%k) )
+           # Remove the old file afterwards
+           os.remove( in_file )
+    elif in_dir:
+       # *** Process a directory ***
+       all_files = os.listdir( in_dir )
+       processed_files = 0
+       split_files     = 0
+       for in_file_name in all_files:
+           if not in_file_name.endswith('.txt'):
+              continue
+           in_file = os.path.join( in_dir, in_file_name )
+           text  = read_document( in_file )
+           texts = split_Text( text, in_file )
+           if len(texts) > 1:
+               # If the text has been split into smaller texts, write smaller texts into 
+               # new files:
+               for k, small_text in enumerate(texts):
+                   write_Text_into_file( small_text, in_file, out_dir, suffix='__'+("%03d"%k) )
+               # Remove the old file afterwards
+               os.remove( in_file )
+               split_files += 1
+           processed_files += 1
+       print(' > Processed ',processed_files,' files.')
+       print(' > Split ',split_files,' files.')
+    end = timer() 
+    print(' Total processing time: '+format_time( end-start ) )
+else:
+    arg_parser.print_help()
 
 
