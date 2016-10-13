531a98a8e70816196bd09d5430d922372105d277
paultammo <paul.tammo@gmail.com>
2016-10-13 09:04:56 +0300

    More verbose test for syntax_preprocessing.

--- estnltk/syntax/test/test_syntax_preprocessing.py
+++ estnltk/syntax/test/test_syntax_preprocessing.py
@@ -9,19 +9,21 @@ subcatFile = '/home/paul/workspace/estnltk/estnltk/syntax/files/abileksikon06utf
 
 pipeline = SyntaxPreprocessing( fs_to_synt=fsToSyntFulesFile, subcat=subcatFile )
 
-test_data = 'test_data_998.json'
+test_data = 'test_data_10.json'
 
 not_ok = []
 with open(test_data, 'r') as f:
     for i, line in enumerate(f):
-        t, expected = json.loads(line)
+        print(i, end=' ', flush=True)
+        file, t, expected = json.loads(line)
+        print(file, end=' ' * (40 - len(file)), flush=True)
         t = words_sentences(t)
         result = pipeline.process_Text(t)
         if result == expected:
-            print(i, 'OK')
+            print('OK')
         else:
             not_ok.append(i)
-            print(i, 'Not OK. First mismatching line:')
+            print('Not OK. First mismatching line:')
             for r, e in zip(result, expected):
                 if r != e:
                     print(r, e)
