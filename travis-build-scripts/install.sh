3d6dbeee1f5293f0cd0f5f542d5db1d196da7492
Uku Raudvere <u.raudvere@gmail.com>
2016-10-18 20:13:13 +0300

    tests running?

--- travis-build-scripts/install.sh
+++ travis-build-scripts/install.sh
@@ -2,7 +2,18 @@
 
 #In the install step we install the dependencies for building estnltk.
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+
+if [ $TRAVIS_OS_NAME = "linux" ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
+if [ $TRAVIS_OS_NAME = "osx" ]; then
+
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+fi
+
+
+
 bash miniconda.sh -b -p "$HOME/miniconda"
 
 
