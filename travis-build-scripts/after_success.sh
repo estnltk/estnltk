a8dff6e8294f5ba1a088e0f5acb233b3ef4d6867
Uku Raudvere <u.raudvere@gmail.com>
2016-10-18 19:44:00 +0300

    tests running?

--- travis-build-scripts/after_success.sh
+++ travis-build-scripts/after_success.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 #run tests here
-x=`cat $HOME/outfile.txt`
-conda install -y -c conda-forge -c estnltk $x
+filename=`cat $HOME/outfile.txt`
+
+
+echo "Starting install from $filename"
+conda install -y -c conda-forge -c estnltk $filename
+echo "install done"
+
+
+
+echo "running tests"
 python -m estnltk.run_tests
+echo "tests ran"
