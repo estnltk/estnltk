b4be2372e234c3b0f76338f84c232df353ea50e7
Uku Raudvere <u.raudvere@gmail.com>
2016-10-18 17:15:22 +0300

    let's try to get travis online again

--- travis-build-scripts/install.sh
+++ travis-build-scripts/install.sh
@@ -2,12 +2,12 @@
 
 #In the install step we install the dependencies for building estnltk.
 
-sudo apt-get update
-sudo apt-get -y remove swig
+apt-get update
+apt-get -y remove swig
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p "$HOME/miniconda"
-export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$HOME/miniconda/bin:usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/home/travis/bin:/home/travis/.local/bin"
 
 conda config --set always_yes yes
 
@@ -15,8 +15,6 @@ conda config --set always_yes yes
 conda config --set changeps1 no
 conda update -q conda
 
-
-conda install conda-build anaconda-client
 #Useful for debugging any issues with conda
 conda info -a
 conda install conda-build anaconda-client
