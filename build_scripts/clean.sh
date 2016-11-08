b192d0746281c7d177513c252d66407394a2236d
Uku Raudvere <u.raudvere@gmail.com>
2016-11-08 20:32:00 +0200

    updated cleanupfiles

--- build_scripts/clean.sh
+++ build_scripts/clean.sh
@@ -1,3 +1,4 @@
-rm -r ./build
-rm estnltk/vabamorf/vabamorf.py
-rm estnltk/vabamorf/vabamorf_wrap.cpp
+#!/usr/bin/env bash
+rm -r ../build
+rm ../estnltk/vabamorf/vabamorf.py
+rm ../estnltk/vabamorf/vabamorf_wrap.cpp
