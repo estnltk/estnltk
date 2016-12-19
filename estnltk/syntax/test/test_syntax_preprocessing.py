a97947272fa731a631e860205256736fed177941
paultammo <paul.tammo@gmail.com>
2016-12-19 17:58:04 +0200

    updated syntax_preprocessing test

--- estnltk/syntax/test/test_syntax_preprocessing.py
+++ estnltk/syntax/test/test_syntax_preprocessing.py
@@ -4,13 +4,27 @@ from estnltk.syntax.syntax_preprocessing import SyntaxPreprocessing
 
 def test_pipeline():
 
-    fsToSyntFulesFile = '../files/tmorftrtabel.txt'
-    subcatFile = '../files/abileksikon06utf.lx'
+    fsToSyntFulesFile = '../../rewriting/syntax_preprocessing/rules_files/tmorftrtabel.txt'
+    subcatFile = '../../rewriting/syntax_preprocessing/rules_files/abileksikon06utf.lx'
+    subcat_extra_file = '../../rewriting/syntax_preprocessing/rules_files/abileksikon_extra.lx'
 
-    pipeline = SyntaxPreprocessing(fs_to_synt=fsToSyntFulesFile, subcat=subcatFile)
+    pipeline = SyntaxPreprocessing(fs_to_synt=fsToSyntFulesFile, 
+                                   subcat=subcatFile, 
+                                   subcat_extra = subcat_extra_file)
 
     test_data = 'test_data_10.json'
+    # this file contains 10 texts from koondkorpus and the output of previous 
+    # version of syntax_preprocessing
+    with open(test_data, 'r') as f:
+        for line in f:
+            file, t, expected = json.loads(line)
+            t = words_sentences(t)
+            _, result = pipeline.process_Text(t)
+            assert result == expected
 
+    test_data = 'test_data_coverage_85.json'
+    # processing the text in this file covers all relevant lines of
+    # previous version of syntax_preprocessing
     with open(test_data, 'r') as f:
         for line in f:
             file, t, expected = json.loads(line)
