82d71eaa01fc9d4903cd84904b398c7486184646
paultammo <paul.tammo@gmail.com>
2017-01-01 17:59:16 +0200

    test for checking abileksikon06utf.lx format

---
+++ estnltk/rewriting/tests/test_abileksikon.py
@@ -0,0 +1,37 @@
+import codecs
+from collections import defaultdict
+import regex
+
+rules = defaultdict(list)
+nonSpacePattern = regex.compile('^\S+$')
+posTagPattern   = regex.compile('_._')
+file = '../syntax_preprocessing/rules_files/abileksikon06utf.lx'
+
+roots = set()
+in_f = codecs.open(file, mode='r', encoding='utf-8')
+root = None
+subcatRules = None
+line_number = 0
+while True:
+    line = next(in_f, None)
+    line_number += 1
+    if line == None:
+        break
+    assert nonSpacePattern.match(line), 'whitespace in root: "' + line +'"'
+    line = line.rstrip()
+    assert line not in roots, 'repetitive root: "' + line + '"'
+    roots.add(line)
+
+    line = next(in_f, None)
+    line_number += 1
+    assert line != None, 'odd number of lines in file'
+    premises = set()
+    for l in line.split('&'):
+        l = l.strip()
+        assert posTagPattern.match(l), 'incorrect pos format on line number {}: "{}"'.format(line_number, line.rstrip())
+        premise, tags = l.split('>')
+        premise = premise.strip()
+        assert premise not in premises, 'repetitive premise on line number {}: "{}"'.format(line_number, line.rstrip())
+        premises.add(premise)
+        if premise not in {'_Y_', '_Z_'}:
+            assert all(tag.strip('|').startswith('#') for tag in tags.split()), 'missing # on line number {}: "{}"'.format(line_number, line.rstrip())
