a03c615471b03185af0ba5afc3e3e2deda78956b
paultammo <paul.tammo@gmail.com>
2017-02-13 17:13:02 +0200

    update test

--- estnltk/syntax/test/test_syntax_preprocessing.py
+++ estnltk/syntax/test/test_syntax_preprocessing.py
@@ -2,24 +2,22 @@ import json
 from estnltk.text import words_sentences
 from estnltk.syntax.syntax_preprocessing import SyntaxPreprocessing
 
-def test_pipeline():
+def test_syntax_preprocessing():
 
     fsToSyntFulesFile = '../../rewriting/syntax_preprocessing/rules_files/tmorftrtabel.txt'
     subcatFile = '../../rewriting/syntax_preprocessing/rules_files/abileksikon06utf.lx'
-    subcat_extra_file = '../../rewriting/syntax_preprocessing/rules_files/abileksikon_extra.lx'
 
-    pipeline = SyntaxPreprocessing(fs_to_synt=fsToSyntFulesFile, 
-                                   subcat=subcatFile, 
-                                   subcat_extra = subcat_extra_file)
+    syntax_preprocessing = SyntaxPreprocessing(fs_to_synt=fsToSyntFulesFile, 
+                                   subcat=subcatFile)
 
     test_data = 'test_data_10.json'
     # this file contains 10 texts from koondkorpus and the output of previous 
     # version of syntax_preprocessing
     with open(test_data, 'r') as f:
         for line in f:
-            file, t, expected = json.loads(line)
+            _, t, expected = json.loads(line)
             t = words_sentences(t)
-            _, result = pipeline.process_Text(t)
+            result = syntax_preprocessing.process_Text(t)
             assert result == expected
 
     test_data = 'test_data_coverage_85.json'
@@ -27,7 +25,7 @@ def test_pipeline():
     # previous version of syntax_preprocessing
     with open(test_data, 'r') as f:
         for line in f:
-            file, t, expected = json.loads(line)
+            _, t, expected = json.loads(line)
             t = words_sentences(t)
-            _, result = pipeline.process_Text(t)
+            result = syntax_preprocessing.process_Text(t)
             assert result == expected
