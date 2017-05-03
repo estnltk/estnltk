1998e01f723c5df780d8cee1ab7e6e674ed962e7
paultammo <paul.tammo@gmail.com>
2017-05-03 12:20:03 +0300

    ignore list for WordNormalizingRewriter

--- estnltk/rewriting/premorph/word_normalizing.py
+++ estnltk/rewriting/premorph/word_normalizing.py
@@ -1,7 +1,36 @@
+from pandas import read_csv
+from pandas.io.common import EmptyDataError
+from os.path import dirname, join
+
 from estnltk.rewriting.helpers.morph_analyzed_token import MorphAnalyzedToken
 
 
 class WordNormalizingRewriter:
+    def __init__(self):
+        file = join(dirname(__file__), 'rules_files/ignore.csv')
+        self.ignore = self.load_ignore(file)
+        self.hyphen_removing_rewriter = HyphenRemovingRewriter()
+
+
+    @staticmethod
+    def load_ignore(file):
+        try:
+            df = read_csv(file, na_filter=False, header=None)
+            return set(df[0])
+        except EmptyDataError:
+            return set()
+
+    def rewrite(self, record):
+        if record['text'] in self.ignore:
+            return None
+
+        record = self.hyphen_removing_rewriter.rewrite(record)
+
+        return record
+
+
+class HyphenRemovingRewriter:
+
     def rewrite(self, record):
         token = MorphAnalyzedToken(record['text'])
         if token is token.normal:
