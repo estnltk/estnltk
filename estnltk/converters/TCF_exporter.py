2afe28efc99e32cd9f13c344b137fa0bd6215f2e
paultammo <paul.tammo@gmail.com>
2017-07-16 22:09:51 +0300

    updated TCF converters

--- estnltk/converters/TCF_exporter.py
+++ estnltk/converters/TCF_exporter.py
@@ -13,8 +13,6 @@ def export_TCF(t: Text, file:str=None):
     If file is not None, saves the result in file.
     Returns str
     '''
-
-    token_ids = {word:'t'+str(i) for i, word in enumerate(t.words)}
     
     text_tree = etree.Element('D-Spin', xmlns="http://www.dspin.de/data", version="0.4")
     doc = etree.ElementTree(text_tree)
@@ -27,50 +25,80 @@ def export_TCF(t: Text, file:str=None):
     
     
     E = ElementMaker(namespace='http://www.dspin.de/data/textcorpus',
-                   nsmap={'tc':'http://www.dspin.de/data/textcorpus'})
+                     nsmap={'tc':'http://www.dspin.de/data/textcorpus'})
+
+    if 'words' in t.layers:
+        token_ids = {word:'t'+str(i) for i, word in enumerate(t.words)}
+        tokens = E('tokens')
+        for word in t.words:
+            tokens.append(E('token', word.text,  {'ID':token_ids[word], 'start':str(word.start), 'end':str(word.end)}))
+        text_tree[1].append(tokens)
     
-    tokens = E('tokens')
-    for word in t.words:
-        tokens.append(E('token', word.text,  {'ID':token_ids[word], 'start':str(word.start), 'end':str(word.end)}))
-    text_tree[1].append(tokens)
-    
-    sentences = E('sentences')
-    for i, sentence in enumerate(t.sentences):
-        token_IDs = ' '.join((token_ids[word] for word in sentence))
-        sentences.append(E('sentence',  {'ID':'s'+str(i), 'tokenIDs':token_IDs}))
-    text_tree[1].append(sentences)
-    
-    lemmas = E('lemmas')
-    for analysis in t.morf_analysis:
-        token_id = token_ids[analysis.words]
-        for a in analysis:
-            # kas nii on õige toimida mitmese märgendiga? (' '.join...)
-            lemmas.append(E('lemma', a.lemma, {'ID':token_id.replace('t', 'l'),'tokenIDs':token_id}))
-    text_tree[1].append(lemmas)
-    
-    postags = E('POStags', {'tagset':''})
-    for analysis in t.morf_analysis:
-        token_id = token_ids[analysis.words]
-        for a in analysis:
-            # kas nii on õige toimida mitmese märgendiga? (' '.join...)
-            postags.append(E('tag', a.partofspeech, {'tokenIDs':token_id}))
-    text_tree[1].append(postags)
-    
-    morphology = E('morphology')
-    for analysis in t.morf_analysis:
-        token_id = token_ids[analysis.words]
-        for a in analysis:
-            # kas nii on õige toimida mitmese märgendiga?
-            features = E('fs')
-            features.append(E('f', a.form, {'name':'form'}))
-            features.append(E('f', a.root, {'name':'root'}))
-            features.append(E('f', ' '.join(a.root_tokens), {'name':'root_tokens'}))
-            features.append(E('f', a.ending, {'name':'ending'}))
-            features.append(E('f', a.clitic, {'name':'clitic'}))
-            tag = E('tag', features)
-            morphology.append(E('analysis', tag, {'tokenIDs':token_id}))
-    text_tree[1].append(morphology)
-    
+    if 'sentences' in t.layers:
+        sentences = E('sentences')
+        for i, sentence in enumerate(t.sentences):
+            token_IDs = ' '.join((token_ids[word] for word in sentence))
+            sentences.append(E('sentence',  {'ID':'s'+str(i), 'tokenIDs':token_IDs}))
+        text_tree[1].append(sentences)
+
+    if 'morf_analysis' in t.layers:
+        lemmas = E('lemmas')
+        for analysis in t.morf_analysis:
+            token_id = token_ids[analysis.words]
+            for a in analysis:
+                # kas nii on õige toimida mitmese märgendiga? (' '.join...)
+                lemmas.append(E('lemma', a.lemma, {'ID':token_id.replace('t', 'l'),'tokenIDs':token_id}))
+        text_tree[1].append(lemmas)
+        
+        postags = E('POStags', {'tagset':''})
+        for analysis in t.morf_analysis:
+            token_id = token_ids[analysis.words]
+            for a in analysis:
+                # kas nii on õige toimida mitmese märgendiga? (' '.join...)
+                postags.append(E('tag', a.partofspeech, {'tokenIDs':token_id}))
+        text_tree[1].append(postags)
+        
+        morphology = E('morphology')
+        for analysis in t.morf_analysis:
+            token_id = token_ids[analysis.words]
+            for a in analysis:
+                # kas nii on õige toimida mitmese märgendiga?
+                features = E('fs')
+                features.append(E('f', a.form, {'name':'form'}))
+                features.append(E('f', a.root, {'name':'root'}))
+                features.append(E('f', ' '.join(a.root_tokens), {'name':'root_tokens'}))
+                features.append(E('f', a.ending, {'name':'ending'}))
+                features.append(E('f', a.clitic, {'name':'clitic'}))
+                tag = E('tag', features)
+                morphology.append(E('analysis', tag, {'tokenIDs':token_id}))
+        text_tree[1].append(morphology)
+
+    if 'clauses' in t.layers:
+        element_maker = E('clauses')
+        for i, clause in enumerate(t.clauses):
+            token_IDs = ' '.join((token_ids[word] for word in clause))
+            element_maker.append(E('clause',  {'tokenIDs':token_IDs}))
+        text_tree[1].append(element_maker)
+
+    element_maker = E('chunks',{'tagset':''})
+    chunk = False
+    count = 0
+    if 'verb_chains' in t.layers:
+        chunk = True
+        for phrase in t.verb_chains:
+            token_IDs = ' '.join((token_ids[token] for token in phrase))
+            element_maker.append(E('chunk',  {'ID':'ch'+str(count), 'type':'VP', 'tokenIDs':token_IDs}))
+            count += 1
+    if 'time_phrases' in t.layers:
+        chunk = True
+        for phrase in t.time_phrases:
+            token_IDs = ' '.join((token_ids[token] for token in phrase))
+            element_maker.append(E('chunk',  {'ID':'ch'+str(count), 'type':'TMP', 'tokenIDs':token_IDs}))
+            count += 1
+    if chunk:
+        text_tree[1].append(element_maker)
+
+
     if file is not None:
         doc.write(file, xml_declaration=True, encoding='UTF-8', pretty_print=True)
     return etree.tostring(text_tree, encoding='unicode', pretty_print=True)
--- estnltk/converters/TCF_importer.py
+++ estnltk/converters/TCF_importer.py
@@ -1,31 +1,81 @@
 from lxml import etree
 from estnltk import Text
-from estnltk.text import Span, Layer
+from estnltk.text import Span, SpanList, Layer
 
-def import_TCF(file):
-    text_tree = etree.parse('out.xml').getroot()
+def import_TCF(string:str=None, file:str=None):
+    if file:
+        text_tree = etree.parse(file).getroot()
+    else:
+        text_tree = etree.fromstring(string)
+
     text_corpus = text_tree.find('{http://www.dspin.de/data/textcorpus}TextCorpus')
 
     element = text_corpus.find('{http://www.dspin.de/data/textcorpus}text')
-    if element is not None:
-        t = element.text
+    assert element is not None, "no text tag in the file '"+file+"'"
+    t = element.text
+    if t is None:
+        t = ''
+    text = Text(t)
 
+    # words layer
     element = text_corpus.find('{http://www.dspin.de/data/textcorpus}tokens')
     if element is not None:
-        tokens = {}
-        token_rec = []
+        id_to_token = {}
+        layer = Layer(name='words')
         for token in element:
-            token_rec.append({'start':int(token.get('start')), 'end':int(token.get('end')), 'ID': token.get('ID')})
-            tokens[token.get('ID')] = token.text
+            token_span = Span(start=int(token.get('start')), end=int(token.get('end')), legal_attributes=())
+            layer.add_span(token_span)
+            id_to_token[token.get('ID')] = token_span
+        text['words'] = layer
 
+    # sentences layer
     element = text_corpus.find('{http://www.dspin.de/data/textcorpus}sentences')
     if element is not None:
-        #print('sentences:')
+        layer = Layer(enveloping='words',
+                      name='sentences')
         for sentence in element:
-            sentence.get('tokenIDs')
-            s = ' '.join((tokens[token_id] for token_id in sentence.get('tokenIDs').split()))
-        #    print('\t', s)
+            span_list = SpanList()
+            for token_id in sentence.get('tokenIDs').split():
+                span_list.add_span(id_to_token[token_id])
+            layer.add_span(span_list)
+        text['sentences'] = layer
 
+    # clauses layer
+    element = text_corpus.find('{http://www.dspin.de/data/textcorpus}clauses')
+    if element is not None:
+        layer = Layer(enveloping='words',
+                      name='clauses')
+        for clause in element:
+            span_list = SpanList()
+            for token_id in clause.get('tokenIDs').split():
+                span_list.add_span(id_to_token[token_id])
+            layer.add_span(span_list)
+        text['clauses'] = layer
+
+    # chunk layers: verb_chains, time_phrases
+    element = text_corpus.find('{http://www.dspin.de/data/textcorpus}chunks')
+    if element is not None:
+        layer_vp = Layer(enveloping='words',
+                         name='verb_chains')
+        layer_tmp = Layer(enveloping='words',
+                          name='time_phrases')
+        for line in element:
+            chunk_type = line.get('type')
+            if chunk_type=='VP':
+                span_list = SpanList()
+                for token_id in line.get('tokenIDs').split():
+                    span_list.add_span(id_to_token[token_id])
+                layer_vp.add_span(span_list)
+            elif chunk_type=='TMP':
+                span_list = SpanList()
+                for token_id in line.get('tokenIDs').split():
+                    span_list.add_span(id_to_token[token_id])
+                layer_tmp.add_span(span_list)
+        text['verb_chains'] = layer_vp
+        text['time_phrases'] = layer_tmp
+
+
+    # morf_analysis layer
     morph_analysis_list = []
     element = text_corpus.find('{http://www.dspin.de/data/textcorpus}lemmas')
     if element is not None:
@@ -61,31 +111,28 @@ def import_TCF(file):
             rec['ending'] = ending if ending else ''
             rec['clitic'] = clitic if clitic else ''
 
-    morph_analysis_records = [[]]
-    token_id = morph_analysis_list[0]['tokenID']
-    for rec in morph_analysis_list:
-        if rec['tokenID'] != token_id:
-            morph_analysis_records.append([rec])
-            token_id = rec['tokenID']
-        else:
-            morph_analysis_records[-1].append(rec)
+        morph_analysis_records = [[]]
+        token_id = morph_analysis_list[0]['tokenID']
+        for rec in morph_analysis_list:
+            if rec['tokenID'] != token_id:
+                morph_analysis_records.append([rec])
+                token_id = rec['tokenID']
+            else:
+                morph_analysis_records[-1].append(rec)
+    
+        morph_attributes = ['lemma', 'root', 'root_tokens', 'ending',
+                            'clitic', 'form', 'partofspeech']
+        morph = Layer(name='morf_analysis',
+                      parent='words',
+                      ambiguous=True,
+                      attributes=morph_attributes
+                      )
+        for word, analyses in zip(text.words, morph_analysis_records):
+            for analysis in analyses:
+                span = morph.add_span(Span(parent=word))
+                for attr in morph_attributes:
+                    setattr(span, attr, analysis[attr])
+    
+        text['morf_analysis'] = morph
 
-    text = Text(t)
-
-    text['words'] = Layer(name='words').from_records(token_rec, rewriting=True)
-
-    morph_attributes = ['lemma', 'root', 'root_tokens', 'ending',
-                        'clitic', 'form', 'partofspeech']
-    morph = Layer(name='morf_analysis',
-                  parent='words',
-                  ambiguous=True,
-                  attributes=morph_attributes
-                  )
-    for word, analyses in zip(text.words, morph_analysis_records):
-        for analysis in analyses:
-            span = morph.add_span(Span(parent=word))
-            for attr in morph_attributes:
-                setattr(span, attr, analysis[attr])
-
-    text['morf_analysis'] = morph
     return text
