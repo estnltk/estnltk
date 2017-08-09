9b037a7b569c282ce7b0b03cc44010ea99ae7b34
paultammo <paul.tammo@gmail.com>
2017-08-09 18:52:15 +0300

    version parameter for TCF exporter

--- estnltk/converters/TCF_exporter.py
+++ estnltk/converters/TCF_exporter.py
@@ -3,18 +3,28 @@ from lxml.builder import ElementMaker
 from estnltk import Text
 
 
-def export_TCF(t: Text, file:str=None):
+def export_TCF(t: Text, file:str=None, version='0.4'):
     '''
     Export Text to TCF XML format for
     https://weblicht.sfs.uni-tuebingen.de/weblicht/
     
     As a side-effect equips Text object with token layer.
 
-    If file is not None, saves the result in file.
-    Returns str
+    file: str
+        If not None, saves the result in file.
+    
+    version: str
+        '0.4' produces xml that works with current TCF version 'v0.4'
+        '0.5' produces xml for future TCF version. This includes layers for 
+            clauses, verb_chains and time_phrases.  
+
+    Returns: str
+        TCF xml string.
     '''
     
-    text_tree = etree.Element('D-Spin', xmlns="http://www.dspin.de/data", version="0.4")
+    assert version in {'0.4', '0.5'}, 'Unknown version: ' + str(version)
+    
+    text_tree = etree.Element('D-Spin', xmlns="http://www.dspin.de/data", version=version)
     doc = etree.ElementTree(text_tree)
     
     etree.SubElement(text_tree, 'MetaData', xmlns="http://www.dspin.de/data/metadata")
@@ -27,21 +37,21 @@ def export_TCF(t: Text, file:str=None):
     E = ElementMaker(namespace='http://www.dspin.de/data/textcorpus',
                      nsmap={'tc':'http://www.dspin.de/data/textcorpus'})
 
-    if 'words' in t.layers:
+    if 'words' in t.layers and version in {'0.4', '0.5'}:
         token_ids = {word:'t'+str(i) for i, word in enumerate(t.words)}
         tokens = E('tokens')
         for word in t.words:
             tokens.append(E('token', word.text,  {'ID':token_ids[word], 'start':str(word.start), 'end':str(word.end)}))
         text_tree[1].append(tokens)
     
-    if 'sentences' in t.layers:
+    if 'sentences' in t.layers and version in {'0.4', '0.5'}:
         sentences = E('sentences')
         for i, sentence in enumerate(t.sentences):
             token_IDs = ' '.join((token_ids[word] for word in sentence))
             sentences.append(E('sentence',  {'ID':'s'+str(i), 'tokenIDs':token_IDs}))
         text_tree[1].append(sentences)
 
-    if 'morf_analysis' in t.layers:
+    if 'morf_analysis' in t.layers and version in {'0.4', '0.5'}:
         lemmas = E('lemmas')
         for analysis in t.morf_analysis:
             token_id = token_ids[analysis.words]
@@ -73,7 +83,7 @@ def export_TCF(t: Text, file:str=None):
                 morphology.append(E('analysis', tag, {'tokenIDs':token_id}))
         text_tree[1].append(morphology)
 
-    if 'clauses' in t.layers:
+    if 'clauses' in t.layers and version in {'0.5'}:
         element_maker = E('clauses')
         for i, clause in enumerate(t.clauses):
             token_IDs = ' '.join((token_ids[word] for word in clause))
@@ -83,13 +93,13 @@ def export_TCF(t: Text, file:str=None):
     element_maker = E('chunks',{'tagset':''})
     chunk = False
     count = 0
-    if 'verb_chains' in t.layers:
+    if 'verb_chains' in t.layers and version in {'0.5'}:
         chunk = True
         for phrase in t.verb_chains:
             token_IDs = ' '.join((token_ids[token] for token in phrase))
             element_maker.append(E('chunk',  {'ID':'ch'+str(count), 'type':'VP', 'tokenIDs':token_IDs}))
             count += 1
-    if 'time_phrases' in t.layers:
+    if 'time_phrases' in t.layers and version in {'0.5'}:
         chunk = True
         for phrase in t.time_phrases:
             token_IDs = ' '.join((token_ids[token] for token in phrase))
