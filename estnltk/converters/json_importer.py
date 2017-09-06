1425f458156a494e86b96168873e18e56a3f0877
paultammo <paul.tammo@gmail.com>
2017-09-06 10:58:57 +0300

    update json importer

--- estnltk/converters/json_importer.py
+++ estnltk/converters/json_importer.py
@@ -7,12 +7,12 @@ def _convert_list_to_tuple(d):
             d[key] = tuple(value)
     return d
 
-def import_json(text_json:str=None, file:str=None) -> 'Text':
+def import_json(json_text:str=None, file:str=None) -> 'Text':
     if file:
         with open(file, 'r') as in_f:
             text_dict = json.load(fp=in_f, object_hook=_convert_list_to_tuple)
-    elif text_json:
-        text_dict = json.loads(text_json, object_hook=_convert_list_to_tuple)
+    elif json_text:
+        text_dict = json.loads(json_text, object_hook=_convert_list_to_tuple)
     else:
         raise TypeError("either 'text_json' or 'file' argument needed")
     return import_dict(text_dict)
