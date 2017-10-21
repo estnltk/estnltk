4680dbf0e6ec3dc5d6f3f4ad5343e3d669e9f6e5
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-10-21 16:40:41 +0300

    Fix on SentenceTokenizer + update in tutorial

--- estnltk/taggers/text_segmentation/sentence_tokenizer.py
+++ estnltk/taggers/text_segmentation/sentence_tokenizer.py
@@ -35,7 +35,7 @@ merge_patterns = [ \
    { 'comment'  : '{Numeric_year} {period} {|a|} + {lowercase_or_number}', \
      'example'  : '"Luunja sai vallaõigused 1991.a." + " kevadel."', \
      'fix_type' : 'numeric_year', \
-     'regexes'  : [re.compile('(.+)?([0-9]{3,4})\s*\.\s*a\s*\.?$', re.DOTALL), re.compile('^('+lc_letter+'|[0-9])+')], \
+     'regexes'  : [re.compile('(.+)?([0-9]{3,4})\s*\.?\s*a\s*\.$', re.DOTALL), re.compile('^('+lc_letter+'|[0-9])+')], \
    },
    { 'comment'  : '{Numeric_year} {period} {|a|} + {lowercase_or_number}', \
      'example'  : '"1946/47 õ.a." + "oli koolis 87 õpilast."', \
@@ -54,18 +54,11 @@ merge_patterns = [ \
      'fix_type' : 'numeric_year', \
      'regexes'  : [re.compile('(.+)?([0-9]{3,4})\s*\.$', re.DOTALL), re.compile('^'+lc_letter+'*aasta.*') ], \
    },
-
    #   {Numeric|Roman_numeral_century} {period} {|sajand|} + {lowercase}
    { 'comment'  : '{Numeric|Roman_numeral_century} {period} {|sajand|} + {lowercase}', \
      'example'  : '"... Mileetose koolkonnd (VI-V saj." + "e. Kr.) ..."', \
      'fix_type' : 'numeric_century', \
      'regexes'  : [re.compile('(.+)?([0-9]{1,2}|[IVXLCDM]+)\s*\.?\s*saj\.?$', re.DOTALL), re.compile('^'+lc_letter+'+') ], \
-   },
-   #   {BCE} {period} + {lowercase}
-   { 'comment'  : '{BCE} {period} + {lowercase}', \
-     'example'  : '"Suur rahvasterändamine oli avanud IV-nda sajandiga p. Kr." + "segaduste ja sõdade ajastu."', \
-     'fix_type' : 'numeric_century', \
-     'regexes'  : [re.compile('(.+)?[pe]\s*\.\s*Kr\s*\.?$', re.DOTALL), re.compile('^'+lc_letter+'+') ], \
    },
    
    #   {Date_dd.mm.yyyy.} + {time_HH:MM}
@@ -93,6 +86,12 @@ merge_patterns = [ \
      'fix_type' : 'numeric_date', \
      'regexes'  : [re.compile('(.+)?([0-9]{1,2})\s*\.$', re.DOTALL), re.compile('^(jaan|veebr?|mär|apr|mai|juul|juun|aug|sept|okt|nov|dets)(\s*\.|\s).*') ], \
    },
+   #  {Month_name_short} {period} + {numeric_year}
+   { 'comment'  : '{Numeric_date} {period} + {month_name_short}', \
+     'example'  : '"17. okt." + "1998 a."', \
+     'fix_type' : 'numeric_date', \
+     'regexes'  : [re.compile('^(.+)?\s(jaan|veebr?|mär|apr|mai|juul|juun|aug|sept|okt|nov|dets)\s*\..*', re.DOTALL), re.compile('([0-9]{4})\s*.*') ], \
+   },
 
    #   {First_10_Roman_numerals} {period} + {lowercase_or_dash}
    { 'comment'  : '{First_10_Roman_numerals} {period} + {lowercase_or_dash}', \
@@ -117,6 +116,12 @@ merge_patterns = [ \
    # ***********************************
    #   Fixes related to specific abbreviations
    # ***********************************
+   #   {BCE} {period} + {lowercase}
+   { 'comment'  : '{BCE} {period} + {lowercase}', \
+     'example'  : '"Suur rahvasterändamine oli avanud IV-nda sajandiga p. Kr." + "segaduste ja sõdade ajastu."', \
+     'fix_type' : 'abbrev_century', \
+     'regexes'  : [re.compile('(.+)?[pe]\s*\.\s*Kr\s*\.?$', re.DOTALL), re.compile('^'+lc_letter+'+') ], \
+   },
    { 'comment'  : '{Abbreviation} {period} + {numeric}', \
      'example'  : '"Hõimurahvaste Aeg Nr." + "7 lk." + "22."', \
      'fix_type' : 'abbrev_numeric', \
