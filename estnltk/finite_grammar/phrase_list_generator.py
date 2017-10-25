0c8ff49fd7e06184279162d90939e1f9cf882268
estnltk <paul.tammo@gmail.com>
2017-10-25 17:14:04 +0300

    depth attribute for phrase_list_generator

--- estnltk/finite_grammar/phrase_list_generator.py
+++ estnltk/finite_grammar/phrase_list_generator.py
@@ -3,18 +3,16 @@ from collections import defaultdict
 from .trees import Grammar
 
 
-def phrase_list_generator(grammar:Grammar):
-    '''
-    :param grammar: Grammar
-    :return: iterator of phrases
-    Generates all phrases of the finite grammar.
-    '''
+def phrase_list_generator(grammar: Grammar, depth: int=float('inf')):
+    """
+    Generates all phrases in the finite grammar tree up to the given depth.
+    """
     ruledict = defaultdict(list)
     for rule in grammar.rules:
-        ruledict[rule['lhs']].append( list(rule['rhs']))
+        ruledict[rule['lhs']].append(list(rule['rhs']))
     nonterminals = grammar.nonterminals
 
-    def gen(symbols):
+    def gen(symbols, depth):
         nonterminal = None
         for i, s in enumerate(symbols):
             if s in nonterminals:
@@ -23,8 +21,10 @@ def phrase_list_generator(grammar:Grammar):
         if nonterminal is None:
             yield symbols
             return
+        if depth <= 0:
+            return
         for replacement in ruledict[nonterminal]:
             new_symbols = symbols[:i] + replacement + symbols[i+1:]
-            yield from gen(new_symbols)
+            yield from gen(new_symbols, depth-1)
 
-    return gen([grammar.start_symbol])
+    return gen([grammar.start_symbol], depth)
