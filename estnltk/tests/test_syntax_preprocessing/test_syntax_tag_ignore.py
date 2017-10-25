ee046f5657ed2086c5a4517eaa328fd4b6a26ddb
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-10-25 16:16:45 +0300

    Updated SyntaxIgnoreTagger's tests

--- estnltk/tests/test_syntax_preprocessing/test_syntax_tag_ignore.py
+++ estnltk/tests/test_syntax_preprocessing/test_syntax_tag_ignore.py
@@ -7,6 +7,7 @@ from estnltk.taggers.syntax_preprocessing.syntax_ignore_tagger import SyntaxIgno
 def test_ignore_content_in_parenthesis():
     syntax_ignore_tagger = SyntaxIgnoreTagger()
     test_texts = [ 
+        # Pattern: parenthesis_1to3
         { 'text': 'Eesti judokate võistlus jäi laupäeval lühikeseks , nii Joel Rothberg ( -66 kg ) kui ka Renee Villemson ( -73 kg ) võidurõõmu maitsta ei saanud .', \
           'expected_ignore_texts': ['( -66 kg )', '( -73 kg )'] }, \
         { 'text': 'Vladimir Grill ( M 66 kg ) , Joel Rothberg ( M 73 kg ) , Rasmus Toompere ( M 81 kg ) ja Sander Maripuu ( M 90 kg ).', \
@@ -15,6 +16,17 @@ def test_ignore_content_in_parenthesis():
           'expected_ignore_texts': ['( 0 : 1 , 1 : 1 , 2 : 5 )'] }, \
         { 'text': 'Meeste meistriliiga eilsed tulemused : Rivaal - Pärnu 1 : 3 ( 21 , -14 , -21 , -12 ) , Audentes - Volle 3 : 1 ( 15 , -25 , 17 , 22 ) .', \
           'expected_ignore_texts': ['( 21 , -14 , -21 , -12 )', '( 15 , -25 , 17 , 22 )'] }, \
+        { 'text': 'Suursoosikutele järgnevad AC Milan ( 7 : 1 ) , Manchester United ( 8 : 1 ) , Londoni Arsenal ja Müncheni Bayern ( 9 : 1 ) ning Torino Juventus ( 10 : 1 ) .', \
+          'expected_ignore_texts': ['( 7 : 1 )', '( 8 : 1 )', '( 9 : 1 )', '( 10 : 1 )'] }, \
+        # Pattern: parenthesis_title_words
+        { 'text': 'Neidude 5 km klassikat võitis Lina Andersson ( Rootsi ) Pirjo Mannineni ( Soome ) ja Karin Holmbergi ( Rootsi ) ees .', \
+          'expected_ignore_texts': ['( Rootsi )', '( Soome )', '( Rootsi )'] }, \
+        # Pattern: parenthesis_ordinal_numbers
+        { 'text': 'Naiste turniirid toimuvad Jakartas ja Budapestis ( 26. aprillini ) .', \
+          'expected_ignore_texts': ['( 26. aprillini )'] }, \
+        { 'text': '1930ndate algul avaldati romaani uus väljaanne ( 4. trükk ) KT XVI ande teise trüki kujul .', \
+          'expected_ignore_texts': ['( 4. trükk )'] }, \
+
     ]
     for test_text in test_texts:
         text = Text( test_text['text'] )
@@ -24,7 +36,7 @@ def test_ignore_content_in_parenthesis():
         # Collect results 
         ignored_texts = \
             [sentence.enclosing_text for sentence in text['syntax_ignore'].spans]
-        print(ignored_texts)
+        #print(ignored_texts)
         # Check results
         assert ignored_texts == test_text['expected_ignore_texts']
 
