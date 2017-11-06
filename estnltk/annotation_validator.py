b3bc630d1a31306b57808e40508cbe3834c3600c
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-11-06 12:02:17 +0200

    Updated annotation validator: added a function for detecting sentences possibly mistakenly broken inside quotations

--- estnltk/annotation_validator.py
+++ estnltk/annotation_validator.py
@@ -14,6 +14,19 @@ _abbrev_pattern      = re.compile('\s([a-zöäüõšž\-.]+\.[a-zöäüõšž\-]
 _abbrev_caps_pattern = re.compile('\s([A-ZÖÄÜÕŠŽ\-.]+\.[A-ZÖÄÜÕŠŽ\-]+|[A-ZÖÄÜÕŠŽ\-]+[.])$')
 _unlikely_sent_start = re.compile('^([a-zöäüõšž\-,;]).*')
 
+_three_lc_words = '[a-zöäüõšž\-]+\s+[a-zöäüõšž\-]+\s+[a-zöäüõšž\-]+'
+_three_lc_words_compiled = re.compile(_three_lc_words)
+
+_double_quotes_patterns = [ # ""
+                            re.compile('("[^"]+?")'), \
+                            # «» 
+                            re.compile('(\u00AB[^\u00AB\u00BB]+?\u00BB)'), \
+                            # “”
+                            re.compile('([“\u201C][^“”\u201C\u201D]+?[”\u201D])'), \
+                            # 
+                          ]
+
+
 def find_sentence_breaks_inside_parentheses_generator( text: 'Text' ):
     ''' Analyses given Text object, and detects sequences of sentences 
         in which the first sentence contains '(', and the last sentence 
@@ -69,6 +82,108 @@ def find_sentence_breaks_inside_parentheses_generator( text: 'Text' ):
                 break
         if len(results['sentences']) > 1:
             yield results
+
+
+
+def find_sentence_breaks_inside_double_quotes_generator( text: 'Text',\
+                        apply_three_lc_words_filter:bool=False,\
+                        apply_character_length_filter:bool=True,\
+                        min_sentence_char_length:int=5, ):
+    ''' Analyses given Text object, and detects sequences of sentences 
+        in which the first sentence contains starting double quotes, and 
+        the last sentence contains ending double quotes, thus, it is 
+        possible that a single sentence containing quotes has been broken 
+        mistakenly into several sentences. 
+        
+        As a result, yields a dict containing start/end position of the 
+        quotation in text (under keys 'start' and 'end'), and a list 
+        of SpanList-s corresponding potentially broken sentences (under
+        key 'sentences');
+        
+        If apply_three_lc_words_filter==True (default: False), then the 
+        results are filtered: only if the one of the sentences overlapping 
+        with the quotation contains less than three consecutive lowercased 
+        words, the context is yielded (as a context containing potentially 
+        broken sentences); In such case, spans of sentences that are 
+        suspiciously short are stored under the key 'short_sentences' of
+        the returned dictionary;
+        
+        If apply_character_length_filter==True (default setting), then the 
+        results are filtered: only if the one of the sentences is shorter 
+        than min_sentence_char_length (default: 5), the context is yielded 
+        (as a context containing potentially broken sentences); In such case, 
+        spans of sentences that are suspiciously short are stored under the
+        key 'short_sentences' of the returned dictionary;
+        
+        In case of regular double quotes ("), sentences overlapping with 
+        the quotation are yielded only when there is an even number of 
+        double quotes in the text;
+    '''
+    # Check the layer requirements
+    requirements = ['words', 'sentences']
+    for requirement in requirements:
+        assert requirement in text.layers, \
+               '(!) The input text is missing the layer "'+requirement+'"!'
+    # Check all possible pairs of quotations
+    for quotes_pattern in _double_quotes_patterns:
+        if '"' in quotes_pattern.pattern:
+            # if we have the pattern of regular double quotes, we should 
+            # apply it only when the double quotes are balanced in the 
+            # input text
+            quotes_balanced = (text.text).count('"') % 2 == 0
+            if not quotes_balanced:
+                continue 
+        # Check overlaps between quotations and sentence boundaries
+        for matchobj in quotes_pattern.finditer(text.text):
+            start = matchobj.span(1)[0]
+            end   = matchobj.span(1)[1]
+            quoted_text = matchobj.group(1)
+            # Collect sentences that are covered by quotations
+            results = { 'sentences':[], 'start':start, 'end':end }
+            # Also remember sentences that are suspiciously short
+            too_short_sentences = []
+            for sid, sentence in enumerate( text['sentences'].spans ):
+                sentence_overlaps_with_quotation = False
+                if sentence.start <= start and start < sentence.end and \
+                   sentence.end < end:
+                    # Start
+                    results['sentences'].append( sentence )
+                    sentence_overlaps_with_quotation = True
+                elif start < sentence.start and sentence.end <= end:
+                    # Midpoint
+                    results['sentences'].append( sentence )
+                    sentence_overlaps_with_quotation = True
+                elif sentence.start <= end and end <= sentence.end:
+                    # End
+                    results['sentences'].append( sentence )
+                    sentence_overlaps_with_quotation = True
+                if sentence_overlaps_with_quotation:
+                    sent_text = sentence.enclosing_text
+                    if apply_three_lc_words_filter:
+                        if not _three_lc_words_compiled.search(sent_text):
+                            # The sentence contains less than three lowercase 
+                            # words, it is more likely that there is something 
+                            # wrong with it ...
+                            too_short_sentences.append(sentence)
+                    if apply_character_length_filter:
+                        if len(sent_text) < min_sentence_char_length:
+                            # if the sentence is smaller than min character length,
+                            # consider it as a sentence potentially problematic
+                            too_short_sentences.append(sentence)
+                if sentence.start > end:
+                    # No need to look further
+                    break
+            if apply_three_lc_words_filter or \
+               apply_character_length_filter:
+                if not too_short_sentences:
+                    # If we did not find any of the sentences being suspiciously
+                    # short, then skip this quotation
+                    continue
+                else:
+                    results['short_sentences'] = too_short_sentences
+            if len(results['sentences']) > 1:
+                yield results
+
 
 
 def find_sentence_breaking_abbreviations_generator( text: 'Text' ):
