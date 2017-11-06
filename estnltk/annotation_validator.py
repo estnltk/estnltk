32a9d35fee8db4c56bb64b2b35a559295e278b6a
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-11-06 10:20:11 +0200

    Updated annotation validator: added a function for detecting sentences possibly mistakenly broken because of abbreviations

--- estnltk/annotation_validator.py
+++ estnltk/annotation_validator.py
@@ -1,7 +1,7 @@
 #
 #   Functions for validating annotations / checking for potential errors
 #   Note that these validations are heuristic: there is no guarantee that 
-#   all cases found during the validation are problematic
+#   all cases found during the validation are actually problematic
 #
 
 import re
@@ -9,6 +9,10 @@ import re
 from estnltk import Text
 
 _parentheses_content = re.compile('(\([^()]+?\))')
+
+_abbrev_pattern      = re.compile('\s([a-zöäüõšž\-.]+\.[a-zöäüõšž\-]+|[a-zöäüõšž\-]+[.])$')
+_abbrev_caps_pattern = re.compile('\s([A-ZÖÄÜÕŠŽ\-.]+\.[A-ZÖÄÜÕŠŽ\-]+|[A-ZÖÄÜÕŠŽ\-]+[.])$')
+_unlikely_sent_start = re.compile('^([a-zöäüõšž\-,;]).*')
 
 def find_sentence_breaks_inside_parentheses_generator( text: 'Text' ):
     ''' Analyses given Text object, and detects sequences of sentences 
@@ -26,7 +30,7 @@ def find_sentence_breaks_inside_parentheses_generator( text: 'Text' ):
     requirements = ['compound_tokens', 'words', 'sentences']
     for requirement in requirements:
         assert requirement in text.layers, \
-               'The input text is missing the layer "'+requirement+'"!'
+               '(!) The input text is missing the layer "'+requirement+'"!'
     # Collect emoticons (as we do not want to mix up parentheses 
     # with emoticons)
     emoticons = []
@@ -65,4 +69,50 @@ def find_sentence_breaks_inside_parentheses_generator( text: 'Text' ):
                 break
         if len(results['sentences']) > 1:
             yield results
-       
+
+
+def find_sentence_breaking_abbreviations_generator( text: 'Text' ):
+    ''' Analyses given Text object, and detects pairs of sentences 
+        in which the first sentence ends with a potential abbreviation,
+        and the second sentence has a prefix which is unlikely a 
+        sentence start. Thus, it is possible that the pair represents
+        a single sentence mistakenly split into two sentences.
+        
+        As a result, yields a dict containing start/end position of the 
+        potential abbreviation in text (under keys 'start' and 'end'), 
+        and a list of SpanList-s containing potentially broken sentences 
+        (under key 'sentences');
+    '''
+    # Check the layer requirements
+    requirements = ['words', 'sentences']
+    for requirement in requirements:
+        assert requirement in text.layers, \
+               '(!) The input text is missing the layer "'+requirement+'"!'
+    # Iterate over consecutive sentences and detect mistakenly split sentences
+    for sid, sentence in enumerate( text['sentences'].spans ):
+        sent_text = sentence.enclosing_text
+        if sid + 1 < len(text['sentences'].spans):
+            next_sent = text['sentences'].spans[sid + 1]
+            next_sent_text = next_sent.enclosing_text
+            if _unlikely_sent_start.search( next_sent_text ):
+                target_span = (-1, -1)
+                target_str  = None
+                # Check for small-letters abbreviation
+                m1 = _abbrev_pattern.search(sent_text)
+                if m1:
+                    target_span = m1.span(1)
+                    target_str = m1.group(1)
+                # Check for capital-letters abbreviation
+                m2 = _abbrev_caps_pattern.search(sent_text)
+                if m2:
+                    target_span = m2.span(1)
+                    target_str = m2.group(1)
+                if target_span != (-1, -1) and target_str:
+                    results = { 'sentences':[], \
+                                'start': sentence.start+target_span[0], \
+                                'end':   sentence.start+target_span[1] }
+                    results['sentences'].append( sentence )
+                    results['sentences'].append( next_sent )
+                    yield results
+
+
