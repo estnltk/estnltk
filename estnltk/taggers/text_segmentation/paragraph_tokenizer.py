7ced2e83cf33496de2a018eb33d8222ecc1d9080
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-11-29 15:47:29 +0200

    Bugfix: paragraph tokenizer should not fail on empty inputs

--- estnltk/taggers/text_segmentation/paragraph_tokenizer.py
+++ estnltk/taggers/text_segmentation/paragraph_tokenizer.py
@@ -25,12 +25,12 @@ class ParagraphTokenizer(Tagger):
         layer = Layer(name=self.layer_name, enveloping ='sentences', ambiguous=False)
         paragraph_ends = {end for _, end in self.paragraph_tokenizer.span_tokenize(text.text)}
         start = 0
-        paragraph_ends.add(text.sentences[-1].end)
+        if text.sentences:
+            paragraph_ends.add(text.sentences[-1].end)
         for i, sentence in enumerate(text.sentences):
             if sentence.end in paragraph_ends:
                 layer.add_span(text.sentences[start:i+1])
                 start = i + 1
-
         if return_layer:
             return layer
         text[self.layer_name] = layer
---
+++ estnltk/tests/test_tokenizers/test_paragraph_tokenizer.py
@@ -0,0 +1,10 @@
+from estnltk.text import Text
+from estnltk.taggers import ParagraphTokenizer
+
+def test_apply_paragraph_tokenizer_on_empty_text():
+    # Applying paragraph tokenizer on empty text should not produce any errors
+    text = Text( '' )
+    text.tag_layer(['words', 'sentences', 'paragraphs'])
+    assert len(text.words) == 0
+    assert len(text.sentences) == 0
+    assert len(text.paragraphs) == 0
