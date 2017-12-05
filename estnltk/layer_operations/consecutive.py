9c22fc055200644325a05a08b9ead1fba355c6cf
estnltk <paul.tammo@gmail.com>
2017-12-05 12:38:42 +0200

    iterate_consequtive_spans max_gap default: max_int -> inf

--- estnltk/layer_operations/consecutive.py
+++ estnltk/layer_operations/consecutive.py
@@ -5,11 +5,9 @@
 from estnltk.text import Layer, SpanList, Span
 from typing import MutableMapping, Tuple, Any, Union, List
 
-from sys import maxsize as MAX_INT
 
-
-def iterate_consecutive_spans(spanlist:Union[List[Span], SpanList], max_gap:int=MAX_INT): 
-    '''  Given a SpanList or a List of Spans, yields pairs of Spans that are 
+def iterate_consecutive_spans(spanlist:Union[List[Span], SpanList], max_gap:int=float('inf')):
+    """  Given a SpanList or a List of Spans, yields pairs of Spans that are 
         positionally consecutive. Consecutive means that there can be a gap (in 
         size of max_gap) between the two spans, but none of the other spans may 
         fall entirely between the two consecutive spans.
@@ -20,27 +18,26 @@ def iterate_consecutive_spans(spanlist:Union[List[Span], SpanList], max_gap:int=
         
          Note that if a List of Spans is given as an input argument, it is sorted 
         before detection of the consecutive spans.
-    '''
+    """
     if max_gap < 0:
-        raise Exception ('(!) Value of max_gap should be positive int or zero!')
+        raise Exception('(!) Value of max_gap should be positive int or zero!')
     # Check the type of input list, and sort the regular spanlist
     if isinstance(spanlist, list) and spanlist:
-        if all( [isinstance(span, Span) for span in spanlist] ):
+        if all( [isinstance(span, Span) for span in spanlist]):
             spanlist = sorted(spanlist)
         else:
-            raise Exception (
+            raise Exception(
                 '(!) The input list contains unexpected types of elements: ', str(spanlist))
     # Assume that:
     #    1) spans in the list are sorted;
     #    2) if overlapping spans have equal start positions, then 
     #       shorter spans come first
     # Iterate over the list and detect consecutive spans
-    spl_enum = list(enumerate(spanlist))
-    for span_id, span in spl_enum:
+    for span_id, span in enumerate(spanlist):
         # Check the following spans
         spans_checked = []
-        for span_id_2 in range(span_id + 1, len(spl_enum)):
-            following_span = spl_enum[span_id_2][1]
+        for span_id_2 in range(span_id + 1, len(spanlist)):
+            following_span = spanlist[span_id_2]
             if 0 <= following_span.start - span.end <= max_gap:
                 if not spans_checked:
                     # No span between the two spans
@@ -67,30 +64,30 @@ def iterate_consecutive_spans(spanlist:Union[List[Span], SpanList], max_gap:int=
                 spans_checked.append(following_span)
 
 
-def iterate_touching_spans(spanlist:Union[List[Span], SpanList]): 
-    '''  Given a SpanList or a List of Spans, yields pairs of Spans that are 
+def iterate_touching_spans(spanlist:Union[List[Span], SpanList]):
+    """ Given a SpanList or a List of Spans, yields pairs of Spans that are 
         positionally touching (i.e. gap between the consecutive spans is 0).
-    '''
+    """
     yield from iterate_consecutive_spans(spanlist, max_gap=0)
 
 
-def iterate_hovering_spans(spanlist:Union[List[Span], SpanList], \
-                           min_gap:int=1,\
-                           max_gap:int=MAX_INT): 
-    '''  Given a SpanList or a List of Spans, yields pairs of Spans that are 
+def iterate_hovering_spans(spanlist:Union[List[Span], SpanList],
+                           min_gap:int=1,
+                           max_gap:int=float('inf')):
+    """ Given a SpanList or a List of Spans, yields pairs of Spans that are 
         positionally hovering, i.e. gap between the consecutive spans is at 
         least 1. 
         
         Arguments min_gap and max_gap can be additionally used for 
         constraining the minimal and maximal allowed gap between the yielded 
         spans. Note that it is required that 0 < min_gap <= max_gap;
-    '''
+    """
     if min_gap < 1:
-        raise Exception ('(!) Value of min_gap should be an integer > 0!')
+        raise Exception('(!) Value of min_gap should be an integer > 0!')
     if max_gap < 1:
-        raise Exception ('(!) Value of max_gap should be an integer > 0!')
+        raise Exception('(!) Value of max_gap should be an integer > 0!')
     if not (min_gap <= max_gap):
-        raise Exception ('(!) Value of min_gap should be <= max_gap!')
+        raise Exception('(!) Value of min_gap should be <= max_gap!')
     for (span, following_span) in iterate_consecutive_spans(spanlist, max_gap=max_gap):
         if min_gap <= (following_span.start - span.end) <= max_gap:
             yield (span, following_span)
