d29a70c82fa9e52011ad8c5fc459b20c46d381da
Siim Orasmaa <siim.orasmaa@gmail.com>
2017-12-22 13:50:50 +0200

    Updated TokensTagger's documentation

--- estnltk/taggers/text_segmentation/tokens_tagger.py
+++ estnltk/taggers/text_segmentation/tokens_tagger.py
@@ -1,3 +1,11 @@
+#
+#  TokensTagger splits text into tokens, based on whitespace 
+#  and/or punctuation. 
+#  This is the most automic segmentation: in later analysis, 
+#  tokens won't be broken into any smaller parts, but only 
+#  joined if necessary.
+# 
+
 from typing import Union
 import re
 
@@ -22,10 +30,40 @@ class TokensTagger(Tagger):
     configuration = None
     
     def __init__(self, apply_punct_postfixes:bool=True):
+        """Initializes TokensTagger.
+        
+        Parameters
+        ----------
+        apply_punct_postfixes: boolean (default: True)
+            If True, post-fixes will be applied to WordPunctTokenizer's
+            tokenization results: if a sequence of punctuation symbols 
+            forms a single token, then the token will be split into 
+            smaller tokens, so that there is one token for each 
+            punctuation symbol.
+        """
         self._apply_punct_postfixes = apply_punct_postfixes
         self.configuration = {'apply_punct_postfixes': self._apply_punct_postfixes}
 
     def tag(self, text:'Text', return_layer:bool=False) -> Union['Text', Layer]:
+        """Segments given Text into tokens. 
+        
+        Parameters
+        ----------
+        text: estnltk.text.Text
+            Text object that is to be tokenized;
+
+        return_layer: boolean (default: False)
+            If True, then the new layer is returned; otherwise 
+            the new layer is attached to the Text object, and 
+            the Text object is returned;
+
+        Returns
+        -------
+        Text or Layer
+            If return_layer==True, then returns the new layer, 
+            otherwise attaches the new layer to the Text object 
+            and returns the Text object;
+        """
         spans = list(tokenizer.span_tokenize(text.text))
         if self._apply_punct_postfixes:
             #  WordPunctTokenizer may leave tokenization of punctuation 
@@ -52,6 +90,13 @@ class TokensTagger(Tagger):
         return text
 
     def _split_into_symbols( self, spans, spans_to_split ):
+        '''Splits a subset of spans from the list spans into 
+           single symbol spans. For instance, if 
+             spans = ['(', 'a', '.).']
+             spans_to_split = [ (2,5) ]
+           then we get:
+             ['(', 'a', '.', ')', '.']
+           '''
         new_spans = []
         for start, end in spans:
             if spans_to_split and (start, end) in spans_to_split:
