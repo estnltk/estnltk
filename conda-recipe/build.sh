d6ca3f1667944b877500d1ac8c8360695eed221d
Alexander Tkachenko <alex.tk.fb@gmail.com>
2018-01-02 20:01:24 +0200

    updated .travis.yml

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 CC=${PREFIX}/bin/g++
 CXX=${PREFIX}/bin/g++
-export PYTHONNOUSERSITE=1
 $PYTHON setup.py build
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
