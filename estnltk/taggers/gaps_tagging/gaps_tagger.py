6677df2549c7b66e6b7176c8dfb269931bb710d1
estnltk <paul.tammo@gmail.com>
2018-01-11 10:10:52 +0200

    GapsTagger now works on enveloping layers

--- estnltk/taggers/gaps_tagging/gaps_tagger.py
+++ estnltk/taggers/gaps_tagging/gaps_tagger.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-from estnltk.spans import Span
+from estnltk.spans import Span, SpanList
 from estnltk.layer import Layer
 from estnltk.taggers import Tagger
 
@@ -26,35 +26,74 @@ class GapsTagger(Tagger):
         self.configuration['decorator function'] = decorator
 
     def make_layer(self, raw_text, layers):
-        assert len({layers[input_layer].parent for input_layer in self.depends_on}) == 1, {layers[input_layer].parent for input_layer in self.depends_on}
-        assert len({layers[input_layer].enveloping for input_layer in self.depends_on}) == 1
+        parent = {layers[input_layer].parent for input_layer in self.depends_on}
+        enveloping = {layers[input_layer].enveloping for input_layer in self.depends_on}
+        assert len(parent) == 1, 'different parent layers: ' + str(parent)
+        assert len(enveloping) == 1, 'different enveloped layers: ' + str(enveloping)
+        enveloping = enveloping.pop()
         layer = Layer(
             name=self.layer_name,
             attributes=self.attributes,
             parent=None,
-            enveloping=None,
+            enveloping=enveloping,
             ambiguous=False
             )
-        layers = [layers[layer_name] for layer_name in self.depends_on]
-        for start, end in find_gaps(layers, len(raw_text)):
-            assert start < end
-            if self.trim:
-                t = self.trim(raw_text[start:end])
-                start = raw_text.find(t, start)
-                end = start + len(t)
-            if start < end:
-                span = Span(start, end)
+        layers_list = [layers[layer_name] for layer_name in self.depends_on]
+        if enveloping is None:
+            for start, end in find_gaps(layers_list, len(raw_text)):
+                assert start < end
+                if self.trim:
+                    t = self.trim(raw_text[start:end])
+                    start = raw_text.find(t, start)
+                    end = start + len(t)
+                if start < end:
+                    span = Span(start, end)
+                    if self.decorator:
+                        decorations = self.decorator(raw_text[start:end])
+                        for attr in self.attributes:
+                            setattr(span, attr, decorations[attr])
+                    layer.add_span(span)
+        else:
+            for gap in enveloping_gaps(layers_list, layers[enveloping]):
+                spl = SpanList()
+                spl.spans = gap
                 if self.decorator:
-                    decorations = self.decorator(raw_text[start:end])
+                    decorations = self.decorator(gap)
                     for attr in self.attributes:
-                        setattr(span, attr, decorations[attr])
-                layer.add_span(span)
+                        setattr(spl, attr, decorations[attr])
+                layer.add_span(spl)
         return layer
 
     def tag(self, text):
         layer = self.make_layer(text.text, text.layers)
         assert isinstance(layer, Layer), 'make_layer must return a Layer instance'
         text[self.layer_name] = layer
+
+
+def enveloping_gaps(layers, enveloped):
+    cover = set()
+    for layer in layers:
+        if layer.ambiguous:
+            for sp_list in layer.spans.spans:
+                cover.update(sp_list[0])
+        else:
+            for sp_list in layer.spans.spans:
+                cover.update(sp_list)
+
+    spans = iter(enveloped)
+    s = next(spans)
+    while s:
+        while s in cover:
+            s = next(spans)
+        gap = []
+        while s not in cover:
+            gap.append(s)
+            try:
+                s = next(spans)
+            except StopIteration:
+                s = None
+                break
+        yield gap
 
 
 def find_gaps(layers, text_length):
