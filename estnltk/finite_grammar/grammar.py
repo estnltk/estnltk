8cc7fdc537c5cb7c23092605675434dbadb12f35
estnltk <paul.tammo@gmail.com>
2018-02-05 12:30:53 +0200

    moved rule_maps from parsing to Grammar; added width_limit

--- estnltk/finite_grammar/grammar.py
+++ estnltk/finite_grammar/grammar.py
@@ -1,11 +1,26 @@
-from typing import Sequence
+from typing import Sequence, Union
+from collections import defaultdict
+import regex as re
+import networkx as nx
+
+_search_parenthesis = re.compile('\(|\)').search
+
+
+def contains_parenthesis(s):
+    return _search_parenthesis(s) is not None
+
+_match_SEQ_pattern = re.compile('(SEQ|REP)\((.*)\)$').match
 
 
 class Grammar:
-
     _internal_attributes = frozenset({'name', 'text', 'start', 'end', '_terminals_', '_support_', '_priority_', '_group_'})
 
-    def __init__(self, *, start_symbols: Sequence, rules: list=None, max_depth: int=float('inf'), legal_attributes=None):
+    def __init__(self, *,
+                 start_symbols: Sequence,
+                 rules: list=None,
+                 depth_limit: int=float('inf'),
+                 width_limit: int=float('inf'),
+                 legal_attributes=None):
         if legal_attributes is None:
             self.legal_attributes = frozenset()
         else:
@@ -17,16 +32,43 @@ class Grammar:
         else:
             self._rules = rules
         self.start_symbols = start_symbols
-        self.max_depth = max_depth
-        self._checked = True
+        self.depth_limit = depth_limit
+        self.width_limit = width_limit
+
+        self._setup = False
+        self._setup_grammar()
 
     @property
     def rules(self):
-        if not self._checked:
-            self._check_rules()
-            self._terminals_and_nonterminals()
-            self._checked = True
+        self._setup_grammar()
         return self._rules
+
+    @property
+    def rule_map(self):
+        self._setup_grammar()
+        return self._rule_map
+
+    @property
+    def hidden_rule_map(self):
+        self._setup_grammar()
+        return self._hidden_rule_map
+
+    def has_finite_max_depth(self):
+        """
+        grammar is finite if there is a finite number of rules that can be applied before getting all terminals
+        when applied standard rules introduce a tree
+        A -> REP(B)
+
+        Returns False if there is a cycle in the rules,
+        that is the case when there are rules e.g. A -> B, B -> C, C -> A,
+        but the rule A -> A A alone does not form a cycle in this sense.
+        Returns True otherwise.
+        """
+        rule_graph = nx.DiGraph()
+        for rule in self._rules:
+            for r in rule.rhs:
+                rule_graph.add_edge(rule.lhs, r)
+        return nx.is_directed_acyclic_graph(rule_graph)
 
     def _terminals_and_nonterminals(self):
         self.nonterminals = frozenset(r['lhs'] for r in self._rules)
@@ -35,12 +77,36 @@ class Grammar:
             terminals.update(i)
         self.terminals = frozenset(terminals - self.nonterminals)
 
-    def _check_rules(self):
+    def _rule_maps(self):
+        self._rule_map = defaultdict(list)
+        plus_symbols = set()
+        for rule in self._rules:
+            for pos, rhs in enumerate(rule.rhs):
+                self._rule_map[rhs].append((rule, pos))
+                m = _match_SEQ_pattern(rhs)
+                if m is not None:
+                    plus_symbols.add((rhs, m.group(2)))
+
+        self._hidden_rule_map = {}
+        for ps, s in plus_symbols:
+            self._hidden_rule_map[ps] = [(Rule(ps, (ps, ps)), 0),
+                                         (Rule(ps, (ps, ps)), 1)]
+            self._hidden_rule_map[s] = [(Rule(ps, s), 0)]
+
+    def _setup_grammar(self):
+        if self._setup:
+            return
         assert len(self._rules) == len({(r.lhs, r.rhs) for r in self._rules}), 'repetitive rules'
+        assert (self.depth_limit < float('inf') or
+                self.width_limit < float('inf') or
+                self.has_finite_max_depth()), 'infinite grammar without depth or width limit'
+        self._terminals_and_nonterminals()
+        self._rule_maps()
+        self._setup = True
 
     def add(self, rule):
         self._rules.append(rule)
-        self._checked = False
+        self._setup = False
 
     def __getitem__(self, key):
         if key in self.nonterminals:
@@ -49,6 +115,7 @@ class Grammar:
             return self.rules[key]
 
     def __str__(self):
+        self._setup_grammar()
         rules = '\n\t'.join([str(i) for i in self.rules])
         terminals = ', '.join(sorted(self.terminals))
         nonterminals = ', '.join(sorted(self.nonterminals))
@@ -58,11 +125,12 @@ Grammar:
 \tterminals: {terminals}
 \tnonterminals: {nonterminals}
 \tlegal attributes: {self.legal_attributes}
-\tmax_depth: {max_depth}
+\tdepth_limit: {self.depth_limit}
+\twidth_limit: {self.width_limit}
 Rules:
 \t{rules}
 '''.format(start=', '.join(self.start_symbols), rules=rules, terminals=terminals,
-           nonterminals=nonterminals, max_depth=self.max_depth, self=self)
+           nonterminals=nonterminals, self=self)
 
     def __repr__(self):
         return str(self)
@@ -77,10 +145,15 @@ class Rule:
     def default_decorator(x):
         return {}
 
-    def __init__(self, lhs, rhs, priority: int=0, group=None, decorator=None, validator=None):
+    def __init__(self, lhs: str, rhs: Union[str, Sequence[str]], priority: int=0, group=None, decorator=None, validator=None):
+        assert not contains_parenthesis(lhs) or _match_SEQ_pattern(lhs), 'parenthesis not allowed: ' + lhs
         self.lhs = lhs
         if isinstance(rhs, str):
             rhs = rhs.split()
+        for r in rhs:
+            assert isinstance(r, str), 'rhs must be a str or sequence of str'
+            if contains_parenthesis(r):
+                assert _match_SEQ_pattern(r) is not None, 'parenthesis only allowed with SEQ or REP: ' + rhs
         self.rhs = tuple(rhs)
 
         self.priority = priority
