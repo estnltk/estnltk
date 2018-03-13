7b74d7565931f065a707bcdb728d8ca311244e8a
estnltk <paul.tammo@gmail.com>
2018-03-13 20:11:17 +0200

    updated MergeTagger

--- estnltk/taggers/merge_tagging/merge_tagger.py
+++ estnltk/taggers/merge_tagging/merge_tagger.py
@@ -1,4 +1,5 @@
-from estnltk.layer import Layer
+from typing import Sequence
+
 from estnltk.taggers import TaggerNew
 from estnltk.layer_operations import merge_layers
 
@@ -9,12 +10,15 @@ class MergeTagger(TaggerNew):
     description = 'Merges input layers.'
     conf_param = ()
 
-    def __init__(self, layer_name, input_layers, attributes=()):
-        self.depends_on = input_layers
-        assert len(self.depends_on) > 0
-        self.layer_name = layer_name
-        self.attributes = tuple(attributes)
+    def __init__(self,
+                 output_layer: str,
+                 input_layers: Sequence[str],
+                 output_attributes: Sequence[str]=()):
+        self.input_layers = input_layers
+        assert len(self.input_layers) > 0
+        self.output_layer = output_layer
+        self.output_attributes = tuple(output_attributes)
 
     def make_layer(self, text, input_layers, status):
         layers = list(input_layers.values())
-        return merge_layers(layers, self.layer_name, self.attributes)
+        return merge_layers(layers, self.output_layer, self.output_attributes)
