b9d08ac2a6e3538afd3f9ac4200f47c014a8ad01
estnltk <paul.tammo@gmail.com>
2018-03-14 14:53:27 +0200

    moved vocabulary.py

--- estnltk/taggers/__init__.py
+++ estnltk/taggers/__init__.py
@@ -3,10 +3,9 @@ from estnltk.taggers.retagger import Retagger
 
 from estnltk.taggers.tagger_new import TaggerNew
 
-from estnltk.taggers.vocabulary import read_vocabulary
-
 from estnltk.taggers.combined_tagger import CombinedTagger
 
+from estnltk.taggers.dict_taggers.vocabulary import read_vocabulary
 from estnltk.taggers.dict_taggers.phrase_tagger import PhraseTagger
 from estnltk.taggers.dict_taggers.span_tagger import SpanTagger
 
