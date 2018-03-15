72f335bcb3c9eaae1ba07d73d34ddc6f1d6214ba
estnltk <paul.tammo@gmail.com>
2018-03-15 11:39:06 +0200

    removed description, added conflict resolving to combined_tagger

--- estnltk/taggers/combined_tagger.py
+++ estnltk/taggers/combined_tagger.py
@@ -1,27 +1,33 @@
 from typing import Sequence
 from estnltk.taggers import TaggerNew
-from estnltk.layer_operations import merge_layers
+from estnltk.layer_operations import merge_layers, resolve_conflicts
 
 
 class CombinedTagger(TaggerNew):
     """Runs input taggers in parallel and resolves conflicts."""
-    description = 'Runs input taggers in parallel and resolves conflicts.'
-    conf_param = ('_taggers', 'priority_attribute')
+    conf_param = ('_taggers', 'priority_attribute', 'conflict_resolving_strategy')
 
     def __init__(self,
                  output_layer: str,
                  output_attributes: Sequence[str],
                  taggers: Sequence[TaggerNew],
+                 conflict_resolving_strategy: str='ALL',
                  priority_attribute: str = None
                  ):
         self.output_layer = output_layer
         self.input_layers = tuple({layer for tagger in taggers for layer in tagger.input_layers})
         self._taggers = taggers
         self.output_attributes = output_attributes
+        self.conflict_resolving_strategy = conflict_resolving_strategy
         self.priority_attribute = priority_attribute
 
     def make_layer(self, raw_text, input_layers, status):
         layers = [tagger.make_layer(raw_text, input_layers, status) for tagger in self._taggers]
-        return merge_layers(layers=layers,
-                            output_layer=self.output_layer,
-                            output_attributes=self.output_attributes)
+        layer = merge_layers(layers=layers,
+                             output_layer=self.output_layer,
+                             output_attributes=self.output_attributes)
+        resolve_conflicts(layer=layer,
+                          conflict_resolving_strategy=self.conflict_resolving_strategy,
+                          priority_attribute=self.priority_attribute,
+                          status=status)
+        return layer
