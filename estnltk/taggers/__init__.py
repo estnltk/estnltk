551db6783a3f590f5966d9a3dcea10d7b1264f6e
estnltk <paul.tammo@gmail.com>
2018-04-10 11:09:08 +0300

    DisambiguatingTagger

--- estnltk/taggers/__init__.py
+++ estnltk/taggers/__init__.py
@@ -27,6 +27,7 @@ from estnltk.taggers.morph_analysis.userdict_tagger import UserDictTagger
 
 from estnltk.taggers.sequential_tagger import SequentialTagger
 
+from estnltk.taggers.standard_taggers.disambiguating_tagger import DisambiguatingTagger
 from estnltk.taggers.standard_taggers.enveloping_gap_tagger import EnvelopingGapTagger
 from estnltk.taggers.standard_taggers.gap_tagger import GapTagger
 from estnltk.taggers.standard_taggers.layer_merge_tagger import MergeTagger
---
+++ estnltk/taggers/standard_taggers/disambiguating_tagger.py
@@ -0,0 +1,47 @@
+from estnltk import Layer, AmbiguousSpan, Span, EnvelopingSpan
+from estnltk.taggers import Tagger
+
+
+def default_decorator(span, raw_text):
+    return {}
+
+
+class DisambiguatingTagger(Tagger):
+    """Disambiguates ambiguous layer."""
+
+    conf_param = ('decorator',)
+
+    def __init__(self,
+                 output_layer: str,
+                 input_layer: str,
+                 output_attributes=(),
+                 decorator=default_decorator):
+        self.input_layers = [input_layer]
+        self.output_layer = output_layer
+        self.output_attributes = output_attributes
+        self.decorator = decorator
+
+    def _make_layer(self, raw_text: str, layers: dict, status: dict) -> Layer:
+        input_layer = layers[self.input_layers[0]]
+        parent = input_layer.parent
+        enveloping = input_layer.enveloping
+        layer = Layer(name=self.output_layer,
+                      attributes=self.output_attributes,
+                      parent=parent,
+                      enveloping=enveloping,
+                      ambiguous=False)
+        for input_span in input_layer:
+            assert isinstance(input_span, AmbiguousSpan)
+            if parent:
+                span = Span(parent=parent,
+                            legal_attributes=self.output_attributes)
+            elif enveloping:
+                span = EnvelopingSpan(spans=input_span[0].spans)
+            else:
+                span = Span(start=input_span[0].start,
+                            end=input_span[0].end,
+                            legal_attributes=self.output_attributes)
+            for k, v in self.decorator(input_span, raw_text).items():
+                setattr(span, k, v)
+            layer.add_span(span)
+        return layer
