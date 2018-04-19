ef9a493e696c903909377c876e7c59210e13e193
estnltk <paul.tammo@gmail.com>
2018-04-19 11:33:18 +0300

    default_values (former decorator) for PhraseTagger

--- estnltk/taggers/dict_taggers/phrase_tagger.py
+++ estnltk/taggers/dict_taggers/phrase_tagger.py
@@ -1,6 +1,5 @@
-from estnltk import EnvelopingSpan
+from estnltk import EnvelopingSpan, Layer
 from estnltk.taggers import Tagger, Vocabulary
-from estnltk.layer.layer import Layer
 from estnltk.layer_operations import resolve_conflicts
 from collections import defaultdict
 from typing import Sequence, Union
@@ -14,11 +13,12 @@ class PhraseTagger(Tagger):
                  output_layer: str,
                  input_layer: str,
                  input_attribute: str,
-                 vocabulary: Union[str, dict]=None,
-                 key: str='_phrase_',
+                 vocabulary: Union[str, list, dict, Vocabulary],
+                 key: str=None,
                  output_attributes: Sequence=None,
                  global_validator: callable=None,
                  validator_attribute: str='_validator_',
+                 default_values=None,
                  conflict_resolving_strategy: str='MAX',
                  priority_attribute: str=None,
                  ambiguous: bool=False
@@ -31,50 +31,70 @@ class PhraseTagger(Tagger):
             The name of the input layer.
         :param input_attribute: str
             The name of the input layer attribute.
-        :param vocabulary: dict, str
-            A vocabulary dict or file name.
-        :param global_validator: callable
-            Global global_validator function.
-        :param validator_attribute: str
-            The name of the attribute that points to the global_validator function in the vocabulary.
+        :param vocabulary: list, dict, str, Vocabulary
+            A vocabulary list of records, dict, file name or Vocabulary object.
         :param key: str
-            The name of the index column if the vocabulary is read from a csv file.
+            The name of the vocabulary index if the input vocabulary is a list of records, a dict or a csv file.
+        :param output_attributes
+            Names of the output layer attributes.
+        :param global_validator: callable
+            Global validator function.
+        :param validator_attribute: str
+            The name of the attribute that points to the validator function in the vocabulary.
+        :param default_values: dict
+            Default values for the input vocabulary records.
         :param  conflict_resolving_strategy: 'ALL', 'MAX', 'MIN' (default: 'MAX')
             Strategy to choose between overlapping events.
+        :param priority_attribute: str
+            Name of the attribute that is used to resolve conflicts.
+        :param ambiguous: bool
+            Whether the output layer is ambiguous.
         """
-        self.conf_param = ('input_attribute', 'global_validator', 'validator_attribute', 'conflict_resolving_strategy',
-                           'priority_attribute', 'ambiguous', '_vocabulary', '_heads')
-        if conflict_resolving_strategy not in {'ALL', 'MIN', 'MAX'}:
-            raise ValueError('Unknown conflict_resolving_strategy: ' + str(conflict_resolving_strategy))
-        self.conflict_resolving_strategy = conflict_resolving_strategy
-        self.priority_attribute = priority_attribute
+        self.conf_param = ('input_attribute', 'vocabulary', 'global_validator', 'validator_attribute', 'default_values',
+                           'conflict_resolving_strategy', 'priority_attribute', 'ambiguous', '_heads')
+
         self.output_layer = output_layer
+        self.input_layers = [input_layer]
         self.input_attribute = input_attribute
+
         if output_attributes is None:
-            self.output_attributes = []
-        else:
-            self.output_attributes = list(output_attributes)
+            output_attributes = ()
+        self.output_attributes = tuple(output_attributes)
+
+        if global_validator is None:
+            global_validator = default_validator
+        self.global_validator = global_validator
 
         self.validator_attribute = validator_attribute
-        if global_validator is None:
-            self.global_validator = default_validator
-        else:
-            self.global_validator = global_validator
 
-        self.input_layers = [input_layer]
+        if default_values is None:
+            default_values = {}
+        self.default_values = default_values
+
+        self.conflict_resolving_strategy = conflict_resolving_strategy
+        self.priority_attribute = priority_attribute
 
         self.ambiguous = ambiguous
 
-        self._vocabulary = Vocabulary(vocabulary=vocabulary,
-                                      key=key,
-                                      default_rec={self.validator_attribute: default_validator})
+        self.vocabulary = Vocabulary(vocabulary=vocabulary,
+                                     key=key,
+                                     default_rec={self.validator_attribute: default_validator,
+                                                  **default_values})
 
-        if not self.ambiguous:
-            assert all(len(values) == 1 for values in self._vocabulary.values()),\
-                'ambiguous==False but vocabulary is ambiguous'
+        assert key is None or key == self.vocabulary.key,\
+            'mismatching key and vocabulary.key: {}!={}'.format(key, self.vocabulary.key)
+        assert self.validator_attribute in self.vocabulary.attributes,\
+            'validator attribute is not among the vocabulary attributes: ' + str(self.validator_attribute)
+        assert set(self.output_attributes) <= set(self.vocabulary.attributes),\
+            'some output_attributes missing in vocabulary attributes: {}'.format(
+                set(self.output_attributes)-set(self.vocabulary.attributes))
+        assert self.priority_attribute is None or self.priority_attribute in self.vocabulary.attributes,\
+            'priority attribute is not among the vocabulary attributes: ' + str(self.priority_attribute)
+        assert self.ambiguous or all(len(values) == 1 for values in self.vocabulary.values()),\
+            'ambiguous==False but vocabulary is ambiguous'
 
         self._heads = defaultdict(list)
-        for phrase in self._vocabulary:
+        for phrase in self.vocabulary:
             self._heads[phrase[0]].append(phrase[1:])
 
     def _make_layer(self, raw_text: str, layers: dict, status: dict):
@@ -99,11 +119,15 @@ class PhraseTagger(Tagger):
                                         break
                                 if match:
                                     phrase = (value,) + tail
-                                    for rec in self._vocabulary[phrase]:
+                                    for rec in self.vocabulary[phrase]:
                                         span = EnvelopingSpan(spans=input_layer[i:i + len(tail) + 1].spans)
-                                        if self.global_validator(raw_text, span) and rec[self.validator_attribute](raw_text, span):
+                                        if self.global_validator(span, raw_text) and rec[self.validator_attribute](span, raw_text):
                                             for attr in self.output_attributes:
-                                                setattr(span, attr, rec[attr])
+                                                attr_value = rec[attr]
+                                                if callable(attr_value):
+                                                    setattr(span, attr, attr_value(span, raw_text))
+                                                else:
+                                                    setattr(span, attr, attr_value)
                                             layer.add_span(span)
         else:
             for i, value in enumerate(value_list):
@@ -117,19 +141,24 @@ class PhraseTagger(Tagger):
                                     break
                             if match:
                                 phrase = (value,) + tail
-                                for rec in self._vocabulary[phrase]:
+                                for rec in self.vocabulary[phrase]:
                                     spans = input_layer.span_list[i:i + len(tail) + 1]
-                                    span = EnvelopingSpan(spans=spans, layer=layer)
-                                    if self.global_validator(raw_text, span) and rec[self.validator_attribute](raw_text, span):
+                                    span = EnvelopingSpan(spans=spans)
+                                    if self.global_validator(span, raw_text) and rec[self.validator_attribute](self, raw_text):
                                         for attr in self.output_attributes:
-                                            setattr(span, attr, rec[attr])
+                                            attr_value = rec[attr]
+                                            if callable(attr_value):
+                                                setattr(span, attr, attr_value(span, raw_text))
+                                            else:
+                                                setattr(span, attr, attr_value)
                                         layer.add_span(span)
 
         resolve_conflicts(layer,
                           conflict_resolving_strategy=self.conflict_resolving_strategy,
-                          priority_attribute=self.priority_attribute)
+                          priority_attribute=self.priority_attribute,
+                          keep_equal=True)
         return layer
 
 
-def default_validator(text, span):
+def default_validator(span: EnvelopingSpan, raw_text: str) -> bool:
     return True
