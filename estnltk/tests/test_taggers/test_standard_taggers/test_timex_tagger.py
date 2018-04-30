6e9a1fc7a26fe2984b8b310a73b6ec9242c5073d
Siim Orasmaa <siim.orasmaa@ut.ee>
2018-04-30 15:40:57 +0300

    Updated TimexTagger's tests

--- estnltk/tests/test_taggers/test_standard_taggers/test_timex_tagger.py
+++ estnltk/tests/test_taggers/test_standard_taggers/test_timex_tagger.py
@@ -3,6 +3,7 @@ from estnltk.taggers import TimexTagger
 
 
 def test_timex_tagging_1():
+    # Test the basic functionality of the TimexTagger
     all_timex_attributes = ['text']+list(TimexTagger.attributes)
     #all_timex_attributes = list(TimexTagger.attributes)
     tagger = TimexTagger()
@@ -65,31 +66,86 @@ def test_timex_tagging_1():
 
 
 def test_timex_tagging_2_implicit_durations():
+    # Test TimexTagger on detecting timexes with implicit durations/intervals ('part_of_interval' attrib):
     all_timex_attributes = ['text']+list(TimexTagger.attributes)+['part_of_interval']
     tagger = TimexTagger( mark_part_of_interval=True )
-    test_data = [ # Timexes with implicit durations/intervals ('part_of_interval'):
-                  {'text': 'Rahvusvaheline Festival Jazzkaar toimub 20.- 28. aprillini.',\
-                   'dct':'2012-04-XX',\
+    test_data = [ {'text': 'Rahvusvaheline Festival Jazzkaar toimub 20.- 28. aprillini.',\
+                   'dct':'2012-04-15',\
                    'expected_timexes': [ \
                                  {'text':['20.'], 'enclosing_text': '20.', 'tid':'t1', 'type':'DATE', 'value':'2012-04-20', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                                  {'text':['28.', 'aprillini'], 'enclosing_text': '28. aprillini', 'tid':'t2', 'type':'DATE', 'value':'2012-04-28', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                               ]
                   },\
                   {'text': 'Filmi lühendatud versiooni saab näha ringvaatena 18.- 23. maini kinos Ateena .',\
-                   'dct':'2013-04-XX',\
+                   'dct':'2013-04-30',\
                    'expected_timexes': [ \
                                  {'text':['18.'], 'enclosing_text': '18.', 'tid':'t1', 'type':'DATE', 'value':'2013-05-18', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                                  {'text':['23.', 'maini'], 'enclosing_text': '23. maini', 'tid':'t2', 'type':'DATE', 'value':'2013-05-23', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                               ]
                   },\
                   {'text': 'Kui varem räägiti aastatest 2015- 2016, siis nüüd oli ajaline tärmin nihkunud 2018.- 2019. aastasse.',\
-                   'dct':'2013-XX-XX',\
+                   'dct':'2013-12-02',\
                    'expected_timexes': [ \
                                  {'text':['aastatest','2015'], 'tid':'t1', 'type':'DATE', 'value':'2015', 'temporal_function':False, 'anchor_time_id':None, 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXY', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                                  {'text':['2016'], 'tid':'t2', 'type':'DATE', 'value':'2016', 'temporal_function':False, 'anchor_time_id':None, 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXY', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                                  {'text':['nüüd'],'tid':'t4', 'type':'DATE', 'value':'PRESENT_REF', 'temporal_function':True, 'anchor_time_id':'t0'},\
                                  {'text':['2018.'], 'tid':'t5', 'type':'DATE', 'value':'2018', 'temporal_function':False, 'anchor_time_id':None, 'part_of_interval': {'beginPoint': 't5', 'endPoint': 't6', 'tid': 't7', 'value': 'PXXY', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
                                  {'text':['2019.', 'aastasse'], 'tid':'t6', 'type':'DATE', 'value':'2019', 'temporal_function':False, 'anchor_time_id':None, 'part_of_interval': {'beginPoint': 't5', 'endPoint': 't6', 'tid': 't7', 'value': 'PXXY', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
+                              ]
+                  },\
+                ]
+
+    for test_item in test_data:
+        # Prepare text
+        text = Text(test_item['text'])
+        if 'dct' in test_item:
+            text.meta['dct'] = test_item['dct']
+        text.tag_layer(['words', 'sentences', 'morph_analysis'])
+        # Tag timexes
+        tagger.tag(text)
+        # Compare attributes and values of all timexes
+        for timex_id, expected_timex in enumerate(test_item['expected_timexes']):
+            # Expected attributes & values
+            expected_attribs = [attr for attr in all_timex_attributes if attr in expected_timex]
+            expected_vals    = [expected_timex[attr] for attr in all_timex_attributes if attr in expected_timex]
+            # Obtained attributes & values
+            result_vals      = list(text.timexes[timex_id, expected_attribs])
+            if 'enclosing_text' in expected_timex:
+               result_vals   = [ text.timexes[timex_id].enclosing_text ] + result_vals
+               expected_vals = [expected_timex['enclosing_text']] + list(expected_vals)
+            #print(expected_vals, result_vals)
+            # Make assertions
+            assert expected_vals == result_vals
+
+    # Clean-up: Terminate Java process
+    tagger._java_process._process.terminate()
+
+
+
+def test_timex_tagging_3_gaps_in_dct():
+    # Test TimexTagger on texts that have gaps in their document creation dates
+    all_timex_attributes = ['text']+list(TimexTagger.attributes)+['part_of_interval']
+    tagger = TimexTagger( mark_part_of_interval=True )
+    test_data = [ {'text': 'Rahvusvaheline Festival Jazzkaar toimub 20.- 28. aprillini.',\
+                   'dct':'2012-04-XX',\
+                   'expected_timexes': [ \
+                                 {'text':['20.'], 'enclosing_text': '20.', 'tid':'t1', 'type':'DATE', 'value':'2012-04-20', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
+                                 {'text':['28.', 'aprillini'], 'enclosing_text': '28. aprillini', 'tid':'t2', 'type':'DATE', 'value':'2012-04-28', 'temporal_function':True , 'anchor_time_id':'t0', 'part_of_interval': {'beginPoint': 't1', 'endPoint': 't2', 'tid': 't3', 'value': 'PXXD', 'type': 'DURATION', 'temporalFunction': 'true'} }, \
+                              ]
+                  },\
+                  {'text': 'Sel aastal jäi maikrahvivalimine maikuus ära.',\
+                   'dct':'2010-XX-XX',\
+                   'expected_timexes': [ \
+                                 {'text':['Sel', 'aastal'], 'enclosing_text': 'Sel aastal', 'tid':'t1', 'type':'DATE', 'value':'2010', 'temporal_function':True , 'anchor_time_id':'t0' }, \
+                                 {'text':['maikuus'], 'enclosing_text': 'maikuus', 'tid':'t2', 'type':'DATE', 'value':'XXXX-XX', 'temporal_function':True , 'anchor_time_id':'t0' }, \
+                              ]
+                  },\
+                  {'text': 'Kui kirjutamise aeg puudu, on üsna raske öelda, mida mõeldakse neljapäeva või tuleva aasta all. 2010 a. on selge.',\
+                   'dct':'XXXX-XX-XX',\
+                   'expected_timexes': [ \
+                                 {'text':['neljapäeva'], 'enclosing_text': 'neljapäeva', 'tid':'t1', 'type':'DATE', 'value':'XXXX-XX-XX', 'temporal_function':True , 'anchor_time_id':'t0' }, \
+                                 {'text':['tuleva', 'aasta'], 'enclosing_text': 'tuleva aasta', 'tid':'t2', 'type':'DATE', 'value':'XXXX', 'temporal_function':True , 'anchor_time_id':'t0' }, \
+                                 {'text':['2010', 'a.'], 'enclosing_text': '2010 a.', 'tid':'t3', 'type':'DATE', 'value':'2010', 'temporal_function':False }, \
                               ]
                   },\
                 ]
