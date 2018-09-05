62206be22f04506b3c98aa86693fad46d7a00201
Rasmus <rasmusmaide@gmail.com>
2018-09-05 13:47:06 +0300

    #98 added prettyprinter.py

---
+++ estnltk/visualisation/prettyprinter.py
@@ -0,0 +1,88 @@
+def find_spans (text, layer_name):
+    layer = getattr(text, layer_name)
+    spancount = len(layer.spans)
+    spanindexes = []
+    text=text.text
+    attributes = layer.attributes    
+    
+    for s in layer.spans:
+        spanstart = s.start
+        spanend = s.end
+        spanindexes.append(spanstart)
+        spanindexes.append(spanend)
+    
+    html_spans=[]
+    
+    spanindexes = sorted(set(spanindexes))
+    
+    for i in range(len(spanindexes)-1):
+        html_spans.append([spanindexes[i],spanindexes[i+1]])
+            
+    printable=[]
+    textspans=[]
+    
+    for i, html_span in enumerate(html_spans):
+        printable.append([])
+        textspans.append([])
+        for s in layer.spans:
+            if html_span[0] in range(s.start,s.end):
+                textspans[i].append([s.start,s.end])
+                for attribute in attributes:
+                    printable[i].append(getattr(s,attribute))
+
+    assert len(html_spans)==len(printable)
+    
+    for i, attributes in enumerate(printable):
+        if len(attributes)==0:
+            printable.remove(attributes)
+            html_spans.remove(html_spans[i])
+            textspans.remove(textspans[i])
+
+    return html_spans,printable,textspans
+
+def add_spans(text,layer):
+    t = text.text
+    fun_output = find_spans(text,layer)
+    spans = fun_output[0]
+
+    contents_without_attributes = fun_output[1]
+    spans_in_html_spans = fun_output[2]
+    attributes = getattr(text,layer).attributes
+    contents = []
+    
+    for content in contents_without_attributes:
+        new_content=[]
+        for i in range(len(content)):
+            new_content.append(attributes[i%len(attributes)])
+            new_content.append(content[i])
+        contents.append(new_content)
+    
+    tabledata=[]
+    
+    for content in contents_without_attributes:
+        for i in range(len(attributes)):
+            tabledata.append(attributes[i])
+            tabledata.append(content[i])
+    
+    fragments = []
+    last_end = 0
+    
+    for i, (s, e) in enumerate(spans):
+        fragments.append(t[last_end:s])
+        fragments.append('<span class="span')
+        if len(contents[i])/2>len(attributes):
+            fragments.append(" overlapping-span")
+        fragments.append('"; onclick="visualise{0}(')
+        fragments.append(str(contents[i]).replace('"',''))
+        fragments.append(',')
+        fragments.append(str(i))
+        fragments.append(',this.classList,this,')
+        fragments.append(str(spans_in_html_spans[i]))
+        fragments.append(')"; indexes="')
+        fragments.append(str(spans_in_html_spans[i]))
+        fragments.append('">')
+        fragments.append(t[s:e])
+        fragments.append('</span>')
+        last_end = e
+    fragments.append(t[last_end:])
+    return ''.join(fragments)
