9e3f69de79f4313ad1d2151c3a778515a4fb66e7
paul <paul.tammo@gmail.com>
2018-09-20 13:46:21 +0300

    update TextaExporter

--- estnltk/converters/texta_exporter.py
+++ estnltk/converters/texta_exporter.py
@@ -1,3 +1,4 @@
+import pandas
 import requests
 import json
 import os
@@ -6,11 +7,13 @@ from regex import sub
 
 class TextaExporter:
 
-    def __init__(self, index, doc_type, facts_layer=None, fact_attr=None, str_val_attr=None, num_val_attr=None,
-                 textapass='~/.textapass'):
+    def __init__(self, index, doc_type, fact_mapping=None, textapass='~/.textapass'):
         # index is like a schema
 
         # doc_type is like a table name
+
+        # fact_mapping: str (default: None)
+        #   name of a csv file that contains fact mapping instructions
 
         # format of the first line of .textapass file:
         # <hostname>:<port>:<username>:<password>
@@ -29,16 +32,33 @@ class TextaExporter:
 
         self.index = index
         self.doc_type = doc_type
-        self.facts_layer = facts_layer
-        self.fact_attr = fact_attr
-        self.str_val_attr = str_val_attr
-        self.num_val_attr = num_val_attr
+        if fact_mapping is None:
+            self.fact_mapping = None
+        else:
+            self.fact_mapping = self._read_fact_mapping(fact_mapping)
+
+    @staticmethod
+    def _read_fact_mapping(file):
+        df = pandas.read_csv(file)
+        df = df.where((pandas.notnull(df)), None)
+        fact_mapping = list(df.itertuples(index=False))
+        for fm in fact_mapping:
+            assert (fm.fact_attr is None) is not (fm.fact_name is None), (fm.fact_attr, fm.fact_name)
+            assert (fm.value_attr is None) is not (fm.value is None), (fm.value_attr, fm.value)
+            assert fm.fact_type in {'str', 'num'}, fm.fact_type
+        return fact_mapping
+
+    @property
+    def fact_layers(self):
+        if self.fact_mapping is not None:
+            return list({fm.facts_layer for fm in self.fact_mapping})
+        return []
 
     def to_dict(self, text, meta):
         data = {**meta,
                 'text': text.text}
         if 'morph_analysis' in text.layers:
-            # words may contain whitespace (eg '25 000') but there shoud be equal number of words, lemmas and POS
+            # words may contain whitespace (eg '25 000') but there should be equal number of words, lemmas and POS
             words = ' '.join(
                              sub('\s', '_', sp.text)
                              for sp in text.words)
@@ -50,7 +70,7 @@ class TextaExporter:
             data['morph_analysis'] = {'words': words,
                                       'lemmas': lemmas,
                                       'partofspeech': partofspeech}
-        if self.facts_layer is not None:
+        if self.fact_mapping is not None:
             data['texta_facts'] = self.extract_facts(text)
 
         d = {'auth_token': self._auth_token,
@@ -65,31 +85,45 @@ class TextaExporter:
         return requests.post('http://{}:{}/import_api/document_insertion'.format(self.host, self.port), json.dumps(d))
 
     def extract_facts(self, text):
-        facts = []
-        fact_attr = self.fact_attr
-        str_val_attr = self.str_val_attr
-        num_val_attr = self.num_val_attr
-        fact = 'None'
-        str_val = 'None'
-        num_val = None
-        layer = text[self.facts_layer]
-        ambiguous = layer.ambiguous
-        for span in layer:
-            spans = json.dumps(span.base_spans)
-            if ambiguous:
-                annotations = span.annotations
+        for fm in self.fact_mapping:
+
+            fact_attr = fm.fact_attr
+            fact_name = fm.fact_name
+            layer = text[fm.facts_layer]
+
+            str_val_attr = None
+            num_val_attr = None
+            str_val = 'None'
+            num_val = None
+            if fm.value_attr is None:
+                if fm.fact_type == 'str':
+                    str_val = fm.value
+                else:
+                    num_val = fm.value
             else:
-                annotations = [span]
-            for annotation in annotations:
-                if fact_attr is not None:
-                    fact = getattr(annotation, fact_attr)
-                if str_val_attr is not None:
-                    str_val = getattr(annotation, str_val_attr)
-                if num_val_attr is not None:
-                    num_val = getattr(annotation, num_val_attr)
-                facts.append({'doc_path': 'text',
-                              'fact': fact,
-                              'spans': spans,
-                              'str_val': str_val,
-                              'num_val': num_val})
-        return facts
+                if fm.fact_type == 'str':
+                    str_val_attr = fm.value_attr
+                else:
+                    num_val_attr = fm.value_attr
+            ambiguous = layer.ambiguous
+
+            facts = []
+            for span in layer:
+                spans = json.dumps(span.base_spans)
+                if ambiguous:
+                    annotations = span.annotations
+                else:
+                    annotations = [span]
+                for annotation in annotations:
+                    if fact_attr is not None:
+                        fact_name = getattr(annotation, fact_attr)
+                    if str_val_attr is not None:
+                        str_val = getattr(annotation, str_val_attr)
+                    if num_val_attr is not None:
+                        num_val = getattr(annotation, num_val_attr)
+                    facts.append({'doc_path': 'text',
+                                  'fact': fact_name,
+                                  'spans': spans,
+                                  'str_val': str_val,
+                                  'num_val': num_val})
+            return facts
