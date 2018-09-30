59f80fdedbe51819d5a2603eaf240f9ab218421a
Siim Orasmaa <siim.orasmaa@ut.ee>
2018-09-30 17:06:08 +0300

    Added a test für reconstruct_text

--- estnltk/tests/test_tokenizers/test_text_reconstruction.py
+++ estnltk/tests/test_tokenizers/test_text_reconstruction.py
@@ -2,8 +2,11 @@ from estnltk import Text
 from estnltk.corpus_processing.parse_koondkorpus import reconstruct_text
 from estnltk.taggers.text_segmentation.whitespace_tokens_tagger import WhiteSpaceTokensTagger
 
-def test_reconstruct_text():
+
+
+def test_reconstruct_text_detached_layers():
     # Tests that the text and its layers can be reconstructed based on a dictionary representation
+    # Test the situation when reconstructed layers are detached from each other
     # ( this kind of reconstruction is used in importing texts from koondkorpus XML files )
     tokenizer = WhiteSpaceTokensTagger()
     # dict representation of the text
@@ -73,4 +76,52 @@ def test_reconstruct_text():
     assert paragraphs.text == expected_paragraphs
 
 
+
+
+def test_reconstruct_text_enveloping_layers():
+    # Tests that the text and its layers can be reconstructed based on a dictionary representation
+    # Test the situation when reconstructed layers are connected via enveloping
+    # ( this kind of reconstruction is used in importing texts from koondkorpus XML files )
+    tokenizer = WhiteSpaceTokensTagger()
+    # dict representation of the text
+    test_text_dict = { 'paragraphs': [ {'sentences':['Millist hinda oleme nõus maksma enese täiustamise eest?']}, \
+                                       {'sentences':['Inimestel on palju eetilisi muresid, mis seostuvad vaimset '+\
+                                        'võimekust parandavate ravimite või seadmetega, kuid tõenäoliselt haihtuvad '+\
+                                        'need hetkel, mil turule ilmub esimene selline läbimurdeline vahend, tõdeb '+\
+                                        'Oxfordi Ülikooli filosoof Anders Sandberg intervjuus Arko Oleskile.']}, \
+                                       {'sentences':['REKLAAM']},\
+                                       {'sentences':['Kas olete näinud filmi «Kõrvalnähud» («Limitless»), kus peategelane '+\
+                                        'hakkab kasutama ravimit, mis tema vaimseid võimeid tohutult parandab, kuid jääb '+\
+                                        'sellest sõltuvusse...']},\
+                                       {'sentences':['Oo, jaa.', 'Mulle meeldis see väga.']},\
+                                       {'sentences':['Tõesti?']},\
+                                     ] }
+    # Reconstruct the text
+    wstokenizer = WhiteSpaceTokensTagger()
+    text, tokenization_layers = reconstruct_text(test_text_dict, \
+                                                 tokens_tagger=wstokenizer, \
+                                                 use_enveloping_layers=True )
+    assert any([layer.name=='words' for layer in tokenization_layers])
+    assert any([layer.name=='sentences' for layer in tokenization_layers])
+    assert any([layer.name=='paragraphs' for layer in tokenization_layers])
+    
+    # Attach layers
+    for layer in tokenization_layers:
+        text[layer.name] = layer
+    words      = [layer for layer in tokenization_layers if layer.name=='words'][0]
+    sentences  = [layer for layer in tokenization_layers if layer.name=='sentences'][0]
+    paragraphs = [layer for layer in tokenization_layers if layer.name=='paragraphs'][0]
+    
+    # Test relations: paragraphs
+    assert text.paragraphs[0].sentences.text == ['Millist', 'hinda', 'oleme', 'nõus', 'maksma', 'enese', 'täiustamise', 'eest?']
+    assert text.paragraphs[4].sentences.text == ['Oo,', 'jaa.', 'Mulle', 'meeldis', 'see', 'väga.']
+
+    # Test relations: sentences
+    assert text.sentences[1].text == ['Inimestel', 'on', 'palju', 'eetilisi', 'muresid,', 'mis', 'seostuvad', 'vaimset', 'võimekust', 'parandavate', 'ravimite', 'või', 'seadmetega,', 'kuid', 'tõenäoliselt', 'haihtuvad', 'need', 'hetkel,', 'mil', 'turule', 'ilmub', 'esimene', 'selline', 'läbimurdeline', 'vahend,', 'tõdeb', 'Oxfordi', 'Ülikooli', 'filosoof', 'Anders', 'Sandberg', 'intervjuus', 'Arko', 'Oleskile.']
+    assert text.sentences[2].text == ['REKLAAM']
+    
+    # Test relations: words
+    assert text.sentences[3].words[4:6].text == ['«Kõrvalnähud»', '(«Limitless»),']
+    assert text.sentences[5].words[0:3].text == ['Mulle', 'meeldis', 'see']
+    assert text.words[29:33].text == ['esimene', 'selline', 'läbimurdeline', 'vahend,']
 
