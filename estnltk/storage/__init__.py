c16d1f921f64e2f727f1284f48ace73dc7d6f5b9
pault <pault@ut.ee>
2019-01-13 20:24:51 +0200

    renamed: postgres_storage.py -> storage.py

--- estnltk/storage/__init__.py
+++ estnltk/storage/__init__.py
@@ -4,4 +4,4 @@ Module enables to store and search `Text` objects in Postgres database.
 For usage examples, see tutorial `estnltk/tutorials/storing_text_objects_in_postgres.ipynb`
 """
 
-from estnltk.storage.postgres.postgres_storage import PostgresStorage
+from estnltk.storage.postgres.storage import PostgresStorage
