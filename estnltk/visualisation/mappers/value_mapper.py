eb0fc708988622a4f4613a4af4a1b8ebc3d11486
Sven Laur <swen@math.ut.ee>
2019-01-30 01:47:48 +0200

    Comments on value_mappers

--- estnltk/visualisation/mappers/value_mapper.py
+++ estnltk/visualisation/mappers/value_mapper.py
@@ -1,29 +1,76 @@
 # tüüpida ja dokumenteerida
 
+# Rename value_mapper_unique
 def value_mapper_discrete(segment, attribute, value_mapping, default_value, conflict_value) -> str:
-    """Function for applying mappings. Arguments are: segment - the same segment that is passed to the visualisers;
-    attribute - the attribute of the span of that segment (e.g. "lemma"); value_mapping - dictionary that maps the
-    attribute values to the values given to css (e.g. {"kala":"red"}); default_value - value to be returned if
-    the attribute value is not in value_mapping; conflict_value - value to be returned if there is an overlapping
-    span. Returns the value for the css element."""
-    """Does not work for spans with ambigious annotations"""
+    """A function for defining mappings that assign css-attributes for each span in a layer. 
+    
+    Keyword arguments:
+
+    segment        -- text segment to be decorated
+    attribute      -- the attribute of the span of that segment (e.g. "lemma")
+    value_mapping  -- dictionary that maps the attribute value to the value of css attribute (e.g. {"kala":"red"})
+    default_value  -- value to be returned if the attribute value is not in value_mapping
+    conflict_value -- value to be returned if the segment is inside the overlap of several spans 
+
+    Returns the value for the css element.
+
+    Important: Can be used only for decorating spans from non-ambigious layers. If a span has several annotations
+    the mapper does not know how to map list of attribute values to appropriate css attribute value.
+
+    Example:
+    --------
+
+    Define a background colour based on the value of a part-of-speach tag   
+
+    bg_mapper = lambda s: value_mapper_discrete(s, 
+                    attribute='pos', value_mapping={'S' : 'green', 'V': 'yellow', 'O': 'blue'}, 
+                    default_value = 'gray', conflict_value='red') 
+
+    """
+
     if len(segment[1]) != 1:
         return conflict_value
 
     return value_mapping.get(getattr(segment[1][0],attribute), default_value)
 
+
 def value_mapper_ambigious(segment, attribute, value_mapping, default_value, conflict_value) -> str:
-    """Function for applying mappings to ambigious annotations. Arguments are: segment - the same segment that is
-     passed to the visualisers; attribute - the attribute of the span of that segment (e.g. "lemma");
-     value_mapping - dictionary that maps the attribute values to the values given to css (e.g. {"kala":"red"}). Note
-     that the last elements take priority over first elements if the annotation contains more than one value that is in
-     the dictionary and ordered dictionary should be used in that case; default_value - value to be returned if
-     the attribute value is not in value_mapping; conflict_value - value to be returned if there is an overlapping
-     span. Returns the value for the css element."""
+    """A function for defining mappings that assign css-attributes for each span in a layer.
+
+    Keyword arguments:
+    segment        -- text segment to be decorated
+    attribute      -- the attribute of the span of that segment (e.g. "lemma")
+    value_mapping  -- dictionary that maps the attribute value to the value of css attribute and priority (e.g. {"kala": ("red", 1)})
+    default_value  -- value to be returned if the attribute value is not in value_mapping
+    conflict_value -- value to be returned if the segment is inside the overlap of several spans 
+
+    Returns the value for the css element.
+
+    Conclict resolving for ambiguous spans:
+    ---------------------------------------
+
+    If span has several annotations then mapping is applies to all attribute values and the value with highest priority
+    is taken. By convention all priorities are integers and the highest possible priority is 0.   
+
+    Example:
+    --------
+
+    Define a background colour based on the value of a part-of-speach tag   
+
+    bg_mapper = lambda s: value_mapper_discrete(s, 
+                    attribute='pos', value_mapping={'S' : ('green', 1), 'V': ('yellow', 0), 'O': ('blue', 2)}, 
+                    default_value = 'gray', conflict_value='red') 
+
+    """
+
     if len(segment[1]) != 1:
         return conflict_value
 
+    #TODO: Update code to reflect documentation    
     for attr in getattr(segment[1][0],attribute):
         if attr in value_mapping:
             value = value_mapping.get(attr)
     return value or default_value
+
+# There is a way to write function with signature (segment, attribute, value_mapping_func) but this hides only the structure
+# of a segment and thus does not make sense
