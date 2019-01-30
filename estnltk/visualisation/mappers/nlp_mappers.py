a3e081c80c9237c4752ebc2333ff88f9d7c8dfe9
Sven Laur <swen@math.ut.ee>
2019-01-31 01:03:54 +0200

    Added stub for standard NLP mappers

---
+++ estnltk/visualisation/mappers/nlp_mappers.py
@@ -0,0 +1,46 @@
+from typing import Mapping, Any, Tuple, Sequence
+
+
+def word_bg_colour(segment: Tuple[str, Sequence]) -> str:
+	"""
+	Default background colouring for words
+
+	- Shows words that have been normalised in different colour
+	- Shows places where tokenization is non-unique in red
+
+	Ask input from Siim
+
+	"""
+
+
+	# Tokenization conflict
+    if len(segment[1]) != 1:
+        return 'red'	
+
+    return 'blue'
+
+
+def morph_bg_colour(segment: Tuple[str, Sequence]) -> str:
+	"""Default background colouring for Estmorph tag-system
+
+	Ask Dage and Liisi what is the standard colour scheme
+	It might be reasonable to define
+
+	"""
+
+	# Tokenization conflict
+    if len(segment[1]) != 1:
+        return 'red'
+
+    # It might be better to convert it to string     
+    pos_tags = getattr(segment[1][0], 'partofspeech')
+
+    # Ambigous POS tagging
+    if len(pos_tags) > 1:
+    	return 'orange'
+    elif len(pos_tags) ==0:
+    	return 'gray'
+
+	pos_bg_coloring = {'S': 'green', 'V': 'yellow', 'O': 'blue'}
+	return pos_bg_coloring.get(pos_tags[0], 'gray')
+
