f40b32151b3bc60a3ae283d7c754507b302bcad5
Sven Laur <swen@math.ut.ee>
2019-02-05 00:33:17 +0200

    Java checks commented out

--- estnltk/java/javaprocess.py
+++ estnltk/java/javaprocess.py
@@ -133,9 +133,10 @@ class JavaProcess( object ):
         with open(os.devnull, "w") as trash: 
             returncode = subprocess.call('java -version', shell=True, stdout=trash, stderr=trash)
             if returncode != 0:
-                raise Exception('(!) Unable to launch a java process. '+\
-                                'Please make sure that java is installed into the system and '+\
-                                'available via environment variable PATH.')
+                pass
+                # raise Exception('(!) Unable to launch a java process. '+\
+                #                'Please make sure that java is installed into the system and '+\
+                #                'available via environment variable PATH.')
 
 
     def process_line(self, line):
