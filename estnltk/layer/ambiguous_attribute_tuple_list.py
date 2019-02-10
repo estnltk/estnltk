62a57adfcf68c812e2c79071542370df945c813a
pault <pault@ut.ee>
2019-02-10 21:34:37 +0200

    improved tuple representation with to_str function

--- estnltk/layer/ambiguous_attribute_tuple_list.py
+++ estnltk/layer/ambiguous_attribute_tuple_list.py
@@ -7,12 +7,16 @@ from estnltk.layer.span import Span
 
 
 def to_str(value, escape_html=False):
-    if callable(value) and hasattr(value, '__name__') and hasattr(value, '__module__'):
+    if isinstance(value, str):
+        value_str = value
+    elif callable(value) and hasattr(value, '__name__') and hasattr(value, '__module__'):
         value_str = '<function {}.{}>'.format(value.__module__, value.__name__)
     elif isinstance(value, re.Pattern):
         value_str = '<Regex {}>'.format(value.pattern)
     elif isinstance(value, Span):
         value_str = 'Span({})'.format(value.text)
+    elif isinstance(value, tuple):
+        value_str = str(tuple(to_str(v) for v in value))
     else:
         value_str = str(value)
 
