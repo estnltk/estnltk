e28ccdf9aae12ea1b1d0af593b4fec0c61077482
pault <pault@ut.ee>
2019-02-17 12:39:19 +0200

    extracted select_and_join_clause from build_sql_query in pg_operations

--- estnltk/storage/postgres/pg_operations.py
+++ estnltk/storage/postgres/pg_operations.py
@@ -260,6 +260,71 @@ def where_clause(storage,
         return SQL(" AND ").join(sql_parts)
 
 
+def select_and_join_clause(storage,
+                           collection_name,
+                           layer_query: dict = None,
+                           layer_ngram_query: dict = None,
+                           layers: list = None,
+                           collection_meta: list = None):
+
+    collection_identifier = collection_table_identifier(storage, collection_name)
+
+
+    # selected_columns(collection_meta, collection_identifier, layers)
+    collection_meta = collection_meta or []
+
+    selected_columns = [SQL('{}.{}').format(collection_identifier, column_id) for
+                          column_id in map(Identifier, ['id', 'data', *collection_meta])]
+    # col.id, col.data, col.meta_*
+
+    # list columns of selected layers
+    layers = layers or []
+    for layer in chain(layers):
+        selected_columns.append(SQL('{}."id"').format(layer_table_identifier(storage, collection_name, layer)))
+        selected_columns.append(SQL('{}."data"').format(layer_table_identifier(storage, collection_name, layer)))
+    # col__layer1__layer.id, col__layer1__layer.data, ...
+
+
+    # no restrictions to the collection
+    if not layers and layer_query is None and layer_ngram_query is None:
+        # select only text table
+        select = SQL("SELECT {} FROM {}").format(SQL(', ').join(selected_columns), collection_identifier)
+        where_and = SQL('WHERE')
+        return select, where_and
+
+    # we have restrictions and must join tables to meet them
+    # need to join text and all layer tables
+    # selected_layer_tables(...)
+    layer_query = layer_query or {}
+    layer_ngram_query = layer_ngram_query or {}
+
+
+    # selected_tables(layers, layer_query, ngram_query)
+    # find all layers needed for the where clause
+    selected_layers = []
+    for layer in sorted(set(chain(layers, layer_query.keys(), layer_ngram_query.keys()))):
+        layer = SQL("{}").format(layer_table_identifier(storage, collection_name, layer))
+        selected_layers.append(layer)
+    # col__layer1_layer, col_layer2_layer
+
+
+    # join_clause(collection_identifier, selected_layers)
+    join_condition = SQL(" AND ").join(SQL('{}."id" = {}."text_id"').format(
+                         collection_identifier, layer)
+                                       for layer in selected_layers)
+
+
+    # selected_tables_clause()
+    selected_tables = [collection_identifier, *selected_layers]
+    select = SQL('SELECT {columns} FROM {tables} WHERE {join_condition}').format(
+                 columns=SQL(', ').join(selected_columns),
+                 tables=SQL(", ").join(selected_tables),
+                 join_condition=join_condition)
+    where_and = SQL('AND')
+
+    return select, where_and
+
+
 def build_sql_query(storage,
                     collection_name,
                     query=None,
@@ -299,37 +364,12 @@ def build_sql_query(storage,
             print(key, txt)
     """
 
-    collection_identifier = collection_table_identifier(storage, collection_name)
-    collection_meta = collection_meta or []
-    collection_columns = [SQL('{}.{}').format(collection_identifier, column_id) for
-                          column_id in map(Identifier, ['id', 'data', *collection_meta])]
-    if not layers and layer_query is None and layer_ngram_query is None:
-        # select only text table
-        select = SQL("SELECT {} FROM {}").format(SQL(', ').join(collection_columns), collection_identifier)
-        where_and = SQL('WHERE')
-    else:
-        # need to join text and all layer tables
-        layers = layers or []
-        layer_query = layer_query or {}
-        layer_ngram_query = layer_ngram_query or {}
-
-        layer_columns = []
-        for layer in chain(layers):
-            layer_columns.append(SQL('{}."id"').format(layer_table_identifier(storage, collection_name, layer)))
-            layer_columns.append(SQL('{}."data"').format(layer_table_identifier(storage, collection_name, layer)))
-
-        layers_join = []
-        for layer in sorted(set(chain(layers, layer_query.keys(), layer_ngram_query.keys()))):
-            layer = SQL("{}").format(layer_table_identifier(storage, collection_name, layer))
-            layers_join.append(layer)
-
-        select = SQL('SELECT {columns} FROM {tables} WHERE {condition}').format(
-                     columns=SQL(', ').join(collection_columns + layer_columns),
-                     tables=SQL(", ").join([collection_identifier, *layers_join]),
-                     condition=SQL(" AND ").join(SQL('{}."id" = {}."text_id"').format(
-                                                     collection_identifier, layer)
-                                                 for layer in layers_join))
-        where_and = SQL('AND')
+    select_and_join, where_and = select_and_join_clause(storage,
+                                                        collection_name,
+                                                        layer_query=layer_query,
+                                                        layer_ngram_query=layer_ngram_query,
+                                                        layers=layers,
+                                                        collection_meta=collection_meta)
 
     where = where_clause(storage=storage,
                          collection_name=collection_name,
@@ -339,9 +379,9 @@ def build_sql_query(storage,
                          keys=keys,
                          missing_layer=missing_layer)
     if where:
-        sql_parts = [select, where_and, where]
+        sql_parts = [select_and_join, where_and, where]
     else:
-        sql_parts = [select]
+        sql_parts = [select_and_join]
 
     if order_by_key is True:
         sql_parts.append(SQL('ORDER BY "id"'))
