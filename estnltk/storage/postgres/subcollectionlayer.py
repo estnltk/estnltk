4675883e3e7d96915c69a01f5803501d7d6cf04e
pault <pault@ut.ee>
2019-02-28 11:34:49 +0200

    some formal fixes in PgSubCollectionLayer

--- estnltk/storage/postgres/subcollectionlayer.py
+++ estnltk/storage/postgres/subcollectionlayer.py
@@ -1,5 +1,3 @@
-from typing import Sequence
-from tqdm import tqdm, tqdm_notebook
 from psycopg2.sql import SQL
 
 from estnltk import logger
@@ -34,9 +32,8 @@ class PgSubCollectionLayer:
         """
         """
 
-        #TODO: Make sure that all objects used by the class are independent copies and cannot be 
-        #changed form the outside. This might invalidate invariants 
-
+        # TODO: Make sure that all objects used by the class are independent copies and cannot be
+        #       changed form outside. This might invalidate invariants
 
         if not collection.exists():
             raise pg.PgCollectionException('collection {!r} does not exist'.format(collection.name))
@@ -50,9 +47,12 @@ class PgSubCollectionLayer:
         else:
             raise TypeError('unexpected type of selection_criterion: {!r}'.format(type(selection_criterion)))
 
-        #TODO: Complete code 
-        #Raise error if the layer is not detachable? 
+        self.detached_layer = detached_layer
+        self.progressbar = progressbar
+        self.return_index = return_index
 
+        # TODO: Complete code
+        #  Raise error if the layer is not detachable?
 
     @property
     def sql_query(self):
@@ -108,13 +108,12 @@ class PgSubCollectionLayer:
 
     @property
     def sql_count_query(self):
-        #TODO: Do not stress SQL analyzer write a flat query for it
+        # TODO: Do not stress SQL analyzer write a flat query for it
         return SQL('SELECT count(*) FROM ({}) AS a').format(self.sql_query)
 
     @property
     def sql_count_query_text(self):
         return self.sql_count_query.as_string(self.collection.storage.conn)
-
 
     def select(self, additional_constraint: pg.WhereClause = None):
         """
@@ -123,18 +122,14 @@ class PgSubCollectionLayer:
         TODO: Document its usages
         """
 
-
         if additional_constraint is None:
             return self
 
-        #TODO: Correct code    
         return PgSubCollectionLayer(collection=self.collection,
-                               selection_criterion=self._selection_criterion & additional_constraint,
-                               selected_layers=selected_layers.copy(),
-                               meta_attributes=self.meta_attributes,
-                               progressbar=self.progressbar,
-                               return_index=self.return_index
-                               )
+                                    selection_criterion=self._selection_criterion & additional_constraint,
+                                    detached_layer=self.detached_layer,
+                                    progressbar=self.progressbar,
+                                    return_index=self.return_index)
 
     def __iter__(self):
         """
@@ -149,7 +144,7 @@ class PgSubCollectionLayer:
         The value of these configuration attributes is fixed before starting the iteration.
         """
 
-        #TODO: Correct code 
+        # TODO: Correct code
         # Check that somebody else has not deleted the collection
         if not self.collection.exists():
             raise pg.PgCollectionException('collection {!r} has been unexpectedly deleted'.format(self.collection.name))
@@ -162,7 +157,7 @@ class PgSubCollectionLayer:
         with self.collection.storage.conn.cursor('read', withhold=True) as c:
             c.execute(self.sql_query)
             logger.debug(c.query.decode())
-            data_iterator = Progressbar(cursor=c, total=total, initial=0, progressbar_type=self.progressbar)
+            data_iterator = pg.Progressbar(iterable=c, total=total, initial=0, progressbar_type=self.progressbar)
 
             # Cash configuration attributes to protect against unexpected changes during iteration
             return_index = self.return_index
@@ -200,35 +195,3 @@ class PgSubCollectionLayer:
                         yield text_id, text
                     else:
                         yield text
-
-
-#TODO: Push it out and reuse for other classes
-class Progressbar:
-    def __init__(self, cursor, total, initial, progressbar_type):
-        self.progressbar_type = progressbar_type
-
-        if progressbar_type is None:
-            self.data_iterator = cursor
-        elif progressbar_type in {'ascii', 'unicode'}:
-            self.data_iterator = tqdm(cursor,
-                                      total=total,
-                                      initial=initial,
-                                      unit='doc',
-                                      ascii=progressbar_type == 'ascii',
-                                      smoothing=0)
-        elif progressbar_type == 'notebook':
-            self.data_iterator = tqdm_notebook(cursor,
-                                               total=total,
-                                               initial=initial,
-                                               unit='doc',
-                                               smoothing=0)
-        else:
-            raise ValueError("unknown progressbar type: {!r}, expected None, 'ascii', 'unicode' or 'notebook'"
-                             .format(progressbar_type))
-
-    def set_description(self, description, refresh=False):
-        if self.progressbar_type is not None:
-            self.data_iterator.set_description(description, refresh=refresh)
-
-    def __iter__(self):
-        yield from self.data_iterator
