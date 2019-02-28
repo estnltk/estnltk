79ddc0465270ae5d86407a8433fea2f3749c7afa
pault <pault@ut.ee>
2019-02-28 07:23:39 +0200

    moved sql_table_naming.py -> sql_composition/sql_table_naming.py

--- estnltk/storage/postgres/__init__.py
+++ estnltk/storage/postgres/__init__.py
@@ -30,10 +30,10 @@ Example usage:
         print(key, txt)
 
 """
-from .sql_table_naming import collection_table_name
-from .sql_table_naming import structure_table_name
-from .sql_table_naming import layer_table_name
-from .sql_table_naming import fragment_table_name
+from estnltk.storage.postgres.sql_composition.sql_table_naming import collection_table_name
+from estnltk.storage.postgres.sql_composition.sql_table_naming import structure_table_name
+from estnltk.storage.postgres.sql_composition.sql_table_naming import layer_table_name
+from estnltk.storage.postgres.sql_composition.sql_table_naming import fragment_table_name
 
 from .pg_operations import collection_table_identifier
 from .pg_operations import structure_table_identifier
