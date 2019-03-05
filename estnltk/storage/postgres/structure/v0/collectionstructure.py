cab4ac9a4dd956053180668ae7e5b1c7f3204360
pault <pault@ut.ee>
2019-03-05 14:07:39 +0200

    CollectionStructureBase class

--- estnltk/storage/postgres/structure/v0/collectionstructure.py
+++ estnltk/storage/postgres/structure/v0/collectionstructure.py
@@ -7,37 +7,7 @@ from estnltk.storage import postgres as pg
 __version__ = '0'
 
 
-class CollectionStructure:
-    def __init__(self, collection):
-        self.collection = collection
-
-        self._structure = None
-        self._modified = True
-        self.load()
-
-    def __bool__(self):
-        return bool(self.structure)
-
-    def __iter__(self):
-        yield from self.structure
-
-    def __contains__(self, item):
-        return item in self.structure
-
-    def __getitem__(self, item):
-        return self.structure[item]
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.structure == other.structure
-        return False
-
-    @property
-    def structure(self):
-        if self._modified:
-            self._structure = self.load()
-            self._modified = False
-        return self._structure
+class CollectionStructure(pg.CollectionStructureBase):
 
     def create_table(self):
         storage = self.collection.storage
