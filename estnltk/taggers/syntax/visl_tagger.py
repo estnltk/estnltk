533930973489e8757ea37c8a31496db1ef39b6dc
pault <pault@ut.ee>
2019-03-13 09:53:22 +0200

    VislTagger

--- estnltk/taggers/syntax/visl_tagger.py
+++ estnltk/taggers/syntax/visl_tagger.py
@@ -1,20 +1,20 @@
 from estnltk.taggers import Tagger
 from estnltk.layer.layer import Layer
 from estnltk.converters.CG3_exporter import export_CG3
-from estnltk.taggers.syntax.vislcg3_syntax import VISLCG3Pipeline, convert_cg3_to_conll
+from estnltk.taggers.syntax.vislcg3_syntax import VISLCG3Pipeline
 from estnltk import PACKAGE_PATH
 import os
 
 
 class VislTagger(Tagger):
-    """Visl"""
+    """Visl tagger"""
 
     conf_param = ['_visl_line_processor']
 
     def __init__(self, output_layer: str = 'visl', morph_extended_layer: str = 'morph_extended'):
         self.input_layers = [morph_extended_layer]
         self.output_layer = output_layer
-        self.output_attributes = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
+        self.output_attributes = ['visl']
 
         vislcgRulesDir = os.path.relpath(os.path.join(PACKAGE_PATH, 'taggers', 'syntax', 'files'))
         vislcg_path = '/usr/bin/vislcg3'
@@ -27,20 +27,21 @@ class VislTagger(Tagger):
         layer = Layer(name=self.output_layer, text_object=text, attributes=self.output_attributes,
                       parent=morph_extended_layer._base, ambiguous=True)
 
-        results2 = self._visl_line_processor(export_CG3(text))
-        visl_lines = filter(None, convert_cg3_to_conll(results2.split('\n')))
+        visl_output = self._visl_line_processor(export_CG3(text))
 
-        for visl_line, span in zip(visl_lines, morph_extended_layer):
-            values = visl_line.split('\t')
-            layer.add_annotation(span,
-                                 id=values[0],
-                                 form=values[1],
-                                 lemma=values[2],
-                                 upostag=values[3],
-                                 xpostag=values[4],
-                                 feats=values[5],
-                                 head=values[6],
-                                 deprel=values[7],
-                                 deps=values[8],
-                                 misc=values[9])
+        visl_lines = []
+        token_in_progress = False
+        for line in visl_output.split('\n'):
+            if line and line[0] == '\t':
+                if token_in_progress:
+                    visl_lines[-1].append(line.strip())
+                else:
+                    visl_lines.append([line.strip()])
+                    token_in_progress = True
+            else:
+                token_in_progress = False
+
+        for token_lines, span in zip(visl_lines, morph_extended_layer):
+            for token_line in token_lines:
+                layer.add_annotation(span, visl=token_line)
         return layer
