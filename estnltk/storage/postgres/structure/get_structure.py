96c2f21127fd7a633f9827f492ff196205eafc78
pault <pault@ut.ee>
2019-03-14 23:40:16 +0200

    collection structure v20

---
+++ estnltk/storage/postgres/structure/get_structure.py
@@ -0,0 +1,12 @@
+from estnltk.storage import postgres as pg
+
+
+def get_structure(collection, version):
+    if version == '00':
+        return pg.v00.CollectionStructure(collection)
+    elif version == '1.0':
+        return pg.v10.CollectionStructure(collection)
+    elif version == '2.0':
+        return pg.v20.CollectionStructure(collection)
+    else:
+        raise ValueError("version must be '0.0', '1.0' or '2.0'")
--- estnltk/storage/postgres/structure/v20/collectionstructure.py
+++ estnltk/storage/postgres/structure/v20/collectionstructure.py
@@ -4,7 +4,7 @@ from estnltk.logger import logger
 from estnltk.storage import postgres as pg
 
 
-__version__ = '0.0'
+__version__ = '2.0'
 
 
 class CollectionStructure(pg.CollectionStructureBase):
@@ -16,34 +16,34 @@ class CollectionStructure(pg.CollectionStructureBase):
         with storage.conn.cursor() as c:
             c.execute(SQL('CREATE {temporary} TABLE {table} ('
                           'layer_name text primary key, '
-                          'detached bool not null, '
                           'attributes text[] not null, '
                           'ambiguous bool not null, '
                           'parent text, '
                           'enveloping text, '
                           '_base text, '
-                          'meta text[]);').format(temporary=temporary,
+                          'meta text[], '
+                          'layer_type text not null, '
+                          'loader text);').format(temporary=temporary,
                                                   table=table))
             logger.debug(c.query.decode())
 
     def insert(self, layer, layer_type: str, meta: dict = None, loader: str = None):
         self._modified = True
 
-        detached = layer_type in {'detached', 'fragmented'}
         meta = list(meta or [])
-
         with self.collection.storage.conn.cursor() as c:
-            c.execute(SQL("INSERT INTO {} (layer_name, detached, attributes, ambiguous, parent, enveloping, _base, meta) "
-                          "VALUES ({}, {}, {}, {}, {}, {}, {}, {});").format(
+            c.execute(SQL("INSERT INTO {} (layer_name, attributes, ambiguous, parent, enveloping, _base, meta, layer_type, loader) "
+                          "VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {});").format(
                 pg.structure_table_identifier(self.collection.storage, self.collection.name),
                 Literal(layer.name),
-                Literal(detached),
                 Literal(list(layer.attributes)),
                 Literal(layer.ambiguous),
                 Literal(layer.parent),
                 Literal(layer.enveloping),
                 Literal(layer._base),
-                Literal(meta)
+                Literal(meta),
+                Literal(layer_type),
+                Literal('')
             )
             )
 
@@ -52,17 +52,18 @@ class CollectionStructure(pg.CollectionStructureBase):
             return None
         structure = {}
         with self.collection.storage.conn.cursor() as c:
-            c.execute(SQL("SELECT layer_name, detached, attributes, ambiguous, parent, enveloping, _base, meta "
+            c.execute(SQL("SELECT layer_name, attributes, ambiguous, parent, enveloping, _base, meta, layer_type, loader "
                           "FROM {};").
                       format(pg.structure_table_identifier(self.collection.storage, self.collection.name)))
 
             for row in c.fetchall():
-                structure[row[0]] = {'attributes': tuple(row[2]),
-                                     'ambiguous': row[3],
-                                     'parent': row[4],
-                                     'enveloping': row[5],
-                                     '_base': row[6],
-                                     'meta': row[7],
-                                     'layer_type': 'detached' if row[1] else 'attached',
-                                     'loader': None}
+                structure[row[0]] = {'attributes': tuple(row[1]),
+                                     'ambiguous': row[2],
+                                     'parent': row[3],
+                                     'enveloping': row[4],
+                                     '_base': row[5],
+                                     'meta': row[6],
+                                     'layer_type': row[7],
+                                     'loader': row[8]
+                                     }
         return structure
