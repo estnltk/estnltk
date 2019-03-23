b3316779da51217a660b031786aa5288be1d89ff
pault <pault@ut.ee>
2019-03-23 14:38:21 +0200

    added __setattr__, __getattr__, __delattr__ methods and __slots__, _attributes attributes for Annotation; tested

--- estnltk/layer/annotation.py
+++ estnltk/layer/annotation.py
@@ -1,75 +1,98 @@
-from typing import MutableMapping, Any, Sequence
+from typing import Any, Sequence, Mapping
 
 
 class Annotation:
-    def __init__(self, span, **kwargs):
+    __slots__ = ['_attributes', '_span']
+
+    def __init__(self, span=None, **attributes):
+        self._attributes = attributes
         self._span = span
-        for key, value in kwargs.items():
-            setattr(self, key, value)
 
-    @property
-    def legal_attribute_names(self) -> Sequence[str]:
-        return self.layer.attributes
-
-    def to_record(self, with_text=False) -> MutableMapping[str, Any]:
-        return {
-        **{k: self.__getattribute__(k) for k in list(self.legal_attribute_names) + (['text'] if with_text else [])},
-            **{'start': self.start, 'end': self.end}}
+    def to_record(self, with_text=False) -> Mapping[str, Any]:
+        return {**{k: getattr(self, k) for k in list(self.legal_attribute_names) + (['text'] if with_text else [])},
+                **{'start': self.start, 'end': self.end}}
 
     def attributes(self):
-        return {attr: self.__getattribute__(attr) for attr in self.legal_attribute_names}
-
-    @property
-    def parent(self):
-        return self._span.parent
-
-    @property
-    def start(self) -> int:
-        return self._span.start
-
-    @property
-    def end(self) -> int:
-        return self._span.end
+        return {attr: getattr(self, attr) for attr in self.legal_attribute_names}
 
     @property
     def span(self):
         return self._span
 
     @property
+    def start(self) -> int:
+        if self._span:
+            return self._span.start
+
+    @property
+    def end(self) -> int:
+        if self._span:
+            return self._span.end
+
+    @property
     def layer(self):
-        return self._span.layer
+        if self._span:
+            return self._span.layer
+
+    @property
+    def legal_attribute_names(self) -> Sequence[str]:
+        if self.layer is not None:
+            return self.layer.attributes
+
+    @property
+    def text_object(self):
+        if self._span:
+            return self._span.text_object
 
     @property
     def text(self):
-        if self.span.layer.text_object:
-            return self.layer.text_object.text[self.start:self.end]
+        if self._span:
+            return self._span.text
 
-    # TODO: remove
-    @property
-    def text_object(self):
-        return self.layer.text_object
+    def __setattr__(self, key, value):
+        if key in self.__slots__:
+            super().__setattr__(key, value)
+        elif key == 'span':
+            if self._span is None:
+                super().__setattr__('_span', value)
+            else:
+                raise AttributeError('this Annotation object already has a span')
+        else:
+            self._attributes[key] = value
 
     def __getattr__(self, item):
-        if item in {'start', 'end', 'layer', 'text', 'spans'}:
-            return self.__getattribute__(item)
-
         if item in {'__getstate__', '__setstate__'}:
             raise AttributeError
 
-        if item in self.__getattribute__('legal_attribute_names'):
-            try:
-                return self.__getattribute__(item)
-            except AttributeError:
-                return None
+        attributes = self._attributes
+        if item in attributes:
+            value = attributes[item]
+            if isinstance(value, str) and value.startswith('lambda a: '):
+                return eval(value)(self)
+            return value
 
+        # item == '__deepcopy__'
         return self.__getattribute__('__class__').__getattribute__(self, item)
+
+    def __delattr__(self, item):
+        attributes = self._attributes
+        if item in attributes:
+            del attributes[item]
+        else:
+            raise AttributeError(item)
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Annotation):
             return False
-        if self.legal_attribute_names != other.legal_attribute_names:
+        if self.legal_attribute_names is None or other.legal_attribute_names is None:
+            if set(self._attributes) == set(other._attributes):
+                return all(getattr(self, i) == getattr(other, i) for i in self._attributes)
             return False
-        return all(self.__getattribute__(i) == other.__getattribute__(i) for i in self.legal_attribute_names)
+
+        if set(self.legal_attribute_names) == set(other.legal_attribute_names):
+            return all(getattr(self, i) == getattr(other, i) for i in self.legal_attribute_names)
+
+        return False
 
     def __hash__(self):
         return hash((self.start, self.end))
@@ -77,17 +100,21 @@ class Annotation:
     def __str__(self):
         # Output key-value pairs in a sorted way
         # (to assure a consistent output, e.g. for automated testing)
+        if self.legal_attribute_names is None:
+            attribute_names = sorted(self._attributes)
+        else:
+            attribute_names = self.legal_attribute_names
+
         mapping_sorted = []
 
-        for k in sorted(self.legal_attribute_names):
-            key_value_str = "{key_val}".format(key_val = {k:self.__getattribute__(k)})
+        for k in sorted(attribute_names):
+            key_value_str = "{key_val}".format(key_val={k: getattr(self, k)})
             # Hack: Remove surrounding '{' and '}'
-            key_value_str = key_value_str[:-1]
-            key_value_str = key_value_str[1:]
+            key_value_str = key_value_str[1:-1]
             mapping_sorted.append(key_value_str)
 
         # Hack: Put back surrounding '{' and '}' (mimic dict's representation)
-        mapping_sorted_str = '{'+ (', '.join(mapping_sorted)) + '}'
+        mapping_sorted_str = '{' + (', '.join(mapping_sorted)) + '}'
         return 'Annotation({text}, {attributes})'.format(text=self.text, attributes=mapping_sorted_str)
 
     def __repr__(self):
