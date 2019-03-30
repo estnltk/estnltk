475a467361e27188bc3dfbd9ee3afeccbf01b2eb
pault <pault@ut.ee>
2019-03-30 23:11:33 +0200

    SyntaxDependencyRetagger now also works on ambiguous syntax layers

--- estnltk/taggers/syntax/syntax_dependency_retagger.py
+++ estnltk/taggers/syntax/syntax_dependency_retagger.py
@@ -6,7 +6,7 @@ from estnltk.taggers.retagger import Retagger
 
 
 class SyntaxDependencyRetagger(Retagger):
-    """Adds 'parent_span', 'children' and 'parent_deprel' attributes to conll syntax layer."""
+    """Adds 'parent_span', 'children' and 'parent_deprel' attributes to syntax layer."""
     conf_param = []
 
     def __init__(self, conll_syntax_layer='conll_syntax'):
@@ -26,27 +26,28 @@ class SyntaxDependencyRetagger(Retagger):
         id_to_children = defaultdict(list)
 
         for span in layer:
-            span.parent_span = None
-            span.children = ()
-            span.parent_deprel = None
-            if span.id == 1:
+            span[0].parent_span = None
+            span[0].children = ()
+            span[0].parent_deprel = None
+            if span[0].id == 1:
                 annotate_spans(id_to_span, id_to_children)
                 id_to_span = {}
                 id_to_children = defaultdict(list)
 
-            id_to_span[span.id] = span
-            id_to_children[span.head].append(span)
+            id_to_span[span[0].id] = span
+            id_to_children[span[0].head].append(span)
 
         annotate_spans(id_to_span, id_to_children)
 
 
 def annotate_spans(id_to_span, id_to_children):
-    for id_, sp in id_to_span.items():
-        sp.parent_span = id_to_span.get(sp.head)
+    for id_, span in id_to_span.items():
+        for annotation in span:
+            annotation.parent_span = id_to_span.get(annotation.head)
 
-        sp.children = tuple(id_to_children[id_])
-        if sp.parent_span is None:
-            sp.parent_deprel = None
-        else:
-            sp.parent_deprel = sp.parent_span.deprel
+            annotation.children = tuple(id_to_children[id_])
+            if annotation.parent_span is None:
+                annotation.parent_deprel = None
+            else:
+                annotation.parent_deprel = annotation.parent_span[0].deprel
 
