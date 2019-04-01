a972197cb65aa5f05aebba9e496ffd8476a61508
pault <pault@ut.ee>
2019-04-01 13:18:47 +0300

    removed parent_deprel attribute from SyntaxDependencyTagger output; assert that in case of ambiguous syntax layer every span has exactly one id and head

--- estnltk/taggers/syntax/syntax_dependency_retagger.py
+++ estnltk/taggers/syntax/syntax_dependency_retagger.py
@@ -6,7 +6,7 @@ from estnltk.taggers.retagger import Retagger
 
 
 class SyntaxDependencyRetagger(Retagger):
-    """Adds 'parent_span', 'children' and 'parent_deprel' attributes to syntax layer."""
+    """Adds 'parent_span' and 'children' attributes to syntax layer."""
     conf_param = []
 
     def __init__(self, conll_syntax_layer='conll_syntax'):
@@ -18,7 +18,7 @@ class SyntaxDependencyRetagger(Retagger):
         layer = layers[self.output_layer]
 
         attributes = list(layer.attributes)
-        attributes.extend(attr for attr in ['parent_span', 'children', 'parent_deprel']
+        attributes.extend(attr for attr in ['parent_span', 'children']
                                    if attr not in layer.attributes)
         layer.attributes = tuple(attributes)
 
@@ -26,16 +26,18 @@ class SyntaxDependencyRetagger(Retagger):
         id_to_children = defaultdict(list)
 
         for span in layer:
-            span[0].parent_span = None
-            span[0].children = ()
-            span[0].parent_deprel = None
-            if span[0].id == 1:
+            id_ = span[0].id
+            head = span[0].head
+            assert all(annotation.id == id_ for annotation in span.annotations)
+            assert all(annotation.head == head for annotation in span.annotations)
+
+            if id_ == 1:
                 annotate_spans(id_to_span, id_to_children)
                 id_to_span = {}
                 id_to_children = defaultdict(list)
 
-            id_to_span[span[0].id] = span
-            id_to_children[span[0].head].append(span)
+            id_to_span[id_] = span
+            id_to_children[head].append(span)
 
         annotate_spans(id_to_span, id_to_children)
 
@@ -44,10 +46,6 @@ def annotate_spans(id_to_span, id_to_children):
     for id_, span in id_to_span.items():
         for annotation in span:
             annotation.parent_span = id_to_span.get(annotation.head)
-
             annotation.children = tuple(id_to_children[id_])
-            if annotation.parent_span is None:
-                annotation.parent_deprel = None
-            else:
-                annotation.parent_deprel = annotation.parent_span[0].deprel
+
 
