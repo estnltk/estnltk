bd314120a3b6510c8c285d136d92854d30bb3ab1
Siim Orasmaa <siim.orasmaa@ut.ee>
2019-05-14 14:52:09 +0300

    Roll back to previous .travis.yml (removed Windows build)

--- conda-recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-"%PYTHON%" setup.py build
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
--- conda-recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CC=${PREFIX}/bin/g++
-CXX=${PREFIX}/bin/g++
-$PYTHON setup.py build
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
