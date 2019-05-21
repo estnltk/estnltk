6cfbeabffc65f6efe5c73169fb93ece431ce18d5
pault <pault@ut.ee>
2019-05-21 15:41:09 +0300

    update and test layer flattening

--- estnltk/layer_operations/flatten.py
+++ estnltk/layer_operations/flatten.py
@@ -1,20 +1,19 @@
 from typing import Sequence
-from estnltk.layer.span import Span
+from estnltk.layer.base_span import ElementaryBaseSpan
 from estnltk.layer.layer import Layer
 
 
-def flatten(input_layer: Layer, output_layer: str, output_attributes: Sequence[str]=None,
-            attribute_mapping: Sequence=None, default_values: dict = None) -> Layer:
+def flatten(input_layer: Layer, output_layer: str, output_attributes: Sequence[str] = None,
+            attribute_mapping: Sequence = None, default_values: dict = None) -> Layer:
     """Reduces enveloping layer or layer with parent to a detached ambiguous layer of simple text spans.
 
     """
     layer_attributes = input_layer.attributes
 
     output_attributes = output_attributes or layer_attributes
-    text_object = input_layer.text_object
     new_layer = Layer(name=output_layer,
                       attributes=output_attributes,
-                      text_object=text_object,
+                      text_object=input_layer.text_object,
                       parent=None,
                       enveloping=None,
                       ambiguous=True,
@@ -27,15 +26,9 @@ def flatten(input_layer: Layer, output_layer: str, output_attributes: Sequence[s
         assert {attr for attr, _ in attribute_mapping} <= set(layer_attributes)
         assert {attr for _, attr in attribute_mapping} <= set(output_attributes)
 
-    if input_layer.ambiguous:
-        for span in input_layer:
-            new_span = Span(span.start, span.end, text_object=text_object)
-            for a in span:
-                attrs = {new_attr: getattr(a, old_attr) for old_attr, new_attr, in attribute_mapping}
-                new_layer.add_annotation(new_span, **attrs)
-    else:
-        for span in input_layer:
-            attrs = {new_attr: getattr(span, old_attr) for old_attr, new_attr in attribute_mapping}
-            new_layer.add_annotation((span.start, span.end), **attrs)
+    for span in input_layer:
+        for annotation in span.annotations:
+            attrs = {new_attr: getattr(annotation, old_attr) for old_attr, new_attr, in attribute_mapping}
+            new_layer.add_annotation(ElementaryBaseSpan(span.start, span.end), **attrs)
 
     return new_layer
