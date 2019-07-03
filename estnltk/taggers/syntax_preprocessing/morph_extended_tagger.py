b3238e556dc27c276f514485b1151140ba644f1a
pault <pault@ut.ee>
2019-07-03 23:16:58 +0300

    TaggerOld -> Tagger in MorphExtendedTagger

--- estnltk/taggers/syntax_preprocessing/morph_extended_tagger.py
+++ estnltk/taggers/syntax_preprocessing/morph_extended_tagger.py
@@ -1,4 +1,4 @@
-from estnltk.taggers import TaggerOld
+from estnltk.taggers import Tagger
 from estnltk.rewriting import PunctuationTypeRewriter
 from estnltk.rewriting import MorphToSyntaxMorphRewriter
 from estnltk.rewriting import PronounTypeRewriter
@@ -14,24 +14,30 @@ from estnltk import PACKAGE_PATH
 import os
 
 
-class MorphExtendedTagger(TaggerOld):
-    """
-    Tags text object with morph_extended layer. In order to do so executes
-    consecutively several syntax preprocessing rewriters.
+class MorphExtendedTagger(Tagger):
+    """Creates morph_extended layer which contains morph analysis attributes and syntax preprocessing attributes.
+    In order to do so executes consecutively several syntax preprocessing rewriters.
 
     (Text object with morph_extended layer can be converted to VISL CG3 input
     format by export_CG3 method.)
+
     """
-    description = "Extends 'morph_analysis' layer with syntax preprocessing attributes."
-    layer_name = 'morph_extended'
-    attributes = VabamorfTagger.output_attributes + ('punctuation_type',
-                                              'pronoun_type',
-                                              'letter_case',
-                                              'fin',
-                                              'verb_extension_suffix',
-                                              'subcat')
-    depends_on = ['morph_analysis']
-    configuration = None
+    input_layers = ['morph_analysis']
+    output_layer = 'morph_extended'
+    output_attributes = VabamorfTagger.output_attributes + ('punctuation_type',
+                                                            'pronoun_type',
+                                                            'letter_case',
+                                                            'fin',
+                                                            'verb_extension_suffix',
+                                                            'subcat')
+
+    conf_param = ['fs_to_synt_rules_file', 'allow_to_remove_all', 'subcat_rules_file',
+                  'quick_morph_extended_rewriter']
+
+    # TODO: remove next three lines
+    layer_name = output_layer
+    attributes = output_attributes
+    depends_on = input_layers
 
     def __init__(self,
                  fs_to_synt_rules_file=None,
@@ -57,17 +63,19 @@ class MorphExtendedTagger(TaggerOld):
                 The original implementation allowed this, but we are now restricting it
                 in order to avoid words without any analyses;
                 Default: False
-        """
 
+        """
         if fs_to_synt_rules_file is None:
             fs_to_synt_rules_file = os.path.relpath(os.path.join(PACKAGE_PATH,
                 'rewriting/syntax_preprocessing/rules_files/tmorftrtabel.txt'))
         if subcat_rules_file is None:
             subcat_rules_file = os.path.relpath(os.path.join(PACKAGE_PATH,
                 'rewriting/syntax_preprocessing/rules_files/abileksikon06utf.lx'))
-        self.configuration = {'fs_to_synt_rules_file': fs_to_synt_rules_file,
-                              'allow_to_remove_all': allow_to_remove_all,
-                              'subcat_rules_file': subcat_rules_file}
+
+        self.fs_to_synt_rules_file = fs_to_synt_rules_file
+        self.allow_to_remove_all = allow_to_remove_all
+        self.subcat_rules_file = subcat_rules_file
+
 
         punctuation_type_rewriter = PunctuationTypeRewriter()
         morph_to_syntax_morph_rewriter = MorphToSyntaxMorphRewriter(fs_to_synt_rules_file)
@@ -90,22 +98,16 @@ class MorphExtendedTagger(TaggerOld):
                     verb_extension_suffix_rewriter=verb_extension_suffix_rewriter,
                     subcat_rewriter=subcat_rewriter)
 
-    @staticmethod
-    def _esc_double_quotes(str1):
-        ''' Escapes double quotes.
-        '''
-        return str1.replace('"', '\\"').replace('\\\\\\"', '\\"').replace('\\\\"', '\\"')
-
-    def tag(self, text, return_layer=False):
+    def _make_layer(self, text, layers, status=None):
         source_attributes = text['morph_analysis'].attributes + ('text',)
 
-        new_layer = text['morph_analysis'].rewrite(
-                                    source_attributes=source_attributes,
-                                    target_attributes=self.attributes,
-                                    rules=self.quick_morph_extended_rewriter,
-                                    name=self.layer_name,
-                                    ambiguous = True
-                                    )
-        if return_layer:
-            return new_layer
-        text[self.layer_name] = new_layer
+        layer = layers['morph_analysis'].rewrite(
+                source_attributes=source_attributes,
+                target_attributes=self.attributes,
+                rules=self.quick_morph_extended_rewriter,
+                name=self.layer_name,
+                ambiguous=True
+        )
+        layer.text_object = text
+
+        return layer
