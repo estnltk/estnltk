c56859c7991094b8213d33abc16c210729a86a1d
pault <pault@ut.ee>
2019-08-03 09:06:33 +0300

    refactored disambiguating_tagger

--- estnltk/taggers/standard_taggers/disambiguating_tagger.py
+++ estnltk/taggers/standard_taggers/disambiguating_tagger.py
@@ -1,6 +1,3 @@
-from estnltk.layer.base_span import EnvelopingBaseSpan
-from estnltk.layer.span import Span
-from estnltk.layer.enveloping_span import EnvelopingSpan
 from estnltk.layer.layer import Layer
 from estnltk.taggers import Tagger
 
@@ -37,20 +34,9 @@ class DisambiguatingTagger(Tagger):
                       parent=parent,
                       enveloping=enveloping,
                       ambiguous=False)
+
+        decorator = self.decorator
         for input_span in input_layer:
-            if parent:
-                # TODO: test and rewrite using add_annotation
-                span = Span(parent=parent,
-                            layer=layer)
-                for k, v in self.decorator(input_span, text.text).items():
-                    setattr(span[0], k, v)
-                layer.add_span(span)
-            elif enveloping:
-                span = EnvelopingSpan(base_span=input_span.base_span, layer=layer, spans=input_span.spans)
-                for k, v in self.decorator(input_span, text.text).items():
-                    setattr(span, k, v)
-                layer.add_span(span)
-            else:
-                layer.add_annotation((input_span.start, input_span.end), **self.decorator(input_span, text.text))
+            layer.add_annotation(input_span.base_span, **decorator(input_span, text.text))
 
         return layer
