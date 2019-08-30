623a92c6e6ed22dd846a7f01f60bcf16b6185d94
pault <pault@ut.ee>
2019-08-30 12:43:51 +0300

    removed PhraseListTagger

--- estnltk/finite_grammar/__init__.py
+++ estnltk/finite_grammar/__init__.py
@@ -6,6 +6,5 @@ from .layer_graph import plot_graph
 
 from .parsing import parse_graph
 
-from .phrase_list_tagger import PhraseListTagger
 from .grammar_operations import phrase_list_generator
 from estnltk.finite_grammar.grammar_operations import ngram_fingerprint
