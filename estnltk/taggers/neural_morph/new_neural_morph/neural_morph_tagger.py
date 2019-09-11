4051292152fae13609e29e2d5cd430b208d35467
pault <pault@ut.ee>
2019-09-11 13:27:48 +0300

    created subclasses for NeuralMorphTagger

--- estnltk/taggers/neural_morph/new_neural_morph/neural_morph_tagger.py
+++ estnltk/taggers/neural_morph/new_neural_morph/neural_morph_tagger.py
@@ -27,29 +27,8 @@ def check_model_files(model_dir):
                     raise FileNotFoundError("TODO: Instructions for downloading model files")
 
 
-def SoftmaxEmbTagSumTagger(output_layer='neural_morph_analysis'):
-    return NeuralMorphTagger(output_layer=output_layer, module_name='softmax_emb_tag_sum',
-                             module_package='estnltk.taggers.neural_morph.new_neural_morph')
-
-
-def SoftmaxEmbCatSumTagger(output_layer='neural_morph_analysis'):
-    return NeuralMorphTagger(output_layer=output_layer, module_name='softmax_emb_cat_sum',
-                             module_package='estnltk.taggers.neural_morph.new_neural_morph')
-
-
-def Seq2SeqEmbTagSumTagger(output_layer='neural_morph_analysis'):
-    return NeuralMorphTagger(output_layer=output_layer, module_name='seq2seq_emb_tag_sum',
-                             module_package='estnltk.taggers.neural_morph.new_neural_morph')
-
-
-def Seq2SeqEmbCatSumTagger(output_layer='neural_morph_analysis'):
-    return NeuralMorphTagger(output_layer=output_layer, module_name='seq2seq_emb_cat_sum',
-                             module_package='estnltk.taggers.neural_morph.new_neural_morph')
-
-
 class NeuralMorphTagger(Tagger):
-    """
-    Performs neural morphological tagging. It takes vabamorf analyses as input to predict
+    """Performs neural morphological tagging. It takes vabamorf analyses as input to predict
     morphological tags with better accuracy than vabamorf, but uses a different tag set.
     
     Do not use this class directly. Use the following methods to get taggers with
@@ -79,7 +58,7 @@ class NeuralMorphTagger(Tagger):
 
     """
     conf_param = ('model',)
-    
+
     def __init__(self, output_layer='neural_morph_analysis', module_name=None, module_package=None,
                  model_module=None, model=None):
         if module_name is not None and module_package is not None:
@@ -94,42 +73,77 @@ class NeuralMorphTagger(Tagger):
             self.model.build()
             self.model.restore_session(config.dir_model)
         else:
-            self.model = model # For unit testing
+            self.model = model  # For unit testing
 
         self.output_layer = output_layer
         self.output_attributes = ('morphtag', 'pos', 'form')
         self.input_layers = ('morph_analysis',)
-        
 
     def _make_layer(self, text, layers, status=None):
-        layer = Layer(name=self.output_layer, 
-                      text_object=text, 
-                      parent='words', 
-                      ambiguous=False, 
+        layer = Layer(name=self.output_layer,
+                      text_object=text,
+                      parent='words',
+                      ambiguous=False,
                       attributes=self.output_attributes)
         morphtags = []
-        
+
         for sentence in layers['sentences']:
             sentence_words = sentence.text
             analyses = []
-            
+
             for word in sentence:
                 word_text = word.text
                 pos_tags = word.morph_analysis['partofspeech']
                 forms = word.morph_analysis['form']
-                
+
                 word_analyses = []
                 for pos, form in zip(pos_tags, forms):
                     word_analyses.extend(neural_model_tags(word_text, pos, form))
                 analyses.append(word_analyses)
-                
+
             morphtags.extend(self.model.predict(sentence_words, analyses))
-        
+
         for word, tag in zip(layers['words'], morphtags):
             vm_pos, vm_form = vabamorf_tags(tag)
             layer.add_annotation(word, morphtag=tag, pos=vm_pos, form=vm_form)
-            
+
         return layer
-    
+
     def reset(self):
         self.model.reset()
+
+
+class SoftmaxEmbTagSumTagger(NeuralMorphTagger):
+    """SoftmaxEmbTagSumTagger
+
+    """
+    def __init__(self, output_layer: str = 'neural_morph_analysis'):
+        super().__init__(output_layer=output_layer, module_name='softmax_emb_tag_sum',
+                         module_package='estnltk.taggers.neural_morph.new_neural_morph')
+
+
+class SoftmaxEmbCatSumTagger(NeuralMorphTagger):
+    """SoftmaxEmbCatSumTagger
+
+    """
+    def __init__(self, output_layer: str = 'neural_morph_analysis'):
+        super().__init__(output_layer=output_layer, module_name='softmax_emb_cat_sum',
+                         module_package='estnltk.taggers.neural_morph.new_neural_morph')
+
+
+class Seq2SeqEmbTagSumTagger(NeuralMorphTagger):
+    """Seq2SeqEmbTagSumTagger
+
+    """
+    def __init__(self, output_layer: str = 'neural_morph_analysis'):
+        super().__init__(output_layer=output_layer, module_name='seq2seq_emb_tag_sum',
+                         module_package='estnltk.taggers.neural_morph.new_neural_morph')
+
+
+class Seq2SeqEmbCatSumTagger(NeuralMorphTagger):
+    """Seq2SeqEmbCatSumTagger
+
+    """
+    def __init__(self, output_layer: str = 'neural_morph_analysis'):
+        super().__init__(output_layer=output_layer, module_name='seq2seq_emb_cat_sum',
+                         module_package='estnltk.taggers.neural_morph.new_neural_morph')
