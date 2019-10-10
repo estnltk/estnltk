601e783e62a755e71bb6ddbaf6175039c412fb63
Rasmus <rasmusmaide@gmail.com>
2019-10-10 14:06:48 +0300

    Added attribute visualisation tests

--- estnltk/visualisation/attribute_visualiser/attribute_visualisation.py
+++ estnltk/visualisation/attribute_visualiser/attribute_visualisation.py
@@ -1,4 +1,4 @@
-from IPython.display import display_html, display_javascript, HTML
+from IPython.display import display_html
 from estnltk.visualisation.attribute_visualiser.direct_attribute_visualiser import DirectAttributeVisualiser
 from estnltk.visualisation.core.span_decomposition import decompose_to_elementary_spans
 from estnltk.core import rel_path
@@ -20,7 +20,7 @@ class DisplayAttributes:
     def __init__(self, name=""):
         self.span_decorator = DirectAttributeVisualiser(text_id=str(name))
         self.name = name
-        display_html(self.general_js(), raw=True)
+        display_html(self.insert_script_tag(), raw=True)
 
     def __call__(self, layer):
         display_html(self.html_output(layer), raw=True)
@@ -29,12 +29,12 @@ class DisplayAttributes:
     def html_output(self, layer):
         segments, span_list = decompose_to_elementary_spans(layer, layer.text_object.text)
 
-        outputs = [self.css(), self.event_handler_code()]
+        outputs = [self.css(), self.insert_event_handler_script()]
 
         for segment in segments:
             outputs.append(self.span_decorator(segment, span_list).replace("\n", "<br>"))
 
-        outputs.append('<button onclick="'+str(self.name)+'.export_data(')
+        outputs.append('<button onclick="' + str(self.name) + '.export_data(')
         outputs.append("'")
         outputs.append(self.name)
         outputs.append("'")
@@ -45,19 +45,19 @@ class DisplayAttributes:
     def css(self):
         with open(self.css_file) as css_file:
             contents = css_file.read()
-            output = ''.join(["<style>\n", contents, "</style>"])
+            output = '\n'.join(["<style>", contents, "</style>"])
         return output
 
-    def general_js(self):
+    def insert_script_tag(self):
         with open(self.general_js_file) as f:
             contents = f.read()
-        return ''.join(["<script>",contents,"</script>"])
+        return '\n'.join(["<script>", contents, "</script>"])
 
-    def event_handler_code(self):
+    def insert_event_handler_script(self):
         with open(self.js_file) as js_file:
             contents = js_file.read()
-            output = ''.join(["<script>var "+ str(self.name) + "= new DisplayCell('" + str(self.name)+ "',-1,0,0);",
-                              contents, "</script>"])
+            output = ''.join(["<script>var ", str(self.name), "= new DisplayCell('", str(self.name), "',-1,0,0); \n",
+                              contents, "\n</script>"])
         return output
 
     def delete_chosen_spans(self):
@@ -73,7 +73,7 @@ class DisplayAttributes:
     def mark_chosen_spans(self):
         if not self.html_displayed:
             warnings.warn("HTML of this attribute visualiser hasn't been displayed yet!"
-                  " Call this visualiser with a layer as an argument to do it.")
+                          " Call this visualiser with a layer as an argument to do it.")
             return None
 
         if self.accepted_array is None:
--- estnltk/visualisation/attribute_visualiser/direct_attribute_visualiser.py
+++ estnltk/visualisation/attribute_visualiser/direct_attribute_visualiser.py
@@ -11,13 +11,13 @@ class DirectAttributeVisualiser(SpanVisualiser):
     direct_span_visualiser"""
 
     def __init__(self, text_id, fill_empty_spans=False, event_attacher=None, js_added=False, css_added=False,
-                 css_file=abs_path("visualisation/attribute_visualiser/prettyprinter.css")):
+                 css_file=abs_path("visualisation/attribute_visualiser_outputs/prettyprinter.css")):
         self.fill_empty_spans = fill_empty_spans
         self.event_attacher = event_attacher
         self.js_added = js_added
         self.css_added = css_added
         self.css_file = css_file
-        self.js_file = abs_path("visualisation/attribute_visualiser/prettyprinter.js")
+        self.js_file = abs_path("visualisation/attribute_visualiser_outputs/prettyprinter.js")
         self.class_mapping = self.default_class_mapper
         self.mapping_dict = {"class": self.default_class_mapper}
         self.text_id = text_id
