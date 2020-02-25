07bccdb40ac5a206d27392182be3485455fc5620
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-02-25 15:49:34 +0200

    An addition to the commit c5cf8be : use vm.FSCInit() & vm.FSCTerminate() also on MacOS

--- estnltk/vabamorf/__init__.py
+++ estnltk/vabamorf/__init__.py
@@ -5,10 +5,10 @@ from . import vabamorf as vm
 import atexit
 import sys
 
-# A patch: use vm.FSCInit() & vm.FSCTerminate() only on Windows
+# A patch: use vm.FSCInit() & vm.FSCTerminate() only on Windows & MacOS
 # Patch discussed in here: https://github.com/estnltk/estnltk/issues/97
 # Credit for the patch goes to: https://github.com/cslarsen
-if sys.platform.startswith('win'):
+if sys.platform.startswith('win') or sys.platform.startswith('darwin'):
     # end of patch
     if not vm.FSCInit():
         raise Exception('Could not initiate estnltk vabamorf library. FSCInit() returned false!')
