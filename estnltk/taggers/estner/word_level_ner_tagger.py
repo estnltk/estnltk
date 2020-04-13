52e631a1ce2d06efb59f87f4ba92b312bf21ca86
Rasmus <rasmusmaide@gmail.com>
2020-04-13 13:16:45 +0300

    Tutorial for ner

--- estnltk/taggers/estner/word_level_ner_tagger.py
+++ estnltk/taggers/estner/word_level_ner_tagger.py
@@ -1,11 +1,9 @@
 from estnltk.taggers import Tagger
-from estnltk.core import DEFAULT_PY2_NER_MODEL_DIR, DEFAULT_PY3_NER_MODEL_DIR
-import six
+from estnltk.core import DEFAULT_PY3_NER_MODEL_DIR
 from estnltk.taggers.estner.refac.ner import ModelStorageUtil
 from estnltk.taggers.estner.fex import FeatureExtractor
 from estnltk.taggers.estner import CrfsuiteTagger
 from estnltk import Layer
-from estnltk import EnvelopingBaseSpan
 from typing import MutableMapping
 from estnltk.text import Text
 
@@ -14,9 +12,9 @@ class NerTagger(Tagger):
     conf_param = ['modelUtil', 'nersettings', 'fex', 'tagger']
     input_layers = []
 
-    DEFAULT_NER_MODEL_DIR = DEFAULT_PY3_NER_MODEL_DIR if six.PY3 else DEFAULT_PY2_NER_MODEL_DIR
+    DEFAULT_NER_MODEL_DIR = DEFAULT_PY3_NER_MODEL_DIR
 
-    def __init__(self, model_dir=DEFAULT_NER_MODEL_DIR, output_layer = 'wordner', output_attributes = ("nertag",)):
+    def __init__(self, model_dir=DEFAULT_NER_MODEL_DIR, output_layer = 'wordner'):
         """Initialize a new NerTagger instance.
 
         Parameters
@@ -25,7 +23,7 @@ class NerTagger(Tagger):
             A directory containing a trained ner model and a settings file.
         """
         self.output_layer = output_layer
-        self.output_attributes = output_attributes
+        self.output_attributes = ("nertag",)
         modelUtil = ModelStorageUtil(model_dir)
         nersettings = modelUtil.load_settings()
         self.fex = FeatureExtractor(nersettings)
@@ -33,12 +31,12 @@ class NerTagger(Tagger):
                                      model_filename=modelUtil.model_filename)
 
     def _make_layer(self, text: Text, layers: MutableMapping[str, Layer], status: dict):
-        # prepare input for nertagger
-        #nerdoc = json_document_to_estner_document(text)
         self.fex.process([text])
         snt_labels = self.tagger.tag(text)
+        flattened = [word for snt in snt_labels for word in snt]
 
         nerlayer = Layer(name=self.output_layer, attributes=self.output_attributes, text_object=text, enveloping="words")
-        for span, label in zip(text.words, snt_labels):
-            nerlayer.add_annotation(span, nertag=label[0])
+        for span, label in zip(text.words, flattened):
+            nerlayer.add_annotation(span, nertag=label)
+        text.pop_layer("ner_features")
         return nerlayer
