7ddb3c43b9df2fa14f1232deb5c338a7a229c70e
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-04-21 18:07:22 +0300

    Modified MaltParser to work on detached layers

--- estnltk/taggers/syntax/maltparser.py
+++ estnltk/taggers/syntax/maltparser.py
@@ -29,7 +29,7 @@ class MaltParser(object):
             parser = MaltParser()
 
             # parse text, and return results as list of lines from maltparser's output
-            results1 = parser.parse_text( text, return_type = "conll" )
+            results1 = parser.parse_text( text, return_type = 'conllu_lines' )
             for line in results1:
                 print(line)
     '''
@@ -72,7 +72,7 @@ class MaltParser(object):
         elif not self.model_name:
             raise Exception('Missing input argument: MaltParser model name')
 
-    def parse_text(self, text, **kwargs):
+    def parse_text(self, text, return_type='conllu_lines'):
         ''' Parses given text with Maltparser.
 
             Parameters
@@ -80,23 +80,88 @@ class MaltParser(object):
             text : Text with conll_morph layer
                The input text that should be analysed for dependency relations;
 
-            return_type : string
-                Returns Maltparser's results as list of CONLL format strings,
-                each element in the list corresponding to one line in
-                MaltParser's output;
-
+            return_type : 'conllu_lines' or 'temp_output_file'
+                Whether this function should return: 
+                1) the list of CONLLU format strings, or
+                2) name of the temporary output file;
+                Note: if the latter is selected, the caller of this 
+                function is responsible for erasing the temporary 
+                file if all work is done;
         '''
+        # Correct return type
+        if return_type in ['conllu_lines', 'text', 'conll', 'conllu']:
+            return_type = 'conllu_lines'
         # Obtain CONLL formatted version of the text
-        textConllStr = conll_to_str(text)
-        # Execute MaltParser and get results as CONLL formatted string
-        resultsConllStr = \
+        textConllStr = conll_to_str( text )
+        # Execute MaltParser and get results 
+        # (either as a CONLL formatted string or as name of the temp conllu file 
+        result = \
             _executeMaltparser(textConllStr, self.maltparser_dir, \
                                self.maltparser_jar, \
-                               self.model_name)
-        return resultsConllStr
+                               self.model_name, \
+                               return_type = return_type)
+        return result
+
+    def parse_detached_layers(self, text_obj, sentences_layer,
+                                              words_layer,
+                                              conll_morph_layer,
+                                              return_type='conllu_lines' ):
+        ''' Parses given text object and its detached layers with Maltparser.
+            Creates MaltParser's input conll string from the detached layers.
+
+            Parameters
+            -----------
+            text : Text with conll_morph layer
+               The input text that should be analysed for dependency relations;
+            
+            sentences_layer: Layer
+               Detached sentences layer.
+            words_layer: Layer
+               Detached words layer.
+            conll_morph_layer: Layer
+               Detached conll_morph layer.
+            
+            return_type : 'conllu_lines' or 'temp_output_file'
+                Whether this function should return: 
+                1) the list of CONLLU format strings, or
+                2) name of the temporary output file;
+                Note: if the latter is selected, the caller of this 
+                function is responsible for erasing the temporary 
+                file if all work is done;
+        '''
+        # Obtain CONLL formatted version of the text
+        textConllStr =''
+        assert len(words_layer) == len(conll_morph_layer)
+        word_id = 0
+        for sent in sentences_layer:
+            while word_id < len(words_layer):
+                word = words_layer[word_id]
+                if sent.start <= word.start and \
+                   word.end   <= sent.end:
+                    conll_morph = conll_morph_layer[word_id]
+                    assert conll_morph.text == word.text
+                    annotation = conll_morph.annotations[0]
+                    textConllStr += '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t\n' % (
+                        annotation.id, annotation.form, annotation.lemma, annotation.upostag, annotation.xpostag,
+                        annotation.feats, annotation.head, annotation.deprel, annotation.deps, annotation.misc)
+                elif sent.end <= word.start:
+                    # End of the sentence
+                    break
+                word_id += 1
+            textConllStr += '\n'
+        assert word_id == len(words_layer)
+        # Execute MaltParser and get results 
+        # (either as a CONLL formatted string or as name of the temp conllu file 
+        result = \
+            _executeMaltparser(textConllStr, self.maltparser_dir, \
+                               self.maltparser_jar, \
+                               self.model_name, \
+                               return_type = return_type)
+        return result
 
 
-def _executeMaltparser(input_string, maltparser_dir, maltparser_jar, model_name):
+def _executeMaltparser(input_string, maltparser_dir, maltparser_jar, model_name,
+                       return_type='conllu_lines'):
     ''' Executes Maltparser on given (CONLL-style) input string, and 
         returns the result. The result is an array of lines from Maltparser's 
         output.
@@ -111,11 +176,19 @@ def _executeMaltparser(input_string, maltparser_dir, maltparser_jar, model_name)
               name of the model that should be used;
         maltparser_dir: string
               the directory containing Maltparser's jar and the model file; 
+        return_type: 'conllu_lines' or 'temp_output_file'
+              Whether this function should return: 
+              1) the list of conllu lines, or
+              2) name of the temporary output conllu file;
+              Note: caller of this function is responsible
+              for erasing the temporary file if all work 
+              is done;
         
         Few of the ideas were also borrowed from NLTK's MaltParser class,
         see  http://www.nltk.org/_modules/nltk/parse/malt.html   for the reference;
     '''
-
+    assert return_type and return_type.lower() in ['conllu_lines', 'temp_output_file']
+    
     temp_input_file = \
         tempfile.NamedTemporaryFile(prefix='malt_in.', mode='w', delete=False)
     temp_input_file.close()
@@ -141,10 +214,15 @@ def _executeMaltparser(input_string, maltparser_dir, maltparser_jar, model_name)
     os.chdir(current_dir)
 
     results = []
-    in_f = codecs.open(temp_output_file.name, mode='r', encoding='utf-8')
-    for line in in_f:
-        results.append(line.rstrip())
-    in_f.close()
+    if return_type.lower() == 'conllu_lines':
+        in_f = codecs.open(temp_output_file.name, mode='r', encoding='utf-8')
+        for line in in_f:
+            results.append(line.rstrip())
+        in_f.close()
+        if not in_f.closed:
+            raise Exception('Input file unclosed!')
+        # Remove temporary output file
+        os.remove(temp_output_file.name)
 
     if not temp_input_file.closed:
         raise Exception('Temp input file unclosed!')
@@ -152,13 +230,14 @@ def _executeMaltparser(input_string, maltparser_dir, maltparser_jar, model_name)
         raise Exception('Temp input file unclosed!')
     if not out_f.closed:
         raise Exception('Output file unclosed!')
-    if not in_f.closed:
-        raise Exception('Input file unclosed!')
+    
     # TODO: For some reason, the method gives "ResourceWarning: unclosed file"
     # in Python 3.4, although, apparently, all file handles seem to be closed;
     # Nothing seems to be wrong in Python 2.7;
-
+    
+    # Remove temporary input file
     os.remove(temp_input_file.name)
-    os.remove(temp_output_file.name)
 
-    return results
+    return results if return_type.lower() == 'conllu_lines' else temp_output_file.name
+
+
