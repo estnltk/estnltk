3bb3cfe9dcb728da5f6eca597b293fc1c1e761b9
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-05-11 16:14:43 +0300

    Completing packaging Wordnet: updated setup and added init to tests

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
         'estnltk.taggers': ['miscellaneous/*.csv', 'syntax/files/*.*', 'syntax/files/LICENSE', 'syntax/java-res/maltparser/*.*', 'syntax/java-res/maltparser/lib/*.*', 'syntax/udpipe_tagger/resources/*.*', 'morph_analysis/hfst/models/*.*', 'morph_analysis/number_fixes/*.*', 'morph_analysis/reorderings/*.*', 'verb_chains/v1_4_1/res/*.*', 'syntax_preprocessing/rules_files/*.*', 'text_segmentation/*.csv', 'estner/gazetteer/*', 'estner/models/py3_default/*'],
         'estnltk.tests': ['test_morph/*.csv', 'test_corpus_processing/*.vert', 'test_taggers/test_dict_taggers/*.csv', 'test_taggers/miscellaneous/*.json', 'test_taggers/test_standard_taggers/*.json', 'test_taggers/*.txt', 'test_visualisation/expected_outputs/direct_plain_span_visualiser_outputs/*.txt', 'test_visualisation/expected_outputs/indirect_plain_span_visualiser_outputs/*.txt', 'test_visualisation/expected_outputs/attribute_visualiser_outputs/*.txt', 'test_converters/*.conll', 'test_syntax_preprocessing/*.txt',],
         'estnltk.vabamorf': ['dct/2020-01-22_nosp/*.dct', 'dct/2020-01-22_sp/*.dct'],
-        'estnltk.wordnet': ['*.cnf', 'data/*.txt', 'data/*.soi', 'data/*.cnf', 'data/scripts/*.py'],
+        'estnltk.wordnet': ['data/estwn_kb74/*.*'],
         'estnltk.mw_verbs': ['res/*'],
         'estnltk.converters': ['*.mrf'],
         'estnltk.visualisation': ['attribute_visualiser/*.css', 'attribute_visualiser/*.js', 'span_visualiser/*.css', 'span_visualiser/*.js']
