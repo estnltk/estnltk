20296af5da83fdf7b962c2334dbb628b7f9a5c59
ckittask <claudia.kittask@tudeng.ut.ee>
2020-05-16 19:40:49 +0300

    Changing behaviour of SyntaxDependencyRetagger, corrects id and head when needed

--- estnltk/taggers/syntax/syntax_dependency_retagger.py
+++ estnltk/taggers/syntax/syntax_dependency_retagger.py
@@ -30,32 +30,63 @@ class SyntaxDependencyRetagger(Retagger):
 
     def _change_layer(self, raw_text: str, layers: MutableMapping[str, Layer], status: dict):
         layer = layers[self.output_layer]
-
+        child_correction_needed = False
         attributes = list(layer.attributes)
         attributes.extend(attr for attr in ['parent_span', 'children']
-                                   if attr not in layer.attributes)
+                          if attr not in layer.attributes)
         layer.attributes = tuple(attributes)
 
         id_to_span = {}
         id_to_children = defaultdict(list)
 
-        for span in layer:
+        for i, span in enumerate(layer):
+
             id_ = span.annotations[0].id
             head = span.annotations[0].head
             assert all(annotation.id == id_ for annotation in span.annotations)
             assert all(annotation.head == head for annotation in span.annotations)
 
-            if id_ == 1:
+            if str(id_) == '1' or str(id_) == '_':  # new sentence starts
+                if child_correction_needed:
+                    id_to_children = correct(id_to_span, id_to_children)
                 annotate_spans(id_to_span, id_to_children)
+                child_correction_needed = False
                 id_to_span = {}
                 id_to_children = defaultdict(list)
 
             id_to_span[id_] = span
-            id_to_children[head].append(span)
 
+            if id_ == head:  # do not add the span as child if true
+                child_correction_needed = True
+            else:
+                id_to_children[head].append(span)
+
+        if child_correction_needed:
+            id_to_children = correct(id_to_span, id_to_children)
         annotate_spans(id_to_span, id_to_children)
 
         layer.serialisation_module = syntax_v0.__version__
+
+
+def correct(id_to_span, id_to_children):
+    for id_, span in id_to_span.items():
+        for annotation in span.annotations:
+            if str(annotation.id) == str(annotation.head):  # in case of punctuation, id can equal head
+                if annotation.id == '_' and len(id_to_span) == 1:
+                    annotation.id = '1'
+                    annotation.head = '0'
+                elif int(annotation.id) > 1:  # previous span exists, add parent as previous span
+                    head = str(int(annotation.id) - 1)
+                    id_to_children[head].append(span)
+                    annotation.head = head
+                elif len(id_to_span) > 1 and int(annotation.id) == 1:  # no previous span, add parent as next span
+                    head = str(int(annotation.id) + 1)
+                    id_to_children[head].append(span)
+                    annotation.head = head
+                else:  # only one span
+                    annotation.head = '0'
+
+    return id_to_children
 
 
 def annotate_spans(id_to_span, id_to_children):
@@ -63,5 +94,3 @@ def annotate_spans(id_to_span, id_to_children):
         for annotation in span.annotations:
             annotation.parent_span = id_to_span.get(annotation.head)
             annotation.children = tuple(id_to_children[id_])
-
-
