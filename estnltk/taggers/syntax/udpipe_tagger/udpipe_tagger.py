4c431f1fb02d90b627736aa736a8c4f03bdb73cc
ckittask <claudia.kittask@tudeng.ut.ee>
2020-05-16 19:17:50 +0300

    correction to udpipetagger

--- estnltk/taggers/syntax/udpipe_tagger/udpipe_tagger.py
+++ estnltk/taggers/syntax/udpipe_tagger/udpipe_tagger.py
@@ -35,31 +35,22 @@ class UDPipeTagger(Tagger):
     Tags dependency syntactic analysis with UDPipe.
     """
     conf_param = ['model', 'version', 'add_parent_and_children', 'syntax_dependency_retagger',
-                  'output_attributes', 'udpipe_cmd']
+                  'udpipe_cmd']
 
     def __init__(self,
                  model=None,
                  output_layer='udpipe_syntax',
-                 input_layers=None,
+                 sentences_layer='sentences',
                  input_syntax_layer='conll_morph',
-                 version=None,
-                 output_attributes=None,
+                 version='conllx',
                  add_parent_and_children=False,
                  udpipe_cmd=None):
-
-        if input_layers is None:
-            input_layers = ['sentences', input_syntax_layer]
 
         if udpipe_cmd is None:
             self.udpipe_cmd = 'udpipe'
         else:
             self.udpipe_cmd = udpipe_cmd
 
-        if version is None:
-            version = 'conllx'
-
-        if output_attributes is None:
-            output_attributes = ('id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc')
         if model is None:
             if version == 'conllx':
                 model = os.path.join(RESOURCES, 'model_0.output')
@@ -70,7 +61,8 @@ class UDPipeTagger(Tagger):
 
         self.model = model
         self.add_parent_and_children = add_parent_and_children
-        self.output_attributes = output_attributes
+        self.output_attributes = (
+            'id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc')
         if self.add_parent_and_children:
             self.syntax_dependency_retagger = SyntaxDependencyRetagger(conll_syntax_layer=output_layer)
             self.output_attributes += ('parent_span', 'children')
@@ -78,7 +70,7 @@ class UDPipeTagger(Tagger):
             self.syntax_dependency_retagger = None
 
         self.output_layer = output_layer
-        self.input_layers = input_layers
+        self.input_layers = [sentences_layer, input_syntax_layer]
 
         self.version = version
 
@@ -90,35 +82,40 @@ class UDPipeTagger(Tagger):
                       " Please make sure that UDPipe is installed in your system and\n" + \
                       " available from system's PATH variable. Alternatively, you can\n" + \
                       " provide the location of UDPipe executable via the input\n" + \
-                      " argument 'udpipe_cmd'. ";
+                      " argument 'udpipe_cmd'. "
                 raise Exception(msg)
 
     def _make_layer(self, text, layers, status=None):
-        conllu_string = sentence_to_conll(layers[self.input_layers[0]][0], layers[self.input_layers[1]]).encode()
-
+        sentences_layer = layers[self.input_layers[0]]
+        conll_layer = layers[self.input_layers[1]]
         layer = Layer(name=self.output_layer, text_object=text, attributes=self.output_attributes, ambiguous=True,
                       parent=self.input_layers[1])
-        cmd = "%s --parse %s" % (self.udpipe_cmd, self.model)
+        # Collecting all sentences as an input to the model
+        conll_strings = []
+        for sentence in sentences_layer:
+            output = sentence_to_conll(sentence, layers[self.input_layers[1]])
+            conll_strings.append(output)
 
+        input_string = ''.join(conll_strings).encode()
+        # Creating command for the udpipe model
+        cmd = "%s --parse %s" % (self.udpipe_cmd, self.model)
         process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-        process.stdin.write(conllu_string)
+        process.stdin.write(input_string)
         result = process.communicate()[0].decode().strip()
         process.stdin.close()
-
-        start = 0
-        for line in result.split(OS_NEWLINE):
+        result = result.replace(OS_NEWLINE + OS_NEWLINE, OS_NEWLINE)  # removing double newlines
+        for line, span in zip(result.split(OS_NEWLINE), conll_layer):
             line = line.split('\t')
-            ID = line[0]
+            ID = int(line[0])
             form = line[1]
             lemma = line[2]
             upostag = line[3]
             xpostag = line[4]
             feats = line[5]
-            head = line[6]
+            head = int(line[6])
             deprel = line[7]
             deps = line[8]
             misc = line[9]
-            span = ElementaryBaseSpan(start, start + len(form))
             feats = OrderedDict([
                 (part.split("=")[0], parse_nullable_value(part.split("=")[1]) if "=" in part else "")
                 for part in feats.split("|") if parse_nullable_value(part.split("=")[0]) is not None
@@ -126,7 +123,6 @@ class UDPipeTagger(Tagger):
             attributes = {'id': ID, 'form': form, 'lemma': lemma, 'upostag': upostag, 'xpostag': xpostag,
                           'feats': feats, 'head': head, 'deprel': deprel, 'deps': deps, 'misc': misc}
             layer.add_annotation(span, **attributes)
-            start += len(form) + 1
 
         if self.add_parent_and_children:
             # Add 'parent_span' & 'children' to the syntax layer
