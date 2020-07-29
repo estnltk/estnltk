3d3f21583c21c54f8398406ee1b477d55a65f681
hendriksuvalov <hendrik.suvalov@tudeng.ut.ee>
2020-07-29 12:33:17 +0300

    Test for missing layer query

--- estnltk/storage/postgres/tests/test_missing_layer_query.py
+++ estnltk/storage/postgres/tests/test_missing_layer_query.py
@@ -1,11 +1,14 @@
 import unittest
 import random
 
+from psycopg2.sql import SQL, Identifier
+
 from estnltk import logger
 from estnltk import Text
 from estnltk.storage.postgres import PostgresStorage
 from estnltk.storage.postgres import create_schema, delete_schema, count_rows
 from estnltk.storage import postgres as pg
+from estnltk.taggers import VabamorfTagger, ParagraphTokenizer
 
 logger.setLevel('DEBUG')
 
@@ -14,11 +17,12 @@ def get_random_collection_name():
     return 'collection_{}'.format(random.randint(1, 1000000))
 
 
-# @TODO: Currently not working
 class TestMissingLayerQuery(unittest.TestCase):
     def setUp(self):
-        schema = "test_schema"
+        schema = "test_schema7"
+        self.schema = schema
         self.storage = PostgresStorage(pgpass_file='~/.pgpass', schema=schema, dbname='test_db')
+
         create_schema(self.storage)
 
     def tearDown(self):
@@ -26,23 +30,47 @@ class TestMissingLayerQuery(unittest.TestCase):
         self.storage.close()
 
     def test_layer_ngram_query(self):
-        collection = self.storage[get_random_collection_name()]
+        collection_name = get_random_collection_name()
+        collection = self.storage[collection_name]
         collection.create()
 
         with collection.insert() as collection_insert:
-            text1 = Text('Teeme ühe naljaka katse').tag_layer(['morph_analysis'])
-            collection_insert(text1, meta_data=text1.meta)
+            text1 = Text('Ööbik laulab.').tag_layer(["sentences"])
+            collection_insert(text1)
 
-            text2 = Text('Joome õhtul teed').tag_layer(['morph_analysis'])
-            collection_insert(text2, meta_data=text2.meta)
+            text2 = Text('Mis kell on?').tag_layer(["sentences"])
+            collection_insert(text2)
 
-            text3 = Text('Sõidan autoga tee peal').tag_layer(['morph_analysis'])
-            collection_insert(text3, meta_data=text3.meta)
+            text3 = Text('Auto sõidab.').tag_layer(["sentences"])
+            collection_insert(text3)
 
-        res = list(collection.select(query=pg.MissingLayerQuery(missing_layer="words")))
-        self.assertEqual(len(res), 3)
+        layer1 = "layer1"
+        layer2 = "layer2"
+        layer3 = "layer3"
+        tagger1 = VabamorfTagger(disambiguate=False, output_layer=layer1)
+        tagger2 = VabamorfTagger(disambiguate=False, output_layer=layer2)
+        tagger3 = VabamorfTagger(disambiguate=False, output_layer=layer3)
 
-        res = list(collection.select(query=pg.MissingLayerQuery(missing_layer="morph_analysis")))
+        collection.create_layer(tagger=tagger1)
+        collection.create_layer(tagger=tagger2)
+        collection.create_layer(tagger=tagger3)
+
+        with self.storage.conn.cursor() as c:
+            c.execute(SQL("DELETE FROM {}.{}__layer1__layer WHERE id = 0;").format(
+                SQL(self.schema),
+                SQL(collection_name)))
+
+            c.execute(SQL("DELETE FROM {}.{}__layer3__layer WHERE id = 0 OR id = 1;").format(
+                SQL(self.schema),
+                SQL(collection_name)))
+
+        res = list(collection.select(query=pg.MissingLayerQuery(missing_layer=layer1)))
+        self.assertEqual(len(res), 1)
+
+        res = list(collection.select(query=pg.MissingLayerQuery(missing_layer=layer2)))
+        self.assertEqual(len(res), 0)
+
+        res = list(collection.select(query=pg.MissingLayerQuery(missing_layer=layer3)))
         self.assertEqual(len(res), 2)
 
         collection.delete()
