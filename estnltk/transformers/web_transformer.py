998b367b786a8b06fb30c55cb5c38056eb5af2ff
pault <pault@ut.ee>
2020-10-08 06:34:27 +0300

    WebTransformer: moved layer_names parameter to __init__

--- estnltk/transformers/web_transformer.py
+++ estnltk/transformers/web_transformer.py
@@ -6,15 +6,16 @@ from estnltk.converters import text_to_json, json_to_text
 class WebTransformer:
     __slots__ = ['url']
 
-    def __init__(self, url: str = 'http://127.0.0.1:5000/tag_layer/morph_analysis,words'):
-        if not url.endswith('/'):
-            url += '/'
-        self.url = url
+    def __init__(self, url: str = 'http://127.0.0.1:5000/tag_layer/morph_analysis,words',
+                 layer_names=('morph_analysis', 'sentences')):
+        separator = '' if url.endswith('/') else '/'
 
-    def __call__(self, text, layer_names=('morph_analysis', 'sentences')):
         if isinstance(layer_names, str):
             layer_names = layer_names,
 
+        self.url = '{}{}{}'.format(url, separator, ','.join(layer_names))
+
+    def __call__(self, text, ):
         text_json = text_to_json(text)
-        resp = requests.post(self.url + ','.join(layer_names), json=text_json)
+        resp = requests.post(self.url, json=text_json)
         return json_to_text(resp.text)
