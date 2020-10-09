113ba0890fe017c5d671faeb6939eea98506d9aa
pault <pault@ut.ee>
2020-10-09 08:08:20 +0300

    WebTransformer._repr_html_

--- estnltk/transformers/web_transformer.py
+++ estnltk/transformers/web_transformer.py
@@ -16,18 +16,41 @@ class WebTransformerChecker(type):
 class WebTransformer(metaclass=WebTransformerChecker):
     """Tags layers using webservice and returns a new Text object."""
 
-    __slots__ = ['url']
+    __slots__ = ['url', 'layer_names']
 
-    def __init__(self, url: str = 'http://127.0.0.1:5000/tag_layer/morph_analysis,words',
-                 layer_names=('morph_analysis', 'sentences')):
-        separator = '' if url.endswith('/') else '/'
-
-        if isinstance(layer_names, str):
-            layer_names = layer_names,
-
-        self.url = '{}{}{}'.format(url, separator, ','.join(layer_names))
+    def __init__(self, url: str = 'http://127.0.0.1:5000', layer_names=('morph_analysis', 'sentences')):
+        self.url = url.rstrip('/')
+        self.layer_names = (layer_names,) if isinstance(layer_names, str) else layer_names
 
     def __call__(self, text, ):
         text_json = text_to_json(text)
-        resp = requests.post(self.url, json=text_json)
-        return json_to_text(resp.text)
+        try:
+            resp = requests.post('{}/tag_layer/{}'.format(self.url, ','.join(self.layer_names)), json=text_json)
+        except requests.ConnectionError as e:
+            return str(e)
+        if resp.status_code == 200:
+            return json_to_text(resp.text)
+
+    def _repr_html_(self):
+        import pandas
+        assert self.__class__.__doc__ is not None, 'No docstring.'
+        try:
+            status = requests.get(self.url+'/status')
+            status = status.text
+        except requests.ConnectionError:
+            status = 'Connection error'
+        try:
+            about = requests.get(self.url+'/about')
+            about = about.text
+        except requests.ConnectionError:
+            about = 'Connection error'
+
+        description = self.__class__.__doc__.strip().split('\n')[0]
+        html_tokens = ['<h4>{}</h4>'.format(self.__class__.__name__), description]
+
+        data = {'key': ['url', 'status', 'about'], 'value': [self.url, status, about]}
+        status_table = pandas.DataFrame(data, columns=['key', 'value'])
+        status_table = status_table.to_html(header=False, index=False)
+
+        html_tokens.append(status_table)
+        return '\n'.join(html_tokens)
