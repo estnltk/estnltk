8b2f797e322380448d9de4e28fc6d736fbf8e5b9
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-10-19 17:27:06 +0300

    Updated make_userdict: empty annotations will be discarded

--- estnltk/taggers/morph_analysis/make_userdict.py
+++ estnltk/taggers/morph_analysis/make_userdict.py
@@ -18,7 +18,8 @@ from estnltk.taggers.morph_analysis.userdict_tagger import UserDictTagger
 
 def make_userdict( lexicon, 
                    output_layer: str = 'morph_analysis',
-                   ignore_case: bool = False, 
+                   ignore_case:     bool = False, 
+                   warn_on_unknown: bool = True,
                    vm_analyzer: VabamorfAnalyzer=None ):
     '''
     Given a lexicon mapping from surface word forms to normalized word 
@@ -33,6 +34,13 @@ def make_userdict( lexicon,
    
     :param output_layer: name of UserDictTagger's output_layer. Defaults
         to 'morph_analysis';
+    
+    :param ignore_case: boolean specifying the value for ignore_case 
+        parameter of created UserDictTagger. Default: False;
+    
+    :param warn_on_unknown: boolean specifying whether the user should be 
+        warned if no entries are generated for a word because all its 
+        normalized forms are unknown to the vm_analyzer. Default: True;
     
     :param vm_analyzer: an instance VabamorfAnalyzer which will be used
         to create morphological analyses for normalized words. If not 
@@ -85,6 +93,7 @@ def make_userdict( lexicon,
         vm_analyzer.tag(analysable_text)
         # Collect analyses and construct userdict entries
         word_id = 0
+        words_with_empty_annotations = 0
         for word_str in sorted( lexicon.keys() ):
             if word_str == _WORD_SEPARATOR:
                 continue
@@ -94,7 +103,16 @@ def make_userdict( lexicon,
             while word_id < len( analysable_text[ vm_analyzer.output_layer ] ):
                 morph_word = analysable_text[ vm_analyzer.output_layer ][word_id]
                 if morph_word.text != _WORD_SEPARATOR:
-                    corresponding_annotations.append( morph_word.annotations )
+                    cur_annotations = []
+                    for annotation in morph_word.annotations:
+                        if _is_empty_annotation( annotation ):
+                            # discard an empty annotation
+                            continue
+                        else:
+                            cur_annotations.append( annotation )
+                    if len( cur_annotations ) == 0:
+                        words_with_empty_annotations += 1
+                    corresponding_annotations.append( cur_annotations )
                     new_normalized_forms.append( morph_word.text )
                 word_id += 1
                 if morph_word.text == _WORD_SEPARATOR:
@@ -108,8 +126,15 @@ def make_userdict( lexicon,
                     for key in ['root', 'ending', 'clitic', 'form', 'partofspeech']:
                         entry[key] = annotation[key]
                     entries.append( entry )
-            words_dict[word_str] = entries
-        assert valid_keys == len(words_dict.keys())
+            if entries:
+                words_dict[word_str] = entries
+            else:
+                # Skip word with no entries
+                if warn_on_unknown:
+                    warnings.warn('(!) No entry created for {!r}: words in {!r} were unknown to morphological analyser.'.format(word_str, new_normalized_forms))
+                pass
+        valid_keys -= words_with_empty_annotations
+        assert valid_keys == len( words_dict.keys() )
     # Create new UserDictTagger
     userdict_tagger = UserDictTagger(words_dict=words_dict,
                                      output_layer=output_layer,
