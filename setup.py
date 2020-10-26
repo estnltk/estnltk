12bd338f3d905e5d098355163e5326841a6d709d
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-10-26 14:27:04 +0200

    Updated setup.py : updated Python versions

--- setup.py
+++ setup.py
@@ -123,6 +123,7 @@ setup(
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
                  'Topic :: Scientific/Engineering',
                  'Topic :: Scientific/Engineering :: Artificial Intelligence',
                  'Topic :: Scientific/Engineering :: Information Analysis',
