3baea016051a0fabd82e8e2ae6ff56e9f77c486e
Siim Orasmaa <siim.orasmaa@ut.ee>
2020-10-27 11:09:29 +0200

    Updated test_splitting_discontinuous

--- estnltk/tests/test_layer_operations/test_splitting_discontinuous.py
+++ estnltk/tests/test_layer_operations/test_splitting_discontinuous.py
@@ -125,3 +125,60 @@ def test_split_by_clauses_2():
     assert clause_texts[2].clauses.text == ['teretas', 'meid', '.']
     assert clause_texts[3].sentences.text == ['(', 'sic', '!', ')']
 
+
+
+def test_split_by_clauses_3_verb_chains():
+    # Test split by clauses (trim overlapping layers) with verb chain annotations
+    text = Text('Und pole, aga ma ei näe põhjust öö läbi üleval olla (sest magada on vaja!).')
+    text.tag_layer(['words', 'sentences', 'morph_analysis'])
+    #from pprint import pprint
+    #pprint(layer_to_dict(text['clauses']))
+    # Add clauses layer
+    clauses_layer_dict = \
+        {'ambiguous': False,
+         'attributes': ('clause_type',),
+         'enveloping': 'words',
+         'meta': {},
+         'name': 'clauses',
+         'parent': None,
+         'serialisation_module': None,
+         'spans': [{'annotations': [{'clause_type': 'regular'}],
+                    'base_span': ((0, 3), (4, 8), (8, 9))},
+                   {'annotations': [{'clause_type': 'regular'}],
+                    'base_span': ((10, 13),
+                                  (14, 16),
+                                  (17, 19),
+                                  (20, 23),
+                                  (24, 31),
+                                  (32, 34),
+                                  (35, 39),
+                                  (40, 46),
+                                  (47, 51),
+                                  (74, 75))},
+                   {'annotations': [{'clause_type': 'embedded'}],
+                    'base_span': ((52, 53),
+                                  (53, 57),
+                                  (58, 64),
+                                  (65, 67),
+                                  (68, 72),
+                                  (72, 73),
+                                  (73, 74))}]}
+    text.add_layer( dict_to_layer(clauses_layer_dict) )
+    # Add verb chains layer
+    from estnltk.taggers import VerbChainDetector
+    vc_detector = VerbChainDetector()
+    vc_detector.tag(text)
+    # Split by clauses
+    clause_texts = _split_by_discontinuous_layer(text, 'clauses',
+                                                 layers_to_keep=list(text.layers),
+                                                 trim_overlapping=True)
+    # Validate results
+    assert len(clause_texts) == len(text['clauses'])
+    assert clause_texts[0].clauses.text == ['Und', 'pole', ',']
+    assert clause_texts[1].clauses.text == ['aga', 'ma', 'ei', 'näe', 'põhjust', 'öö', 'läbi', 'üleval', 'olla', '.']
+    assert clause_texts[2].clauses.text == ['(', 'sest', 'magada', 'on', 'vaja', '!', ')']
+    assert clause_texts[0].verb_chains.text == ['pole']
+    assert clause_texts[1].verb_chains.text == ['ei', 'näe']
+    assert clause_texts[2].verb_chains.text == ['magada', 'on', 'vaja']
+
+
