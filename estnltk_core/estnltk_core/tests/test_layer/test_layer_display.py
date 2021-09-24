b61f9c06d3ebe33e4da2a2d5ce0b343740952ed0
Siim Orasmaa <siim.orasmaa@ut.ee>
2021-09-24 16:41:49 +0300

    Added test for layer_display (just to remind that it also needs fixing)

---
+++ estnltk_core/estnltk_core/tests/test_layer/test_layer_display.py
@@ -0,0 +1,17 @@
+import pytest
+
+from estnltk_core import Text
+from estnltk_core import Layer
+
+
+@pytest.mark.xfail(reason="TODO needs fixing.")
+def test_layer_display():
+    t = Text("0123456789")
+    layer = Layer(name='base', ambiguous=False)
+    layer.add_annotation((1, 3))
+    layer.add_annotation((7, 9))
+    t.add_layer( layer )
+    
+    # In order to fix this, we need 'estnltk.visualisation' which adds dependencies
+    # TODO: can we make an extract from 'estnltk.visualisation' concerning only layer.display()
+    layer.display()
