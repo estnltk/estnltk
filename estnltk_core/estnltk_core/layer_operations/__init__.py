51fa280cb03bcc607078a741cc38d4803647530a
Siim Orasmaa <siim.orasmaa@ut.ee>
2021-10-12 11:41:55 +0300

    Relocated obsolete layer_operations to estnltk_core.legacy

--- estnltk_core/estnltk_core/layer_operations/__init__.py
+++ estnltk_core/estnltk_core/layer_operations/__init__.py
@@ -4,15 +4,10 @@ from .rolling import Rolling
 from .layer_operations import apply_filter
 from .layer_operations import drop_annotations
 from .layer_operations import keep_annotations
-from .layer_operations import apply_simple_filter
 from .layer_operations import unique_texts
 from .layer_operations import count_by
-from .layer_operations import count_by_document
-from .layer_operations import dict_to_df
 from .layer_operations import diff_layer
 from .layer_operations import get_enclosing_spans
-from .layer_operations import group_by_spans
-from .layer_operations import conflicts
 from .merge import merge_layers
 from .combine import combine_layers
 from .conflict_resolver import iterate_conflicting_spans
