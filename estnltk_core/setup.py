8b9b6fac11adf9e36931d9a60c9cd0120d9cb37b
Siim Orasmaa <siim.orasmaa@ut.ee>
2021-10-18 11:43:01 +0300

    Updated estnltk_core's setup + added MANIFEST.in

--- estnltk_core/setup.py
+++ estnltk_core/setup.py
@@ -7,16 +7,25 @@ setup(
     packages=find_packages(),
     author="University of Tartu",
     author_email="siim.orasmaa@gmail.com, alex.tk.fb@gmail.com, tpetmanson@gmail.com, swen@math.ut.ee",
-    description="EstNLTK core — basic components of the EstNLTK v1.7 library",
+    description="EstNLTK core — basic components of the EstNLTK v1.6 library",
     # TODO: long_description
-    long_description="EstNLTK core — basic components of the EstNLTK v1.7 library",
+    long_description="EstNLTK core — basic components of the EstNLTK v1.6 library",
     #long_description=open('description.txt').read(),
     license="GPLv2",
+    # the list of package data used by "build", "bdist" and "install"
+    include_package_data=True,
+    package_data={
+        'license_headers' : ['*.*'],
+        'estnltk.visualisation': ['span_visualiser/*.css', 
+                                  'span_visualiser/*.js']
+    },
     url="https://github.com/estnltk/estnltk",
     install_requires=[
         'regex>=2015.07.19',       # improved Python regular expressions
         'networkx ; python_version >  "3.6"',      # building graphs: required for layers, WordNet and grammars (> py36)
         'networkx==2.5 ; python_version == "3.6"', # building graphs: required for layers, WordNet and grammars (= py36)
+        'ipython ; python_version >  "3.6"',         # required for integration with Jupyter Notebook-s (> py36)
+        'ipython< 7.17.0 ; python_version == "3.6"', # required for integration with Jupyter Notebook-s (= py36)
         'pandas>=1.1.5 ; python_version >  "3.6"', # Panel Data Analysis library for Python (> py36)
         'pandas<=1.1.5 ; python_version == "3.6"', # Panel Data Analysis library for Python (= py36)
         'numpy==1.19.4 ; python_version == "3.6"', # This is the last numpy version that supports py36
