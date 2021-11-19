e1f205a602756b1fd4b2e99a28aeef44d8c78c4e
Siim Orasmaa <siim.orasmaa@ut.ee>
2021-11-19 10:58:13 +0200

    Refactored find_layer_dependencies(...) + added check for a missing layer

--- estnltk_core/estnltk_core/layer_operations/layer_dependencies.py
+++ estnltk_core/estnltk_core/layer_operations/layer_dependencies.py
@@ -5,14 +5,15 @@ def find_layer_dependencies(text:'Text', layer:'Layer', include_enveloping:bool 
                                                         reverse:bool= False,
                                                         add_bidirectional_parents:bool=False):
     '''Finds all layers that the given layer depends on. 
-       Returns list of dependency layer names. 
+       Returns set of dependency layer names. 
        If include_enveloping=True (default), then finds dependency layers over 
           enveloping relations. 
-       If include_parents=True (default), then traces dependency layers over 
+       If include_parents=True (default), then finds dependency layers over 
           parent relations.
        
        Optionally, if reverse=True, then searches for reverse relations: given 
-       the parent layer, finds all its children and/or layers enveloping it. 
+       a (parent) layer, find all layers that are depending on it (descendant
+       layers).
        
        Optionally, if add_bidirectional_parents=True, adds parent relations in 
        both ways: child -> parent and parent -> child 
@@ -21,21 +22,19 @@ def find_layer_dependencies(text:'Text', layer:'Layer', include_enveloping:bool 
     if not include_enveloping and not include_parents:
         raise ValueError('(!) Unexpected configuration: '+
               'parameters include_enveloping and include_parents cannot be both False.')
+    if layer not in text.layers:
+        raise ValueError( '(!) Layer {!r} is missing from the given Text object.'.format(layer) )
     graph = nx.DiGraph()
     for layer_name in text.layers:
+        graph.add_node( layer_name )
         layer_object = text[layer_name]
         if include_enveloping and layer_object.enveloping:
-            if not reverse:
-                graph.add_edge( layer_name, layer_object.enveloping )
-            else:
-                graph.add_edge( layer_object.enveloping, layer_name )
+            graph.add_edge( layer_object.enveloping, layer_name )
         if include_parents and layer_object.parent:
             if add_bidirectional_parents:
-                graph.add_edge( layer_name, layer_object.parent )
                 graph.add_edge( layer_object.parent, layer_name )
-            elif not reverse:
                 graph.add_edge( layer_name, layer_object.parent )
             else:
                 graph.add_edge( layer_object.parent, layer_name )
-    return nx.descendants(graph,layer) if layer in graph.nodes else set()
+    return nx.ancestors(graph,layer) if not reverse else nx.descendants(graph,layer)
 
