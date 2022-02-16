f7344e4ac37fe71a46df3b3dca6261c908f46e48
Siim Orasmaa <siim.orasmaa@ut.ee>
2022-02-16 18:13:19 +0200

    Added rebase docstring

--- estnltk_core/estnltk_core/layer_operations/rebase.py
+++ estnltk_core/estnltk_core/layer_operations/rebase.py
@@ -3,6 +3,20 @@ import networkx as nx
 from .layer_dependencies import find_layer_dependencies
 
 def rebase(text, layer, new_base):
+    """
+    Changes parent of the layer to new_base.
+
+    Parameters
+    ----------
+    text: Union[BaseText, 'Text']
+        Text object containing both the target layer and the new_base layer.
+    layer: Union[BaseLayer, Layer]
+        Target layer which parent layer will be changed.
+    new_base: str
+        Name of the new parent layer. Rebasing can be applied successfully
+        only if the new parent layer is direct or indirect parent of the
+        current parent layer.
+    """
     dependency_layers = find_layer_dependencies(text, layer, 
                                                  include_enveloping=False,
                                                  include_parents=True)
