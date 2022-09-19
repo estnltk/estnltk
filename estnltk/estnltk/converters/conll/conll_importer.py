9f1c07cd79b1300db243f2475080b4c2614d8840
Siim Orasmaa <siim.orasmaa@ut.ee>
2022-09-19 17:46:19 +0300

    A patch for conll_importer: annotations with malformed id/head values are now discarded by default.

--- estnltk/estnltk/converters/conll/conll_importer.py
+++ estnltk/estnltk/converters/conll/conll_importer.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import List
 from conllu import parse_incr
 from estnltk import Layer, Text, ElementaryBaseSpan
@@ -57,7 +58,7 @@ def add_layer_from_conll(file: str, text: Text, syntax_layer: str):
     return text
 
 
-def conll_to_text(file: str, syntax_layer: str = 'conll_syntax') -> Text:
+def conll_to_text(file: str, syntax_layer: str = 'conll_syntax', discard_malformed_ids:bool=True) -> Text:
     """
     Reads file in conll format and creates a Text object with words and syntax layers.
 
@@ -65,6 +66,10 @@ def conll_to_text(file: str, syntax_layer: str = 'conll_syntax') -> Text:
         name of the conll file
     :param syntax_layer: str
         name of the syntax layer
+    :param discard_malformed_ids: bool
+        if True, then annotations which 
+        'id' or 'head' values are not 
+        integers will be discarded.
     :return: Text
     """
 
@@ -102,8 +107,17 @@ def conll_to_text(file: str, syntax_layer: str = 'conll_syntax') -> Text:
                 len_w = len(token)
                 base_span = ElementaryBaseSpan(cur, cur+len_w)
                 words.add_annotation(base_span)
-
-                syntax.add_annotation(base_span, **w)
+                ids_ok = True
+                if discard_malformed_ids:
+                    # Check that both 'id' and 'head' are integers.
+                    # If not, discard annotation as broken.
+                    ids_ok = isinstance(w['id'], int) and \
+                             isinstance(w['head'], int)
+                if ids_ok:
+                    syntax.add_annotation(base_span, **w)
+                else:
+                    warnings.warn(('(!) Unexpected id={!r}, head={!r} in token {!r} at position {}. '+\
+                                   'Discarding broken annotation.').format(w['id'], w['head'], w['form'], cur))
                 cur += len_w + 1
 
             sentences.add_annotation(words[sentence_start:])
@@ -119,7 +133,8 @@ def conll_to_text(file: str, syntax_layer: str = 'conll_syntax') -> Text:
     return text
 
 
-def conll_to_texts_list(file: str, syntax_layer: str = 'conll_syntax', postcorrect_sent_ids: bool=True) -> List[Text]:
+def conll_to_texts_list(file: str, syntax_layer: str = 'conll_syntax', postcorrect_sent_ids: bool=True,
+                                                                       discard_malformed_ids:bool=True) -> List[Text]:
     """
     Reads file in conll format and creates separate Text objects according to 
     file names read from the 'sent_id' attributes in the metadata.
@@ -136,6 +151,10 @@ def conll_to_texts_list(file: str, syntax_layer: str = 'conll_syntax', postcorre
         if True, then postcorrections 
         will be applied to broken 
         'sent_id'-s;
+    :param discard_malformed_ids: bool
+        if True, then annotations which 
+        'id' or 'head' values are not 
+        integers will be discarded.
     :return: List[Text]
     """
     texts = []
@@ -242,7 +261,17 @@ def conll_to_texts_list(file: str, syntax_layer: str = 'conll_syntax', postcorre
                 base_span = ElementaryBaseSpan(cur, cur+len_w)
                 words_layers[-1].add_annotation(base_span)
 
-                syntax_layers[-1].add_annotation(base_span, **w)
+                ids_ok = True
+                if discard_malformed_ids:
+                    # Check that both 'id' and 'head' are integers.
+                    # If not, discard annotation as broken.
+                    ids_ok = isinstance(w['id'], int) and \
+                             isinstance(w['head'], int)
+                if ids_ok:
+                    syntax_layers[-1].add_annotation(base_span, **w)
+                else:
+                    warnings.warn(('(!) Unexpected id={!r}, head={!r} in token {!r} at position {}. '+\
+                                   'Discarding broken annotation.').format(w['id'], w['head'], w['form'], cur))
                 cur += len_w + 1
 
             sentences_layers[-1].add_annotation(words[sentence_start:])
