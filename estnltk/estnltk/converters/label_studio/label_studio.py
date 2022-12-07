c70304294dca720be4b3f08777b349eceb8d3a9b
rasmusmaide <36761599+rasmusmaide@users.noreply.github.com>
2022-12-07 10:35:44 +0100

    Add checkbox and reformat labelstudio converter

--- estnltk/estnltk/converters/label_studio/label_studio.py
+++ estnltk/estnltk/converters/label_studio/label_studio.py
@@ -6,31 +6,40 @@ import random
 
 class LabelStudioExporter:
 
-    def __init__(self, filename: str, texts: List[Text], layer: str, label_level: str = 'annotation',
+    def __init__(self, filename: str, layer: str, label_level: str = 'annotation',
                  attribute: str = None,
-                 attribute_values: List = None):
+                 attribute_values: List = None,checkbox=False):
         if attribute is not None and attribute_values is None:
             raise RuntimeError(
                 "If attribute is given then possible attribute values must also be passed as an argument")
         self.filename = filename
-        self.texts = texts
         self.layer = layer
         self.attribute_values = attribute_values
         self.label_level = label_level
         self.attribute = attribute
 
-        self.labeling_interface = self.interface_generator()
+        self.labeling_interface = self.interface_generator(checkbox)
 
-    def interface_generator(self):
+    def interface_generator(self, checkbox = False):
 
         single_label = '\t<Label value="{label_value}" background="{background_value}"/> \n'
         conf_string = """
         <View>
             <Labels name="label" toName="text">\n"""
-        end_block = """
+        mid_block = """
             </Labels>
         <Text name="text" value="$text"/>
-        </View>"""
+            """
+        if checkbox:
+            end_block = """<Header value="Are the annotations correct?"/>
+                <Choices name="review" toName="text">
+                    <Choice value="yes"/>
+                    <Choice value="no"/>
+                </Choices>
+            </View>"""
+        else:
+            end_block = """
+            </View>"""
 
         if self.attribute_values is None:
             conf_string += single_label.format(
@@ -44,23 +53,34 @@ class LabelStudioExporter:
                     background_value=("#" + "%06x" % random.randint(0, 0xFFFFFF)).upper()
                 )
 
+        conf_string += mid_block
         conf_string += end_block
 
         return conf_string
 
-    def convert(self):
-        try:
-            output = open(self.filename, "wt")
-        except OSError:
-            raise ValueError("Could not open/read file: {}".format(self.filename))
-
-        json.dump([
+    def convert(self, texts, append=False):
+        if append:
+            with open(self.filename, "r") as input:
+                existing = input.readlines()
+            existing = "\n".join(existing)
+            new_str = json.dumps([
             text_to_dict(text, text[self.layer], self.attribute)
-            for text in self.texts], output, indent=2)
+            for text in texts])
+            existing = existing[:-1]
+            new_str = new_str[1:]
+            new_str = existing + "," + new_str
+            with open(self.filename, "w") as output:
+                output.write(new_str)
+        else:
+            with open(self.filename, "wt") as output:
+                json.dump([
+                text_to_dict(text, text[self.layer], i, self.attribute)
+                for i,text in enumerate(texts)], output, indent=2)
 
 
 def text_to_dict(
         text: Text, layer: Layer,
+        idx: int,
         attribute: str = None,
         text_name: str = 'text',
         labelset_name: str = 'label') -> dict:
@@ -72,9 +92,11 @@ def text_to_dict(
     for span in layer:
 
         all_attributes = ['start', 'end', 'text']
-        all_attributes.append(attribute)
-
+        if attribute is not None:
+            all_attributes.append(attribute)
         annotation = {key: getattr(span, key) for key in all_attributes}
+        annotation['idx'] = idx
+
         if attribute is None:
             annotation['labels'] = [layer.name]
         else:
