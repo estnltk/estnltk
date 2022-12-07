701ce33b36c0cdef359ee58f32d245ca329c11b0
Siim Orasmaa <siim.orasmaa@ut.ee>
2022-12-07 11:23:39 +0200

    Refactored experimental DateTagger: use new RegexTagger instead of the legacy one

--- estnltk/estnltk/taggers/miscellaneous/date_tagger/date_tagger.py
+++ estnltk/estnltk/taggers/miscellaneous/date_tagger/date_tagger.py
@@ -1,51 +1,56 @@
 import datetime
+import regex
 
-from estnltk.legacy.dict_taggers.regex_tagger import RegexTagger
-from estnltk.taggers.miscellaneous.date_tagger.regexes_v import regexes
+from estnltk.taggers.miscellaneous.date_tagger.regexes_v import regexes as REGEXES_VOC
+
+from estnltk.taggers.system.rule_taggers import RegexTagger, Ruleset
+from estnltk.taggers.system.rule_taggers import StaticExtractionRule
+
+
+def _global_decorator(text, base_span, annotation):
+    assert 'match' in annotation and annotation['match'] is not None
+    # Fill in dynamic attributes
+    annotation['groups'] = annotation['groups']( annotation['match'] )
+    annotation['date_text'] = annotation['date_text']( annotation['match'] )
+    annotation['extracted_values'] = annotation['extracted_values']( annotation['match'] )
+    return annotation
 
 
 class DateTagger(RegexTagger):
-    """Tags date and time expressions.
-
-    """
+    """Tags date and time expressions (experimental)."""
 
     __slots__ = []
 
-    def __init__(self, output_layer='dates', conflict_resolving_strategy='MAX', overlapped=False):
-        regexes_dict = regexes.reset_index().to_dict('records')
-        vocabulary = self._create_vocabulary(regexes_dict)
-
-        super().__init__(vocabulary=vocabulary,
+    def __init__(self, output_layer='dates', conflict_resolver: str='KEEP_MAXIMAL', overlapped: bool=False):
+        ruleset = DateTagger._create_extraction_rules( REGEXES_VOC )
+        super().__init__(ruleset=ruleset, 
                          output_layer=output_layer,
-                         output_attributes=['date_text', 'type', 'probability', 'groups', 'extracted_values'],
-                         conflict_resolving_strategy=conflict_resolving_strategy,
-                         overlapped=overlapped,
-                         )
+                         output_attributes=('date_text', 'type', 'probability', 'groups', 'extracted_values'),
+                         conflict_resolver=conflict_resolver,
+                         decorator=_global_decorator,
+                         overlapped=overlapped)
 
-    def _create_vocabulary(self, regexes):
-        """Creates _vocabulary for regex_tagger
-
-        """
-        vocabulary = []
-        for record in regexes:
-            rec = {'_regex_pattern_': record['regex'],
-                   '_group_': 0,
-                   '_priority_': (0, 0),
-                   'groups': lambda m: m.groupdict(),
-                   'date_text': lambda m: m.group(0),
-                   'type': record['type'],
-                   'probability': record['probability'],
-                   'example': record['example'],
-                   'extracted_values': lambda m: self._extract_values(m)
-                   }
-            vocabulary.append(rec)
-        return vocabulary
+    @staticmethod
+    def _create_extraction_rules(vocabulary):
+        """Creates extraction rules for regex_tagger."""
+        rules = []
+        for voc_item in vocabulary:
+            rule = StaticExtractionRule( pattern=regex.Regex( voc_item['regex'] ), 
+                                         group=0, 
+                                         attributes={ 'date_text': lambda m: m.group(0), 
+                                                      'type': voc_item['type'], 
+                                                      'probability': voc_item['probability'], 
+                                                      'groups': lambda m: m.groupdict(), 
+                                                      'extracted_values': lambda m: DateTagger._extract_values(m)
+                                                      } )
+            rules.append(rule)
+        ruleset = Ruleset()
+        ruleset.add_rules(rules)
+        return ruleset
 
     @staticmethod
     def _clean_year(yearstring):
-        """If year is two digits, adds 1900 or 2000
-
-        """
+        """If year is two digits, adds 1900 or 2000."""
         year = int(yearstring)
         if year < 100:
             if year < 30:
@@ -54,14 +59,13 @@ class DateTagger(RegexTagger):
                 year += 1900
         return year
 
-    def _extract_values(self, match):
-        """Extracts datetime, date or time values from regex matches if possible
-
-        """
+    @staticmethod
+    def _extract_values(match):
+        """Extracts datetime, date or time values from regex matches if possible."""
         d = match.groupdict()
         if 'YEAR' in d or 'LONGYEAR' in d:
             if 'YEAR' in d:
-                year = self._clean_year(d['YEAR'])
+                year = DateTagger._clean_year(d['YEAR'])
             else:
                 year = int(d['LONGYEAR'])
             if 'DAY' in d:
