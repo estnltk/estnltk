372d4a417b8d05e0658c7bf47eab6352cdf57490
Siim Orasmaa <siim.orasmaa@ut.ee>
2023-02-15 16:27:24 +0200

    Added tests for basic API of RelationsLayer

--- estnltk_core/estnltk_core/tests/layer/test_relations_layer.py
+++ estnltk_core/estnltk_core/tests/layer/test_relations_layer.py
@@ -3,9 +3,95 @@ import pytest
 import re
 from copy import copy, deepcopy
 
+from estnltk_core import ElementaryBaseSpan
 from estnltk_core import RelationsLayer, Relation
 
 from estnltk_core.common import load_text_class
+
+
+def test_relations_layer_basic():
+    # Test basic API of the relations layer (w/o Text object)
+    
+    # Test creating layer:
+    layer = RelationsLayer('test', span_names=['arg0', 'arg1'], 
+                                   attributes=['attr1', 'attr2'])
+    assert layer.span_names == ('arg0', 'arg1')
+    assert layer.attributes == ('attr1', 'attr2')
+    assert layer.ambiguous == False
+    assert len(layer) == 0
+    assert layer.span_level is None
+    
+    with pytest.raises(AssertionError):
+        # error: span_names and attribute names cannot overlap
+        layer2 = RelationsLayer('test2', span_names=['arg0', 'arg1'], 
+                                         attributes=['arg1', 'attr1', 'attr2'])
+
+    # Test adding annotations:
+    # add relation annotation via dict 
+    layer.add_annotation( {'arg0': (0, 4), 'arg1': (6, 12), 'attr1': 1})
+    # add relation annotation via kwargs
+    layer.add_annotation( arg0=(20, 24), arg1=(26, 32), attr1=2, attr2=2 )
+    # combine dict and keywords, skip one arg
+    layer.add_annotation( {'arg0': (14, 18), 'attr1': 3}, attr2=3 )
+    # leave annotations empty
+    layer.add_annotation( arg0=(40, 44), arg1=(46, 52) )
+
+    assert len(layer) == 4
+    assert layer.span_level == 0
+    assert [sp.as_tuple for sp in layer[0].spans] == \
+            [('arg0', ElementaryBaseSpan(0, 4)),   ('arg1', ElementaryBaseSpan(6, 12))]
+    assert [sp.as_tuple for sp in layer[1].spans] == \
+            [('arg0', ElementaryBaseSpan(20, 24)), ('arg1', ElementaryBaseSpan(26, 32))]
+    assert [sp.as_tuple for sp in layer[2].spans] == \
+            [('arg0', ElementaryBaseSpan(14, 18))]
+    assert [sp.as_tuple for sp in layer[3].spans] == \
+            [('arg0', ElementaryBaseSpan(40, 44)), ('arg1', ElementaryBaseSpan(46, 52))]
+    assert [dict(a) for a in layer[0].annotations] == [{'attr1': 1, 'attr2': None}]
+    assert [dict(a) for a in layer[1].annotations] == [{'attr1': 2, 'attr2': 2}]
+    assert [dict(a) for a in layer[2].annotations] == [{'attr1': 3, 'attr2': 3}]
+    assert [dict(a) for a in layer[3].annotations] == [{'attr1': None, 'attr2': None}]
+
+    with pytest.raises(ValueError):
+        # error: named span(s) cannot be missing
+        layer.add_annotation( {'attr1': 4, 'attr2': 4} )
+    with pytest.raises(ValueError):
+        # error: layer has different span level than newly addable spans
+        layer.add_annotation( {'arg0': [(50, 54), (56, 64)], 'attr1': 4} )
+    with pytest.raises(TypeError):
+        # error: wrong type for named span
+        layer.add_annotation( {'arg0': 1, 'attr1': 4} )
+    with pytest.raises(ValueError):
+        # error: wrong type for named span
+        layer.add_annotation( {'arg1': None, 'attr1': 4} )
+    with pytest.raises(Exception):
+        # error: cannot add more than one annotation to unambiguous layer
+        layer.add_annotation( arg0=(20, 24), arg1=(26, 32), attr1=4, attr2=4 )
+
+
+def test_relations_layer_basic_with_text_obj():
+    # Test basic API of the relations layer (with Text object)
+    
+    # Load Text or BaseText class (depending on the available packages)
+    Text = load_text_class()
+    
+    text = Text('0123456789')
+    layer = RelationsLayer('test', span_names=['arg0', 'arg1'], 
+                                   attributes=['summa'],
+                                   text_object=text)
+    layer.add_annotation( {'arg0': (0, 1), 'arg1': (1, 2), 'summa': 1} )
+    layer.add_annotation( {'arg0': (2, 3), 'arg1': (3, 4), 'summa': 5} )
+    layer.add_annotation( {'arg0': (5, 6), 'arg1': (2, 3), 'summa': 7} )
+    layer.add_annotation( {'arg0': (5, 6), 'arg1': (4, 5), 'summa': 9} )
+    relations = []
+    for rel in layer:
+        relation_dict = {**{sp: rel[sp].text for sp in layer.span_names},
+                         **{a: rel[a] for a in layer.attributes} }
+        relations.append(relation_dict)
+    assert relations == \
+        [{'arg0': '0', 'arg1': '1', 'summa': 1}, \
+         {'arg0': '2', 'arg1': '3', 'summa': 5}, \
+         {'arg0': '5', 'arg1': '2', 'summa': 7}, \
+         {'arg0': '5', 'arg1': '4', 'summa': 9}]
 
 
 def test_relations_layer_deep_copy():
