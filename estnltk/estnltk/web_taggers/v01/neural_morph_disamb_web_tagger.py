2d1da4a68475d1234d0b47b1088ff6e1fee40c34
Siim Orasmaa <siim.orasmaa@ut.ee>
2023-03-23 11:02:33 +0200

    Updated NeuralMorphDisambWebTagger's docs and tests

--- estnltk/estnltk/web_taggers/v01/neural_morph_disamb_web_tagger.py
+++ estnltk/estnltk/web_taggers/v01/neural_morph_disamb_web_tagger.py
@@ -7,11 +7,21 @@ from estnltk.web_taggers import BatchProcessingWebTagger
 
 class NeuralMorphDisambWebTagger( BatchProcessingWebTagger, Retagger ):
     """
-    Tags neural morph disambiguation using EstNLTK NeuralMorphTagger's webservice. 
+    Performs neural morphological disambiguation using EstNLTK NeuralMorphTagger's webservice. 
     Uses softmax emb_cat_sum model.
-
     Note: this model resolves only Vabamorf's partofspeech and form ambiguities, 
     lemma ambiguities will remain unresolved.
+
+    NeuralMorphDisambWebTagger can be used either as a tagger or as a retagger: 
+    *) if used as a tagger (output_layer != morph_analysis_layer), then 
+       tagger.tag(...) / tagger.make_layer(...) methods create a new morph layer 
+       with attributes ('morphtag', 'pos', 'form'). Attributes ('pos', 'form') 
+       are based on Vabamorf's categories, and can be used to disambiguate the 
+       input morph_analysis layer; the attribute 'morphtag' contains model-
+       specific morphological features (mixed Vabamorf and UD categories);
+    *) if used as a retagger (set output_layer = morph_analysis_layer), then 
+       tagger.retag(...) / tagger.change_layer(...) methods disambiguate the 
+       input morph_analysis layer; 
 
     See also Neural Morphological Tagger's documentation:
     https://github.com/estnltk/estnltk/blob/539ae945aa52df43b94699866a6f43d021807894/tutorials/nlp_pipeline/B_morphology/08_neural_morph_tagger_py37.ipynb
@@ -32,11 +42,18 @@ class NeuralMorphDisambWebTagger( BatchProcessingWebTagger, Retagger ):
         self.batch_enveloping_layer = self.input_layers[1]
 
     def _change_layer(self, text, layers, status):
+        # Validate inputs
         if self.output_layer != self.input_layers[2]:
             raise Exception( ('(!) Mismatching output_layer and input_morph_analysis_layer {!r} != {!r}. '+\
                              'Cannot use as a retagger.').format(self.output_layer, self.input_layers[2]) )
-        disamb_layer = self._make_layer(text, layers, status)
         morph_layer = layers[self.output_layer]
+        for attr in ['partofspeech', 'form']:
+            if attr not in morph_layer.attributes:
+                raise Exception( ('(!) Missing attribute {!r} in input_morph_analysis_layer {!r}.'+\
+                                  '').format(attr, morph_layer.name) )
+        # Create disambiguation layer
+        disamb_layer = self._make_layer(text, layers, status)
+        # Disambiguate input_morph_analysis_layer
         assert len(morph_layer) == len(disamb_layer)
         for original_word, disamb_word in zip(morph_layer, disamb_layer):
             disamb_pos  = disamb_word.annotations[0]['pos']
