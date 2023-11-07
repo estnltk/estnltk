00e9e53c014f85eaf8a0ba4d80630845494a735e
Siim Orasmaa <siim.orasmaa@ut.ee>
2023-11-07 15:53:01 +0200

    Fixed EstBERTNERTagger: added proper input tokenization that does not fail on rare unicode symbols

--- estnltk_neural/estnltk_neural/tests/taggers/ner/test_estbert_ner_tagger.py
+++ estnltk_neural/estnltk_neural/tests/taggers/ner/test_estbert_ner_tagger.py
@@ -86,6 +86,29 @@ def test_estbertner_v2_smoke():
          (221, 231, '"Loomaaed"', 'PROD')]
 
 
+@pytest.mark.skipif(not check_if_transformers_is_available(),
+                    reason="package tranformers is required for this test")
+@pytest.mark.skipif(not check_if_pytorch_is_available(),
+                    reason="package pytorch is required for this test")
+@pytest.mark.skipif(ESTBERTNER_V2_PATH is None,
+                    reason="EstBERTNERTagger's model location not known. "+\
+                           "Use estnltk.download('estbertner_v2') to get the missing resources.")
+def test_estbertner_v2_tokenization_fail():
+    # Test EstBERTNERTagger on texts that contain rare unicode symbols. 
+    # Previous versions of the tagger crashed on these symbols. 
+    from estnltk_neural.taggers import EstBERTNERTagger
+    neural_ner_tagger = EstBERTNERTagger( model_location=ESTBERTNER_V2_PATH )
+    text = Text('Gröönimaa - Magssanguaq Qujaukitsoq ; Island - Žórarinn Eldjárn ; Soome - Joni Pyysalo ; '+\
+                'Läti - Kārlis Vērdiņš ; Laïta jõe suudme vaheline rannikuala ; Yangi­Millsi kalibratsioonivälja ; '+\
+                'arbitrio sobre la productión y sobre las importaciones').tag_layer('words')
+    neural_ner_tagger.tag(text)
+    output_layer = neural_ner_tagger.output_layers[0]
+    assert len(text[output_layer]) > 0
+    # Note: although the model does not detect all the entities, at least 
+    # it does not crash on the input
+    #print( _ner_spans_as_tuples( text[output_layer] ) )
+
+
 # ========================================================================
 #   Test postfixes. Note: these postfixes are only required if we 
 #   do not align Bert tokens to EstNLTK's words.
