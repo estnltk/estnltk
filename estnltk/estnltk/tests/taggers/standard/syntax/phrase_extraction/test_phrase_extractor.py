a0ae4f5d54b06052b4d3d298fa682fd468846de5
Siim Orasmaa <siim.orasmaa@ut.ee>
2023-11-21 14:24:25 +0200

    Updated PhraseExtractor's unit tests

--- estnltk/estnltk/tests/taggers/standard/syntax/phrase_extraction/test_phrase_extractor.py
+++ estnltk/estnltk/tests/taggers/standard/syntax/phrase_extraction/test_phrase_extractor.py
@@ -1,12 +1,24 @@
 import os, os.path
-import pytest
+import unittest
+import pkgutil
 
 from estnltk import Text
 from estnltk.converters import dict_to_text, text_to_dict
+from estnltk.downloader import get_resource_paths
+
 from estnltk.taggers.standard.syntax.phrase_extraction.phrase_extractor import PhraseExtractor
+
+# Try to get the resources path for stanzasyntaxtagger. If missing, do nothing. It's up for the user to download the missing resources
+STANZA_SYNTAX_MODELS_PATH = get_resource_paths("stanzasyntaxtagger", only_latest=True, download_missing=False)
+
+def check_if_estnltk_neural_is_available():
+    return pkgutil.find_loader("estnltk_neural") is not None
+
 
 def test_syntax_phrase_extractor_smoke():
     # Smoke test that PhraseExtractor works on (manually corrected) conllu syntax
+    # Based on: 
+    #   https://github.com/estnltk/estnltk/blob/93a9435c353c6b0c39fdf2596ca9b9f48bcf8205/estnltk/estnltk/taggers/standard/syntax/phrase_extraction/tests/phrase_extractor_jupyter_tests.ipynb
     phrase_tagger1 = PhraseExtractor(deprel="obl", 
                                      syntax_layer="conll_syntax", 
                                      sentences_layer='sentences',
@@ -335,4 +347,48 @@ def test_syntax_phrase_extractor_smoke():
     assert list(sentence2.obl_phrases[0].text) == ['29.', 'mail'], list(sentence2.obl_phrases[0].text)
     assert len(sentence2.obl_phrases[1]) == 1, len(sentence2.obl_phrases[1])
     assert list(sentence2.obl_phrases[1].text) == ['Väike-Maarjas'], list(sentence2.obl_phrases[1].text)
-    
+
+
+
+@unittest.skipIf(not check_if_estnltk_neural_is_available(),
+                 reason="estnltk_neural is not installed. You'll need estnltk_neural for running this test.")
+@unittest.skipIf(STANZA_SYNTAX_MODELS_PATH is None,
+                 reason="StanzaSyntaxTagger's model is required by this test. Use estnltk.download('stanzasyntaxtagger') to fetch the missing resource.")
+def test_syntax_phrase_extractor_on_stanza_syntax():
+    # Test that PhraseExtractor works on the output of StanzaSyntaxTagger (requires estnltk_neural)
+    # Based on: 
+    #   https://github.com/estnltk/estnltk/blob/93a9435c353c6b0c39fdf2596ca9b9f48bcf8205/estnltk/estnltk/taggers/standard/syntax/phrase_extraction/tests/phrase_extractor_jupyter_tests.ipynb
+    from estnltk_core.layer_operations import split_by_sentences
+    from estnltk_neural.taggers import StanzaSyntaxTagger
+    stanza_tagger = StanzaSyntaxTagger(input_type="morph_extended", input_morph_layer="morph_extended", add_parent_and_children=True)
+    phrase_tagger2 = PhraseExtractor(deprel="obl", syntax_layer="stanza_syntax", output_layer="obl_phrases")
+    three_sentences = Text('Kolme aastaga on Eminem alias Marshall Mathers III ( 30 ) kindlalt meie teadvusesse sööbinud . '+\
+                           'Aga ma sain sellest nõiaringist välja . '+\
+                           'Oleksin võinud vangi sattuda .')
+    three_sentences.tag_layer('sentences')
+    assert len( three_sentences['sentences'] ) == 3
+    three_sentences.tag_layer('morph_extended')
+    stanza_tagger.tag( three_sentences )
+    phrase_tagger2.tag( three_sentences )
+    split_sentences = split_by_sentences(text=three_sentences,
+                                         layers_to_keep=list(three_sentences.layers),
+                                         trim_overlapping=True)
+    assert len(split_sentences) == 3
+    # Validate sentence Text objects
+    txt1 = split_sentences[0]
+    assert len(txt1.obl_phrases) == 2, len(txt1.obl_phrases)
+    assert list(txt1.obl_phrases[0].text) == ['Kolme', 'aastaga'], list(txt1.obl_phrases[0].text)
+    assert len(txt1.obl_phrases[0]) == 2, len(txt1.obl_phrases[0])
+    assert len(txt1.obl_phrases[1]) == 2, len(txt1.obl_phrases[1])
+    assert list(txt1.obl_phrases[1].text) == ['meie', 'teadvusesse'], list(txt1.obl_phrases[1].text)
+    txt2 = split_sentences[1]
+    assert len(txt2.obl_phrases) == 2, len(txt2.obl_phrases)
+    assert list(txt2.obl_phrases[0].text) == ['sellest'], list(txt2.obl_phrases[0].text)
+    assert len(txt2.obl_phrases[0]) == 1, len(txt2.obl_phrases[0])
+    assert len(txt2.obl_phrases[1]) == 1, len(txt2.obl_phrases[1])
+    assert list(txt2.obl_phrases[1].text) == ['nõiaringist'], list(txt2.obl_phrases[1].text)
+    txt3 = split_sentences[2]
+    assert len(txt3.obl_phrases) == 1, len(txt3.obl_phrases)
+    assert list(txt3.obl_phrases[0].text) == ['vangi'], list(txt3.obl_phrases[0].text)
+    assert len(txt3.obl_phrases[0]) == 1, len(txt3.obl_phrases[0])
+
