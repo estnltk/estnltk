e47ca7d4e04609062c4069059026e113cbe2ba94
Siim Orasmaa <siim.orasmaa@ut.ee>
2024-02-16 14:24:02 +0200

    Removed PostgresStorage import from estnltk.storage init

--- estnltk/estnltk/storage/__init__.py
+++ estnltk/estnltk/storage/__init__.py
@@ -4,4 +4,6 @@ Module enables to store and search `Text` objects in Postgres database.
 For usage examples, see tutorial `estnltk/tutorials/storing_text_objects_in_postgres.ipynb`
 """
 
-from estnltk.storage.postgres.storage import PostgresStorage
+# DO not import PostgresStorage, avoid ModuleNotFoundError: No module named 'psycopg2'
+# (people should be able to import estnltk.storage.sqlite without running into this error)
+#from estnltk.storage.postgres.storage import PostgresStorage
