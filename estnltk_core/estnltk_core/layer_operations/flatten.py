6516fd85f043092821e05947b47055828e69ffa8
Siim Orasmaa <siim.orasmaa@ut.ee>
2025-12-04 15:48:07 +0200

    Updated flatten: added parameter gaps_strategy/gaps_pattern for cutting out gaps within enveloping spans

--- estnltk_core/estnltk_core/layer_operations/flatten.py
+++ estnltk_core/estnltk_core/layer_operations/flatten.py
@@ -1,17 +1,27 @@
 from typing import Sequence, Union
+from itertools import pairwise
 from estnltk_core.layer.base_span import ElementaryBaseSpan
+from estnltk_core.layer.base_span import EnvelopingBaseSpan
 
 
 def flatten(input_layer: Union['BaseLayer', 'Layer'], output_layer: str, output_attributes: Sequence[str] = None,
             attribute_mapping: Sequence = None, default_values: dict = None,
-            disambiguation_strategy:str = None ) -> Union['BaseLayer', 'Layer']:
-    """Reduces enveloping layer or layer with parent to a detached ambiguous layer of simple text spans.
+            disambiguation_strategy:str = None, gaps_strategy:str = None, 
+            gaps_pattern:str = r'\s*\S.*\s*') -> Union['BaseLayer', 'Layer']:
+    r"""Reduces enveloping layer or layer with parent to a detached ambiguous layer of simple text spans.
     
        Note: By default, the output layer will be ambiguous. However, if you set 
        disambiguation_strategy = 'pick_first', then only the first annotation of 
        every span will be preserved and an unambiguous layer will be returned. 
        If you need to customize the disambiguation strategy, use a 
        Disambiguator. 
+
+       Note: If the input layer is enveloping and contains discontinuous text 
+       spans, the output layer will still have continuous text spans, covering 
+       all the gaps between spans. 
+       However, if you set gaps_strategy = 'cut_out', then gaps between 
+       discontinuous spans (indicated by non-whitspace) will be cut out, 
+       splitting spans correspondingly. 
        
        Parameters
        ----------
@@ -35,14 +45,51 @@ def flatten(input_layer: Union['BaseLayer', 'Layer'], output_layer: str, output_
            the output layer will be ambiguous.
            If set to 'pick_first', then the first annotation of every span will be 
            preserved.
-
+       gaps_strategy: str
+           Specifies strategy for handling gaps inside an enveloping span of an 
+           enveloping layer. 
+           By default, there is no strategy and even if there is a gap between 
+           two consecutive sub-spans of an enveloping span, the new flat span 
+           will cover the gap entirely. For instance, in case of clauses layer 
+           with an embedded clause ("[Mees, [keda seal kohtasime,] oli tuttav.]"), 
+           the flat clauses will be: ['Mees, keda seal kohtasime, oli tuttav.', 
+           ', keda seal kohtasime,'], so the first flat clause covers entirely 
+           the embedded clause.
+           If set to 'cut_out', then all consecutive sub-spans that have a gap 
+           between them will be split into two spans. the existence of the gap 
+           is checked with the gaps_pattern.
+           For instance, in case of clauses layer with an embedded clause 
+           ("[Mees, [keda seal kohtasime,] oli tuttav.]"), the flat clauses will 
+           be: ['Mees', ', keda seal kohtasime,', 'oli tuttav.'].
+       gaps_pattern :str
+           String with regular expression pattern that can be used for detecting 
+           gaps between two sub-spans of an enveloping span. 
+           Defaults to string '\s*\S.*\s*'.
        Returns
        -------
        Union['BaseLayer', 'Layer']
             flattened version of the input_layer
     """
+    # Prepare gaps_strategy [Optional]
+    gaps_regex = None
+    if gaps_strategy is not None and \
+       gaps_strategy.lower() == 'cut_out':
+           # Sanity check
+           if input_layer.enveloping is None:
+               raise Exception(f'(!) Cannot use gaps_strategy="cut_out": the input layer {input_layer.name!r} is not enveloping layer.')
+           if input_layer.text_object is None:
+               raise Exception(f'(!) Cannot use gaps_strategy="cut_out": the input layer {input_layer.name!r} is not attached to a Text object.')
+           # Compile the regex for detecting gaps
+           import regex as re
+           if isinstance(gaps_pattern, str):
+               gaps_regex = re.compile( gaps_pattern )
+           elif isinstance(gaps_pattern, re.regex.Pattern):
+               gaps_regex = gaps_pattern
+           if gaps_regex is None:
+               raise Exception(f'(!) Cannot use gaps_strategy="cut_out": please provide a valid gaps_pattern for detecting gaps in text.')
+           assert isinstance(gaps_regex, re.regex.Pattern)
+    # Prepare layer
     layer_attributes = input_layer.attributes
-
     output_attributes = output_attributes or layer_attributes
     # Create new BaseLayer or Layer
     new_layer = input_layer.__class__(name=output_layer,
@@ -61,14 +108,34 @@ def flatten(input_layer: Union['BaseLayer', 'Layer'], output_layer: str, output_
         assert {attr for _, attr in attribute_mapping} <= set(output_attributes)
 
     for span in input_layer:
-        for annotation_id, annotation in enumerate( span.annotations ):
-            attrs = {new_attr: getattr(annotation, old_attr) for old_attr, new_attr, in attribute_mapping}
-            new_layer.add_annotation(ElementaryBaseSpan(span.start, span.end), **attrs)
-            if annotation_id == 0 and \
-               disambiguation_strategy is not None and \
-               disambiguation_strategy.lower() == 'pick_first':
-                # Cancel after the first annotation has been added
-                break
+        base_spans = [ElementaryBaseSpan(span.start, span.end)]
+        if gaps_strategy is not None and \
+           gaps_strategy.lower() == 'cut_out':
+            # Split enveloping spans into chunks of continuous spans [Optional]
+            new_base_spans = []
+            assert isinstance(span.base_span, EnvelopingBaseSpan)
+            last_start = span.base_span[0].start
+            for (sub_span_1, sub_span_2) in pairwise( span.base_span ):
+                gap_str = input_layer.text_object.text[sub_span_1.end:sub_span_2.start]
+                if gaps_regex.match(gap_str):
+                    new_base_spans.append( ElementaryBaseSpan(last_start, sub_span_1.end) )
+                    last_start = sub_span_2.start
+            if len(new_base_spans) > 0:
+                # gaps detected: add the last base span
+                new_base_spans.append( ElementaryBaseSpan(last_start, span.base_span[-1].end) )
+                assert new_base_spans[0].start == base_spans[0].start
+                assert new_base_spans[-1].end == base_spans[-1].end
+                base_spans = new_base_spans
+        # Add annotations
+        for base_span in base_spans:
+            for annotation_id, annotation in enumerate( span.annotations ):
+                attrs = {new_attr: getattr(annotation, old_attr) for old_attr, new_attr, in attribute_mapping}
+                new_layer.add_annotation(base_span, **attrs)
+                if annotation_id == 0 and \
+                   disambiguation_strategy is not None and \
+                   disambiguation_strategy.lower() == 'pick_first':
+                    # Cancel after the first annotation has been added
+                    break
 
     if disambiguation_strategy is not None and \
        disambiguation_strategy.lower() == 'pick_first':
--- estnltk_core/estnltk_core/tests/layer_operations/test_flattening.py
+++ estnltk_core/estnltk_core/tests/layer_operations/test_flattening.py
@@ -71,3 +71,112 @@ def test_flatten_pick_first_disambiguation():
     assert len(result[1].annotations) == 1
     assert len(result[2].annotations) == 1
 
+
+
+def test_flatten_cut_out_gaps_strategy():
+    # Load Text or BaseText class (depending on the available packages)
+    Text = load_text_class()
+
+    #
+    # Case 1: simple nested/embedded structure
+    #
+    text = Text('Kõik suud, kes meie asjast teawad, on täieste kinni.')
+
+    layer_words = Layer('words', text_object=text)
+    layer_words.add_annotation( (0, 4) )
+    layer_words.add_annotation( (5, 9) )
+    layer_words.add_annotation( (9, 10) )
+    layer_words.add_annotation( (11, 14) )
+    layer_words.add_annotation( (15, 19) )
+    layer_words.add_annotation( (20, 26) )
+    layer_words.add_annotation( (27, 33) )
+    layer_words.add_annotation( (33, 34) )
+    layer_words.add_annotation( (35, 37) )
+    layer_words.add_annotation( (38, 45) )
+    layer_words.add_annotation( (46, 51) )
+    layer_words.add_annotation( (51, 52) )
+    text.add_layer( layer_words )
+
+    layer_clauses = Layer( 'clauses', \
+                           text_object=text, \
+                           attributes=('clause_type',), \
+                           enveloping='words' )
+    layer_clauses.add_annotation( \
+                [ (0, 4), (5, 9), (35, 37), (38, 45), (46, 51), (51, 52) ], \
+                {'clause_type': 'regular'} )
+    layer_clauses.add_annotation( \
+                [ (9, 10), (11, 14), (15, 19), (20, 26), (27, 33), (33, 34) ], \
+                {'clause_type': 'embedded'} )
+
+    text.add_layer( layer_clauses )
+
+    #
+    # Default behaviour: Flatten without specifying gaps_strategy
+    #
+    result1 = flatten( layer_clauses, 'flatten_clauses_1' )
+    assert len(result1) == 2
+    assert result1[0].enclosing_text == 'Kõik suud, kes meie asjast teawad, on täieste kinni.'
+    assert result1[1].enclosing_text == ', kes meie asjast teawad,'
+    # Note that the result is not entirely correct as one span overlaps with another
+
+    #
+    # Flatten with specifying gaps_strategy='cut_out'
+    #
+    result2 = flatten( layer_clauses, 'flatten_clauses_2', gaps_strategy='cut_out' )
+    assert len(result2) == 3
+    assert result2[0].enclosing_text == 'Kõik suud'
+    assert result2[1].enclosing_text == ', kes meie asjast teawad,'
+    assert result2[2].enclosing_text == 'on täieste kinni.'
+
+    #
+    # Case 2: deeply nested/embedded clause structure
+    #
+    text2 = Text('Ja (vt konspekti (lk 53 (1)))')
+
+    layer_words2 = Layer('words', text_object=text2)
+    layer_words2.add_annotation( (0, 2) )
+    layer_words2.add_annotation( (3, 4) )
+    layer_words2.add_annotation( (4, 6) )
+    layer_words2.add_annotation( (7, 16) )
+    layer_words2.add_annotation( (17, 18) )
+    layer_words2.add_annotation( (18, 20) )
+    layer_words2.add_annotation( (21, 23) )
+    layer_words2.add_annotation( (24, 25) )
+    layer_words2.add_annotation( (25, 26) )
+    layer_words2.add_annotation( (26, 27) )
+    layer_words2.add_annotation( (27, 28) )
+    layer_words2.add_annotation( (28, 29) )
+    text2.add_layer( layer_words2 )
+    
+    layer_clauses2 = Layer( 'clauses', \
+                            text_object=text2, \
+                            attributes=('clause_type',), \
+                            enveloping='words' )
+    layer_clauses2.add_annotation( \
+                [ (0, 2) ], {'clause_type': 'regular'} )
+    layer_clauses2.add_annotation( \
+                [ (3, 4), (4, 6), (7, 16), (28, 29) ], {'clause_type': 'embedded'} )
+    layer_clauses2.add_annotation( \
+                [ (17, 18), (18, 20), (21, 23), (27, 28) ], {'clause_type': 'embedded'} )
+    layer_clauses2.add_annotation( \
+                [ (24, 25), (25, 26), (26, 27) ], {'clause_type': 'embedded'} )
+    text2.add_layer( layer_clauses2 )
+
+    #
+    # Default behaviour: Flatten without specifying gaps_strategy
+    #
+    result3 = flatten( layer_clauses2, 'flatten_clauses_3' )
+    #print([s.enclosing_text for s in result3] )
+    assert len(result3) == 4
+    assert [s.enclosing_text for s in result3] == \
+        ['Ja', '(vt konspekti (lk 53 (1)))', '(lk 53 (1))', '(1)']
+
+    #
+    # Flatten with specifying gaps_strategy='cut_out'
+    #
+    result4 = flatten( layer_clauses2, 'flatten_clauses_4', gaps_strategy='cut_out' )
+    #print([s.enclosing_text for s in result4] )
+    assert len(result4) == 6
+    assert [s.enclosing_text for s in result4] == \
+        ['Ja', '(vt konspekti', '(lk 53', '(1)', ')', ')']
+
