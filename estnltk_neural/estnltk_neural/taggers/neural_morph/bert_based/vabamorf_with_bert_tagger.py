dd52f7bfb169aa36e44c30623a3663eabebf3922
Siim Orasmaa <siim.orasmaa@ut.ee>
2026-08-14 18:28:25 +0300

    Updated VabamorfWithBertTagger: fixed input layer handling, added more input parameters and improved docs

--- estnltk_neural/estnltk_neural/taggers/neural_morph/bert_based/vabamorf_with_bert_tagger.py
+++ estnltk_neural/estnltk_neural/taggers/neural_morph/bert_based/vabamorf_with_bert_tagger.py
@@ -1,15 +1,19 @@
+#
+#  Provides Vabamorf-based morphological analysis with Bert-based disambiguation. 
+#
 
 from estnltk import Text
-import estnltk
 from estnltk.taggers import Tagger
 from estnltk import Span, Layer, Text
 from estnltk.taggers.standard.morph_analysis.morf_common import IGNORE_ATTR
+from estnltk.taggers.standard.morph_analysis.morf_common import DEFAULT_PARAM_PHONETIC
+from estnltk.taggers.standard.morph_analysis.morf_common import DEFAULT_PARAM_COMPOUND
+from estnltk.taggers.standard.morph_analysis.morf_common import DEFAULT_PARAM_STEM
 import json
 
 from estnltk.taggers import VabamorfAnalyzer
 from estnltk.taggers import PostMorphAnalysisTagger
 from estnltk_neural.taggers import BertMorphTagger
-#from bert_morph_tagger import BertMorphTagger
 
 from estnltk.vabamorf.morf import Vabamorf
 from estnltk.taggers import CompoundTokenTagger
@@ -18,38 +22,123 @@ from estnltk.taggers import CompoundTokenTagger
 class VabamorfWithBertTagger(Tagger):
     """
     Tagger that creates and refines 'morph_analysis' layer with
-      1. VabamorfAnalyzer
-      2. PostMorphAnalysisTagger
-      3. BertMorphTagger
-      and returns 'morph_analysis' or user defined layer. 
-      PostMorphAnalysisTagger can be disabled using use_postanalysis=False (default True)
+     1. VabamorfAnalyzer
+     2. PostMorphAnalysisTagger
+     3. BertMorphTagger
+    and returns 'morph_analysis' or user defined layer. 
+    PostMorphAnalysisTagger can be disabled using use_postanalysis=False (default True).
+    
+    VabamorfAnalyzer will be analysed with the default parameters (guess = True,
+    propername = True, compound = True, phonetic = False, stem=False), except slang_lex, 
+    which can be changed. 
     """
 
-    conf_param = ['use_postanalysis', 'vabamorf', 'post_morph', 'bert', 'output_layer', 'slang_lex', 'device','vm_instance' , 'input_layers', 'correct_verb_annotation', 'change_to_bert_form']
+    conf_param = ['use_postanalysis', 'vabamorf', 'post_morph', 'bert_disamb', 'output_layer', 'slang_lex', "compound",
+                  "phonetic", "stem", 'device', 'vm_instance', 'input_layers', 'correct_verb_annotation', 'change_to_bert_form']
     output_layer = 'morph_analysis'
     output_attributes = ['normalized_text', 'lemma', 'root', 'root_tokens', 'ending', 'clitic', 'form', 'partofspeech']
     input_layers = ('words', 'sentences')
 
     def __init__(self,
-                post_morph=None,
-                 vabamorf = None,
-                 bert = None,
-                 vm_instance = None,
-                 use_postanalysis: bool=True,
-                 output_layer:str=output_layer,
-                 slang_lex: bool=True,
-                 device:str='cpu', # for gpu - 'cuda'
-                 input_layers=input_layers,
-                correct_verb_annotation: bool = True,
-                change_to_bert_form: bool = True,
-                ):
+                 output_layer:str = output_layer,
+                 input_layers: 'List[str]' = ['words', 'sentences'],
+                 input_compound_tokens_layer: str = None,
+                 compound: bool = DEFAULT_PARAM_COMPOUND,
+                 phonetic: bool = DEFAULT_PARAM_PHONETIC,
+                 stem: bool     =  DEFAULT_PARAM_STEM,
+                 vm_instance: 'estnltk.vabamorf.morf.Vabamorf' = None,
+                 slang_lex: bool = True,
+                 use_postanalysis: bool = True,
+                 device:str = 'cpu', # for gpu - 'cuda'
+                 correct_verb_annotation: bool = True,
+                 change_to_bert_form: bool = True):
+        """Initialize VabamorfWithBertTagger class.
+        
+        Parameters
+        ----------
+        output_layer: str (default: 'morph_analysis')
+            Name of the layer where analysis results are stored.
+        input_layers: List[str] (default: ['words', 'sentences'])
+            Names of the input words and sentences layers.
+        input_compound_tokens_layer: str (default: None)
+            Name of the input compound_tokens layer. 
+            If set to None (default), then no compound tokens layer is 
+            assumed to be attached to the input Text object, and the 
+            post_morph will use an empty compound tokens layer.
+            Otherwise, the input Text object should have that layer 
+            and it is also used by the post_morph as an input. 
+        compound: boolean (default: True)
+            VabamorfAnalyzer: Add compound word markers to root forms.
+        phonetic: boolean (default: False)
+            VabamorfAnalyzer: Add phonetic information to root forms.
+        stem: boolean (default: False)
+            VabamorfAnalyzer: Replace lemma with word stem in the 'root' and 
+            'root_tokens' (so called stem-based morphological analysis). 
+            In the stem-based analysis, inflectional forms are not normalized 
+            to their lemmas, but instead kept as they are, and only endings 
+            are separated from roots. 
+            For instance, with lemma-based analysis (default), the 
+            word 'läks' gets root='mine' (lemma='minema'); 
+            however, with the stem-based analysis, the word 'läks' 
+            gets root='läk' (with ending='s' and no lemma). 
+            Note that with stem-based analysis, there will be no 
+            lemmas in the output.
+        vm_instance: estnltk.vabamorf.morf.Vabamorf
+            An instance of Vabamorf that is to be used for 
+            analysing text morphologically. Note that if you 
+            provide a custom instance, the parameters compound, 
+            phonetic, stem and slang_lex will have no effect. 
+        slang_lex: boolean (default: False)
+            If True, then uses an extended version of Vabamorf's binary lexicon, 
+            which provides valid analyses to spoken and slang words, such as 
+            'kodukas', 'mõnsa', 'mersu', 'kippelt'. However, using "the slang 
+            lexicon" also hinders Vabamorf's ability to clearly distinguish 
+            between written language and slang words, and this is the reason 
+            that "the slang lexicon" is not switched on by default;
+            Note: this only works if you leave the parameter vm_instance 
+            unspecified;
+        use_postanalysis: boolean (default: True)
+            Whether PostMorphAnalysisTagger will be applied for post-correcting 
+            morph layer. Post-corrections will be applied after Vabamorf's morph 
+            analysis and before Bert-based disambiguation.
+        device (str):
+            The device to run the Bert model on ('cpu' or 'cuda'). 
+            Defaults to 'cpu'.
+        correct_verb_annotation (bool):
+            Bert-based disambiguation: if there is word multiplicity but not verb 
+            multiplicity and correct annotation could be chosen based on Bert 
+            partofspeech, then correct_verb_annotation=True will take the Vabamorf 
+            annotation that matches Bert prediction. The comparison is done 
+            with "neg" removed from Bert predicted "form".
+        change_to_bert_form (bool):
+            Bert-based disambiguation: In case of verbs: if there is multiplicity 
+            but not verb multiplicity and Bert prediction form contains "neg" while 
+            Vabamorf does not, then Vabamorf annotation will be changed to also 
+            include "neg" if change_to_bert_form=True. This is tested on UD treebank 
+            2.18 where out of 1945 words (with no verb multiplicity) 91.98% matched 
+            UD annotation if "neg" was exluded from Bert prediction. 
+        """
 
         self.output_layer = output_layer
-        self.slang_lex = slang_lex
-        self.device = device
-        self.vm_instance = vm_instance
-        self.input_layers=input_layers
-
+        self.compound     = compound
+        self.phonetic     = phonetic
+        self.stem         = stem
+        self.slang_lex    = slang_lex
+        self.device       = device
+        self.vm_instance  = vm_instance
+        self.use_postanalysis = use_postanalysis
+        assert len(input_layers) == 2
+        self.input_layers = input_layers
+        # Update dependencies: add dependencies specific to post_morph
+        if self.use_postanalysis and input_compound_tokens_layer is not None:
+            self.input_layers.append( input_compound_tokens_layer )
+        if self.stem:
+            # Modify output layer's attributes: remove lemma
+            new_output_attributes = ()
+            for attr in self.output_attributes:
+                if attr != 'lemma':
+                    new_output_attributes += (attr,)
+            self.output_attributes = new_output_attributes
         self.correct_verb_annotation = correct_verb_annotation
         self.change_to_bert_form = change_to_bert_form
         
@@ -69,22 +158,49 @@ class VabamorfWithBertTagger(Tagger):
                     "(!) Slang words lexicon with suffix '_nosp' not found from the default list of lexicons: {!r}".format(VM_LEXICONS)
                 _vm_instance = Vabamorf( lexicon_dir=nosp_lexicons[-1] )
 
-        
-        self.vabamorf = VabamorfAnalyzer(output_layer=self.output_layer, vm_instance=_vm_instance)
-        self.post_morph = PostMorphAnalysisTagger(output_layer=self.output_layer)
-        self.bert = BertMorphTagger(output_layer=self.output_layer, 
-                                                            disambiguate=True, 
-                                                            device=self.device, 
-                                                            correct_verb_annotation=self.correct_verb_annotation,
-                                                            change_to_bert_form=self.change_to_bert_form,)
-        self.use_postanalysis = use_postanalysis
+        self.vabamorf = VabamorfAnalyzer(output_layer=self.output_layer, 
+                                         input_words_layer=self.input_layers[0],
+                                         input_sentences_layer=self.input_layers[1],
+                                         compound=self.compound,
+                                         phonetic=self.phonetic,
+                                         stem=self.stem, 
+                                         vm_instance=_vm_instance)
+        input_compound_tokens_layer = "compound_tokens" if input_compound_tokens_layer is None else input_compound_tokens_layer
+        self.post_morph = PostMorphAnalysisTagger(output_layer=self.output_layer,
+                                                  input_compound_tokens_layer=input_compound_tokens_layer,
+                                                  input_words_layer=self.input_layers[0],
+                                                  stem=self.stem)
+        self.bert_disamb = BertMorphTagger(output_layer=self.output_layer, 
+                                           words_layer=self.input_layers[0],
+                                           sentences_layer=self.input_layers[1],
+                                           disambiguate=True, 
+                                           device=self.device, 
+                                           correct_verb_annotation=self.correct_verb_annotation,
+                                           change_to_bert_form=self.change_to_bert_form,)
 
-       
+
     def _make_layer_template(self):
+        """Creates and returns a template of the layer."""
         return Layer(name=self.output_layer, text_object=None, attributes=self.output_attributes, parent=self.input_layers[0], ambiguous=True)
-        
-    
+
+
     def _make_layer(self, text, layers, status=None):
+        """Analyses given Text object morphologically.
+
+        Parameters
+        ----------
+        text: estnltk.text.Text
+            Text object that is to be analysed morphologically.
+            The Text object must have layers 'words', 'sentences'.
+
+        layers: MutableMapping[str, Layer]
+           Layers of the text. Contains mappings from the
+           name of the layer to the Layer object. Must contain
+           words, and sentences;
+
+        status: dict
+           This can be used to store metadata on layer tagging.
+        """
 
         # --------------------------------------------
         #   Morphological analysis
@@ -92,18 +208,26 @@ class VabamorfWithBertTagger(Tagger):
         #text.tag_layer( self.vabamorf.input_layers )
         morph_layer = self.vabamorf.make_layer( text, layers, status )
 
-        # --------------------------------------------
-        #   Adding necessary layers for post processing
-        # -------------------------------------------- 
         layers2 = layers.copy()
         layers2["words"] = text["words"]
         layers2["sentences"] = text["sentences"]
-        compound_tokens = \
-           Layer(name=CompoundTokenTagger.output_layer, \
-                 attributes=CompoundTokenTagger.output_attributes, \
-                 text_object=text,\
-                 ambiguous=False)
-        layers2["compound_tokens"] = compound_tokens
+        # --------------------------------------------
+        #   Adding necessary layers for post processing
+        # -------------------------------------------- 
+        if self.use_postanalysis and self.post_morph:
+            if len(self.input_layers) == 2:
+                # Make an empty compound tokens layer
+                compound_tokens = \
+                   Layer(name=CompoundTokenTagger.output_layer, \
+                         attributes=CompoundTokenTagger.output_attributes, \
+                         text_object=text,\
+                         ambiguous=False)
+                layers2["compound_tokens"] = compound_tokens
+            else:
+                # Assure a compound tokens layer exists
+                assert self.post_morph.input_layers[0] in layers2.keys(), \
+                    f'(!) Missing required input layer {self.post_morph.input_layers[0]}'
+
         layers2[self.output_layer] = morph_layer
         
         # --------------------------------------------
@@ -119,9 +243,9 @@ class VabamorfWithBertTagger(Tagger):
                         delattr(annotation, IGNORE_ATTR)
                         
         # --------------------------------------------
-        #   Bert based analysis
+        #   Bert based disambiguation
         # --------------------------------------------
-        self.bert.change_layer( text, layers2, status )
+        self.bert_disamb.change_layer( text, layers2, status )
 
         return morph_layer
 
